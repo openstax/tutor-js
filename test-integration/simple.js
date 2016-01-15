b7b961cc2c0c96d81f850be98443d975584771fb
Philip Schatz <phil@cnx.org>
2016-01-15 09:59:50 -0500

    add support for babel in addition to coffee files
    starting with the tests

---
+++ test-integration/simple.js
@@ -0,0 +1,19 @@
+import {describe, CourseSelect, Calendar, ReadingBuilder} from './helpers';
+import {expect} from 'chai';
+
+const TEACHER_USERNAME = 'teacher01';
+
+describe('Draft Tests', function() {
+
+  this.it('Shows Validation Error when saving a blank Reading, Homework, and External (idempotent)', async function() {
+    this.addTimeout(30);
+
+    const title = this.freshId();
+    var y = await this.login(TEACHER_USERNAME);
+    console.log('Done logging in. No need for promises');
+
+    // Go to the 1st courses dashboard
+    var x = await CourseSelect.goTo(this, 'ANY');
+    console.log('Done selecting a course. No need for promises');
+  });
+});
