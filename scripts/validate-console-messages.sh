685fa332cbd75572573baa5cd7a6682e46d8811f
Philip Schatz <phil@cnx.org>
2016-08-09 15:27:33 -0400

    print console.log for exercises and shared components

--- scripts/validate-console-messages.sh
+++ scripts/validate-console-messages.sh
@@ -2,10 +2,21 @@
 
 # Check that there are no added console.log messages
 
-MESSAGES=$(grep 'console\.log' -R ./tutor/src/)
+MESSAGES=""
+MESSAGES="${MESSAGES}$(grep 'console\.log' -R ./tutor/src/)"
 MESSAGES="${MESSAGES}$(grep 'console\.info' -R ./tutor/src/)"
 # MESSAGES="${MESSAGES}$(grep 'console\.warn' -R ./tutor/src/)"
 # MESSAGES="${MESSAGES}$(grep 'alert' -R ./tutor/src/)"
+
+MESSAGES="${MESSAGES}$(grep 'console\.log' -R ./exercises/src/)"
+MESSAGES="${MESSAGES}$(grep 'console\.info' -R ./exercises/src/)"
+MESSAGES="${MESSAGES}$(grep 'console\.warn' -R ./exercises/src/)"
+MESSAGES="${MESSAGES}$(grep 'alert' -R ./exercises/src/)"
+
+MESSAGES="${MESSAGES}$(grep 'console\.log' -R ./shared/src/)"
+MESSAGES="${MESSAGES}$(grep 'console\.info' -R ./shared/src/)"
+MESSAGES="${MESSAGES}$(grep 'console\.warn' -R ./shared/src/)"
+MESSAGES="${MESSAGES}$(grep 'alert' -R ./shared/src/)"
 
 if [ "$(echo ${MESSAGES} | wc -w)" -gt 0 ];
 then
