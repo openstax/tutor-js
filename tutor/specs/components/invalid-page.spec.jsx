c9b0a7b35004e471479b5c54bf18f73aa76f6c94
Nathan Stitt <nathan@stitt.org>
2017-02-24 09:07:20 -0600

    update snapshot, add spec for message

--- tutor/specs/components/invalid-page.spec.jsx
+++ tutor/specs/components/invalid-page.spec.jsx
@@ -9,4 +9,9 @@ describe('Invalid Page', () => {
       <Wrapper _wrapped_component={InvalidPage} noReference />).toJSON()
     ).toMatchSnapshot();
   });
+
+  it('renders a custom message', () => {
+    const wrapper = shallow(<InvalidPage message="Yo test message" />);
+    expect(wrapper.text()).toContain('Yo');
+  });
 });
