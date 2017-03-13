dc177b6369b1f1a9f19a9b9952e0f9e92e186359
Nathan Stitt <nathan@stitt.org>
2017-03-13 17:51:02 -0500

    check in and out of the context

--- tutor/src/components/tours/region.jsx
+++ tutor/src/components/tours/region.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { observable } from 'mobx';
 import { inject, observer } from 'mobx-react';
-
+import invariant from 'invariant';
+import TourRegionModel from '../../models/tour/region';
 import TourContext from '../../models/tour/context';
 
 
@@ -9,32 +10,43 @@ import TourContext from '../../models/tour/context';
 export default class TourRegion extends React.PureComponent {
 
   static defaultProps = {
-    courseId: false,
+    courseId: undefined,
   }
 
   static propTypes = {
-    tourIds: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
-    children: React.PropTypes.node.isRequired,
+    id: React.PropTypes.string.isRequired,
     courseId: React.PropTypes.string,
+    children: React.PropTypes.node.isRequired,
     tourContext: React.PropTypes.instanceOf(TourContext).isRequired,
   }
 
   constructor(props) {
     super(props);
-//    this.tourContext = new TourContext(this.props);
+    this.region = TourRegionModel.forIdentifier(this.props.id);
+    invariant(this.region, `Region for ID: ${this.props.id} was not found`);
   }
-  //
-  //   componentWillReceiveProps(nextProps) {
-  //     this.tourContext.update(nextProps);
-  //   }
+
+  componentDidMount() {
+    this.region.el = this.wrapperEl;
+    this.props.tourContext.openRegion(this.region, this.props);
+  }
+
+  // not really sure this is needed, but we may update the courseId somwhere
+  componentWillReceiveProps({ id, ...otherProps }) {
+    invariant(id === this.props.id,
+              `Cannot update region id, was ${this.props.id} attempted to set ${id}`);
+    this.props.tourContext.updateRegion(this.region, otherProps);
+  }
 
   componentWillUnmount() {
-    this.tourContext.shutdown();
+    this.props.tourContext.closeRegion(this.region);
   }
 
   render() {
     return (
-      {this.props.children}
+      <div data-tour-region-id={this.props.id} ref={ref => (this.wrapperEl = ref)}>
+        {this.props.children}
+      </div>
     );
   }
 
