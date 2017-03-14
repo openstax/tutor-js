25e9353862741b228db1e41a6787bfa13a6dacae
Nathan Stitt <nathan@stitt.org>
2017-03-14 16:50:17 -0500

    rides, the act of viewing a tour

---
+++ tutor/specs/models/tour/ride.spec.js
@@ -0,0 +1,33 @@
+import { bootstrapCoursesList } from '../../courses-test-data';
+import Tour from '../../../src/models/tour';
+import TourContext from '../../../src/models/tour/context';
+import TourRide from '../../../src/models/tour/ride';
+import TourRegion from '../../../src/models/tour/region';
+
+describe('Tour View Model', () => {
+  let context;
+  let region;
+  let ride;
+  beforeEach(() => {
+    bootstrapCoursesList();
+    context = new TourContext();
+    region = new TourRegion({ id: 'foo', courseId: '1', tour_ids: ['teach-new-preview'] });
+    context.openRegion(region);
+    ride = context.tourRide;
+  });
+
+  it('calculates props for joyride', () => {
+    expect(ride.joyrideProps).toMatchObject({
+      tourId: 'teach-new-preview',
+    });
+
+    expect(ride.joyrideProps.steps).toHaveLength(1);
+    expect(ride.joyrideProps.steps[0]).toMatchObject({
+      title: 'Your Openstax Tutor beta course dashboard',
+      selector: '[data-tour-region-id="foo"]',
+      position: 'center',
+    });
+
+  });
+
+});
