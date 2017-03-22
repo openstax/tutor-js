272ba758970ca3a15dec407167d9bc13e62b5a3d
Nathan Stitt <nathan@stitt.org>
2017-03-22 14:00:54 -0500

    move JSON into tutor/src/tours

---
+++ tutor/src/tours/index.js
@@ -0,0 +1,8 @@
+/* global require:true */
+import { extend } from 'lodash';
+
+export default {
+
+  'teach-new-preview': require('./teacher-calendar.json'),
+  'add-homework': require('./add-homework.json'),
+};
