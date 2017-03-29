dbd6ee758abbacab119f609d9d37fd427eec5dfb
Nathan Stitt <nathan@stitt.org>
2017-03-29 10:31:48 -0500

    content for scores training wheels

--- tutor/src/tours/index.js
+++ tutor/src/tours/index.js
@@ -4,6 +4,7 @@ import { extend, each } from 'lodash';
 const TOURS = {};
 
 [
+  require('./scores.json'),
   require('./teacher-calendar.json'),
   require('./add-homework.json'),
 ].forEach(tours => tours.forEach(tour => TOURS[tour.id] = tour));
