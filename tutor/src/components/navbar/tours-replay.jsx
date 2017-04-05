251d2d4c0c89b752c09bfeb009ad169a3c44c4f7
Nathan Stitt <nathan@stitt.org>
2017-04-05 17:07:02 -0500

    scale image size fit in bar

--- tutor/src/components/navbar/tours-replay.jsx
+++ tutor/src/components/navbar/tours-replay.jsx
@@ -34,7 +34,7 @@ export default class ToursReplay extends React.PureComponent {
         className={classes}
       >
         <svg
-          width="100%" height="100%" viewBox="0 0 12 14" version="1.1"
+          width="24" height="24" viewBox="0 0 12 14" version="1.1"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path d={[
