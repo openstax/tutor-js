d506f4eadeccb2240c7088f275267f7b9d26ea99
Nathan Stitt <nathan@stitt.org>
2017-04-05 15:36:29 -0500

    task panel rework with models

--- tutor/src/components/task/progress/panel.jsx
+++ tutor/src/components/task/progress/panel.jsx
@@ -1,68 +1,142 @@
-React = require 'react'
-PureRenderMixin = require 'react-addons-pure-render-mixin'
+import React from 'react';
 
-keymaster = require 'keymaster'
+import TaskUI from '../../../models/task/ui';
+import PagingNavigation from '../../paging-navigation';
 
-PagingNavigation  = require '../../paging-navigation'
+export default class TaskProgressPanel extends React.PureComponent {
+  static propTypes = {
+    taskUI: React.PropTypes.instanceOf(TaskUI).isRequired,
+    enableKeys: React.PropTypes.bool.isRequired,
+    children: React.PropTypes.node.isRequired,
+  }
 
-{TaskStore} = require '../../../flux/task'
-{StepPanel} = require '../../../helpers/policies'
-{TaskStepStore, TaskStepActions} = require '../../../flux/task-step'
+  render() {
+    const { taskUI: ui, enableKeys } = this.props;
 
-ProgressPanel = React.createClass
-  propTypes:
-    taskId: React.PropTypes.string
-    stepId: React.PropTypes.string
-    stepKey: React.PropTypes.number
-    goToStep: React.PropTypes.func
+    return (
+      <PagingNavigation
+        className="progress-panel"
+        enableKeys={enableKeys}
+        isForwardEnabled={ui.canGoForward}
+        isBackwardEnabled={ui.canGoBackward}
+        onForwardNavigation={ui.goForward}
+        onBackwardNavigation={ui.goBackward}
+      >
+        {this.props.children}
+      </PagingNavigation>
+    );
 
-  mixins: [PureRenderMixin]
+  }
 
-  getInitialState: ->
-    @getShouldShows()
+}
 
-  componentWillUnmount: ->
-    TaskStepStore.off('step.completed', @updateShouldShows)
 
-  componentWillMount: ->
-    TaskStepStore.on('step.completed', @updateShouldShows)
-
-  componentWillReceiveProps: (nextProps) ->
-    @setState(@getShouldShows(nextProps))
-
-  getShouldShows: (props = @props) ->
-    {stepKey, stepId, isSpacer} = props
-    shouldShowLeft: stepKey > 0
-    shouldShowRight: isSpacer or (stepId? and StepPanel.canForward(stepId))
-
-  updateShouldShows: ->
-    @setState(@getShouldShows())
-
-  goForward: ->
-    { stepId } = @props
-    if stepId and not TaskStepStore.get(stepId)?.is_completed
-      TaskStepStore.once('step.completed', =>
-        @props.goToStep(@props.stepKey + 1)
-      )
-      TaskStepActions.complete(stepId)
-    else
-      @props.goToStep(@props.stepKey + 1)
-    undefined # silence React return value warning
-
-  goBackward: ->
-    @props.goToStep(@props.stepKey - 1)
-    undefined # silence React return value warning
-
-  render: ->
-    <PagingNavigation
-      className="progress-panel"
-      enableKeys={@props.enableKeys}
-      isForwardEnabled={@state.shouldShowRight}
-      isBackwardEnabled={@state.shouldShowLeft}
-      onForwardNavigation={@goForward}
-      onBackwardNavigation={@goBackward}
-    >
-      {@props.children}
-    </PagingNavigation>
-
-module.exports = ProgressPanel
+  // import React from 'react';
+  // import PureRenderMixin from 'react-addons-pure-render-mixin';
+  //
+  // import keymaster from 'keymaster';
+  //
+  // import PagingNavigation from '../../paging-navigation';
+  //
+  // import { TaskStore } from '../../../flux/task';
+  // import { StepPanel } from '../../../helpers/policies';
+  // import { TaskStepStore, TaskStepActions } from '../../../flux/task-step';
+  //
+  // const ProgressPanel = React.createClass({
+  //   propTypes: {
+  //     taskId: React.PropTypes.string,
+  //     stepId: React.PropTypes.string,
+  //     stepKey: React.PropTypes.number,
+  //     goToStep: React.PropTypes.func,
+  //   },
+  //
+  //   mixins: [PureRenderMixin],
+  //
+  //   getInitialState() {
+  //     return (
+  //         this.getShouldShows()
+  //     );
+  //   },
+  //
+  //   componentWillUnmount() {
+  //     return (
+  //         TaskStepStore.off('step.completed', this.updateShouldShows)
+  //     );
+  //   },
+  //
+  //   componentWillMount() {
+  //     return (
+  //         TaskStepStore.on('step.completed', this.updateShouldShows)
+  //     );
+  //   },
+  //
+  //   componentWillReceiveProps(nextProps) {
+  //     return (
+  //         this.setState(this.getShouldShows(nextProps))
+  //     );
+  //   },
+  //
+  //   getShouldShows(props) {
+  //     if (props == null) { ({ props } = this); }
+  //     const { stepKey, stepId, isSpacer } = props;
+  //     return (
+  //     {
+  //       shouldShowLeft: stepKey > 0,
+  //       shouldShowRight: isSpacer || ((stepId != null) && StepPanel.canForward(stepId)),
+  //     }
+  //     );
+  //   },
+  //
+  //   updateShouldShows() {
+  //     return (
+  //         this.setState(this.getShouldShows())
+  //     );
+  //   },
+  //
+  //   goForward() {
+  //     const { stepId } = this.props;
+  //     if (stepId && !__guard__(TaskStepStore.get(stepId), x => x.is_completed)) {
+  //       TaskStepStore.once('step.completed', () => {
+  //         return (
+  //             this.props.goToStep(this.props.stepKey + 1)
+  //         );
+  //       }
+  //       );
+  //       TaskStepActions.complete(stepId);
+  //     } else {
+  //       this.props.goToStep(this.props.stepKey + 1);
+  //     }
+  //     return (
+  //         undefined
+  //     ); // silence React return value warning
+  //   },
+  //
+  //   goBackward() {
+  //     this.props.goToStep(this.props.stepKey - 1);
+  //     return (
+  //         undefined
+  //     ); // silence React return value warning
+  //   },
+  //
+  //   render() {
+  //     return (
+  //       <PagingNavigation
+  //         className="progress-panel"
+  //         enableKeys={this.props.enableKeys}
+  //         isForwardEnabled={this.state.shouldShowRight}
+  //         isBackwardEnabled={this.state.shouldShowLeft}
+  //         onForwardNavigation={this.goForward}
+  //         onBackwardNavigation={this.goBackward}>
+  //         {this.props.children}
+  //       </PagingNavigation>
+  //     );
+  //   },
+  // });
+  //
+  // export default ProgressPanel;
+  //
+  // function __guard__(value, transform) {
+  //   return (
+  //       (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined
+  //   );
+  // }
