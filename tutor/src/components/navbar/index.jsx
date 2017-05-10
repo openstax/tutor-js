5e780095a8d2c634125b9ec095d2ded89524be4c
Nathan Stitt <nathan@stitt.org>
2017-05-10 15:28:49 -0500

    remove uneeded collapseNav

--- tutor/src/components/navbar/index.jsx
+++ tutor/src/components/navbar/index.jsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import { Navbar, Nav } from 'react-bootstrap';
-import Router from '../../helpers/router';
-import ToursReplay from './tours-replay';
-import CourseName from './course-name';
+import Router                from '../../helpers/router';
+import Courses               from '../../models/courses-map';
+import TutorLink             from '../link';
 import ServerErrorMonitoring from '../error-monitoring';
-import UserActionsMenu from './user-actions-menu';
-import BookLinks from './book-links';
-import CenterControls from './center-controls';
-import TutorLink from '../link';
+import ToursReplay           from './tours-replay';
+import CourseName            from './course-name';
+import UserActionsMenu       from './user-actions-menu';
+import BookLinks             from './book-links';
+import CenterControls        from './center-controls';
+import PreviewAddCourseBtn   from './preview-add-course-btn';
 
-import Courses from '../../models/courses-map';
-import PreviewAddCourseBtn from './preview-add-course-btn';
-import { action } from 'mobx';
 
 export default class NavigationBar extends React.PureComponent {
-
-  @action.bound
-  collapseNav() {
-    const navBar = ReactDOM.findDOMNode(this.navBar);
-    const collapsibleNav = navBar.querySelector('div.navbar-collapse');
-    const toggleBtn = navBar.querySelector('button.navbar-toggle');
-    if (collapsibleNav.classList.contains('in')) { toggleBtn.click(); }
-  }
 
   render() {
     const params = Router.currentParams();
@@ -31,16 +20,13 @@ export default class NavigationBar extends React.PureComponent {
     const course = courseId ? Courses.get(courseId) : null;
 
     return (
-      <nav
-        className="tutor-top-navbar"
-        ref={nb => (this.navBar = nb)}
-      >
+      <nav className="tutor-top-navbar">
         <div className="left-side-controls">
           <TutorLink to="listing" className="brand">
             <i className="ui-brand-logo" />
           </TutorLink>
           <CourseName course={course} />
-          <BookLinks courseId={courseId} onItemClick={this.collapseNav} />
+          <BookLinks courseId={courseId} />
         </div>
         <CenterControls params={params} />
         <div className="right-side-controls">
