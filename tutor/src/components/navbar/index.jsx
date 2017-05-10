1f6b92f3f845fae037017d4e18694cda31b6a905
Nathan Stitt <nathan@stitt.org>
2017-05-10 15:30:13 -0500

    convert to pure function

--- tutor/src/components/navbar/index.jsx
+++ tutor/src/components/navbar/index.jsx
@@ -12,32 +12,29 @@ import CenterControls        from './center-controls';
 import PreviewAddCourseBtn   from './preview-add-course-btn';
 
 
-export default class NavigationBar extends React.PureComponent {
+export default function NavigationBar() {
+  const params = Router.currentParams();
+  const { courseId } = params;
+  const course = courseId ? Courses.get(courseId) : null;
 
-  render() {
-    const params = Router.currentParams();
-    const { courseId } = params;
-    const course = courseId ? Courses.get(courseId) : null;
-
-    return (
-      <nav className="tutor-top-navbar">
-        <div className="left-side-controls">
-          <TutorLink to="listing" className="brand">
-            <i className="ui-brand-logo" />
-          </TutorLink>
-          <CourseName course={course} />
-          <BookLinks courseId={courseId} />
-        </div>
-        <CenterControls params={params} />
-        <div className="right-side-controls">
-          <PreviewAddCourseBtn courseId={courseId} />
-          <ToursReplay />
-          <UserActionsMenu
-            courseId={courseId}
-          />
-        </div>
-        <ServerErrorMonitoring />
-      </nav>
-    );
-  }
+  return (
+    <nav className="tutor-top-navbar">
+      <div className="left-side-controls">
+        <TutorLink to="listing" className="brand">
+          <i className="ui-brand-logo" />
+        </TutorLink>
+        <CourseName course={course} />
+        <BookLinks courseId={courseId} />
+      </div>
+      <CenterControls params={params} />
+      <div className="right-side-controls">
+        <PreviewAddCourseBtn courseId={courseId} />
+        <ToursReplay />
+        <UserActionsMenu
+          courseId={courseId}
+        />
+      </div>
+      <ServerErrorMonitoring />
+    </nav>
+  );
 }
