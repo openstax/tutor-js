29cee87fa27ad05496878451060f9f5f67ea82cf
Nathan Stitt <nathan@stitt.org>
2017-05-23 10:06:16 -0500

    Track if user has viewed preview warning on per-session basis

--- tutor/src/models/course/preview-ux.js
+++ tutor/src/models/course/preview-ux.js
@@ -7,28 +7,44 @@ import Courses from '../courses-map';
 import { filter, find } from 'lodash';
 import User from '../user';
 import TeacherTaskPlans from '../teacher-task-plans';
+import { TaskPlanStore } from '../../flux/task-plan';
 
 import Nags from '../../components/course-preview/nags';
 
 const IS_DISMISSED = observable.box(false);
+const HAS_PUBLISHED = observable.box(false);
+TaskPlanStore.on('publish-queued', () => HAS_PUBLISHED.set(true))
 
 export default class CoursePreviewUX extends BasicCourseUX {
+
+  hasViewedPublishWarning() {
+    HAS_PUBLISHED.set(false);
+    IS_DISMISSED.set(true);
+  }
 
   set isDismissed(value) {
     IS_DISMISSED.set(value);
   }
 
   @computed get shouldWarnPreviewOnly() {
+    if (!HAS_PUBLISHED.get() ||
+        this.hasCreatedRealCourse ||
+        TeacherTaskPlans.hasApiRequestPending
+       ) { return false; }
+
     const plans = TeacherTaskPlans.forCourseId(this.course.id).active;
     const realPlanCount = filter(plans, { is_preview: false }).length;
+
     return (realPlanCount > 0 && realPlanCount % 2 === 0);
   }
 
   @computed get nagComponent() {
+    // we warn about creating assigments in a preview regardless of previous dismissals
+    if (this.shouldWarnPreviewOnly)  { return Nags.previewOnlyWarning;  }
+
     if (IS_DISMISSED.get() || this.hasCreatedRealCourse) { return null; }
 
     if (this.course.hasEnded)       { return Nags.expiredPreviewWarning; }
-    if (this.shouldWarnPreviewOnly) { return Nags.previewOnlyWarning;    }
     if (User.sessionCount > 2)      { return Nags.secondSessionWarning;  }
     return null;
   }
@@ -41,4 +57,7 @@ export default class CoursePreviewUX extends BasicCourseUX {
     return !this.hasCreatedRealCourse;
   }
 
+  onTaskPlanPublish() {
+    HAS_PUBLISHED.set(true);
+  }
 }
