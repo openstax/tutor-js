679a4e767ae4ef3fdab008c3d929e4fcc8680538
Nathan Stitt <nathan@stitt.org>
2017-05-30 16:14:12 -0500

    use our own button for launching chat

--- tutor/src/components/my-courses/pending-verification.jsx
+++ tutor/src/components/my-courses/pending-verification.jsx
@@ -6,7 +6,22 @@ import Icon from '../icon';
 export default class PendingVerification extends React.PureComponent {
 
   componentDidMount() {
-    Chat.setElementVisiblity(this.onlineChatImg);
+    Chat.setElementVisiblity(this.onlineChatButton);
+  }
+
+  renderChat() {
+    if (!Chat.isEnabled) { return null; }
+
+    return (
+      <button
+        className="chat btn"
+        ref={btn => this.onlineChatButton = btn}
+        style={{ display: 'none' }}
+        onClick={Chat.start}
+      >
+        <Icon type='comment' /><span>VERIFY NOW VIA CHAT</span>
+      </button>
+    );
   }
 
   render() {
@@ -23,15 +38,7 @@ export default class PendingVerification extends React.PureComponent {
             <p className="lead">
               We’re manually verifying that you’re an instructor and we’ll email you in 3-4 business days when your account is ready.
             </p>
-
-            <img
-              id='liveagent_button_online_573U0000000k9cB'
-              ref={img => this.onlineChatImg = img}
-              style={{ display: 'none', border: '0', cursor: 'pointer' }}
-              onClick={Chat.start}
-              src="https://openstax.secure.force.com/support/resource/1491006720000/chat_online_button"
-            />
-
+            {this.renderChat()}
           </div>
         </div>
       </div>
--- tutor/src/models/chat.js
+++ tutor/src/models/chat.js
@@ -1,6 +1,7 @@
-// FIXME - get real values
-const DEPLOYMENT_ID = '572U0000000k9cB';
+const DEPLOYMENT_ID   = '572U0000000k9cB';
 const ORGANIZATION_ID = '00DU0000000Kwch';
+const ELEMENT_ID      = '573U0000000k9cB';
+
 import { computed } from 'mobx';
 import User from './user';
 
@@ -12,17 +13,20 @@ export default {
 
   initialize() {
     window.liveagent.init('https://d.la2-c2-dfw.salesforceliveagent.com/chat', DEPLOYMENT_ID, ORGANIZATION_ID);
+
+    // will emit debugging info to the console
+    // liveagent.enableLogging();
   },
 
   setElementVisiblity(online, offline) {
-    window.liveagent.showWhenOnline(DEPLOYMENT_ID, online);
+    window.liveagent.showWhenOnline(ELEMENT_ID, online);
     if (offline) {
-      window.liveagent.showWhenOffline(DEPLOYMENT_ID, offline);
+      window.liveagent.showWhenOffline(ELEMENT_ID, offline);
     }
   },
 
   start() {
-    window.liveagent.startChat(DEPLOYMENT_ID);
+    window.liveagent.startChat(ELEMENT_ID);
   },
 
 };
