3aea257317422fad665636f3ab1407c80eee82c1
Nathan Stitt <nathan@stitt.org>
2017-06-05 10:16:48 -0500

    add filter for finding courses that are being taught

--- tutor/src/models/courses-map.js
+++ tutor/src/models/courses-map.js
@@ -22,6 +22,10 @@ class AllCoursesMap extends CoursesMap {
     return this.where(c => c.is_active);
   }
 
+  @computed get teaching() {
+    return this.where(c => c.isTeacher);
+  }
+
   @computed get completed() {
     return this.where(c => c.hasEnded);
   }
