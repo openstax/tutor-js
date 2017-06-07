2caf5c814b72d7c8c582f58e0b75dbb1a76bd646
Nathan Stitt <nathan@stitt.org>
2017-06-07 15:49:11 -0500

    Add approved assigment videos

--- tutor/specs/components/student-preview/builder-popup.spec.jsx
+++ tutor/specs/components/student-preview/builder-popup.spec.jsx
@@ -1,11 +1,15 @@
-import BuilderPopup from '../../../src/components/student-preview/builder-popup'
 import FakeWindow from 'shared/specs/helpers/fake-window';
+import BuilderPopup from '../../../src/components/student-preview/builder-popup';
+import { bootstrapCoursesList } from '../../courses-test-data';
 
 describe('Student Preview Builder', () => {
   let props;
   beforeEach(() => {
+    const course = bootstrapCoursesList().get('2');
+    course.appearance_code = 'college_biology';
     props = {
-      courseId:   '1',
+      courseId: '2',
+      planType: 'homework',
       windowImpl: new FakeWindow(),
     };
   });
@@ -13,10 +17,10 @@ describe('Student Preview Builder', () => {
   it('opens a preview window when clicked', () => {
     const builder = mount(<BuilderPopup {...props} />);
     builder.simulate('click');
-    expect(props.windowImpl.open).to.have.been.called
+    expect(props.windowImpl.open).to.have.been.called;
     expect(props.windowImpl.openedDOM.window.document.body.textContent).toContain(
       'coming soon'
-    )
+    );
   });
 
 });
