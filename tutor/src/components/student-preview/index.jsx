96984f2a418fa59cb376f09b93bec6cbb7fe8816
Nathan Stitt <nathan@stitt.org>
2017-06-14 11:56:02 -0500

    update copy and video

--- tutor/src/components/student-preview/index.jsx
+++ tutor/src/components/student-preview/index.jsx
@@ -12,12 +12,12 @@ export default function StudentPreview() {
         <Col sm={6}>
           <h3>Student dashboard</h3>
           <p>
-            Cat ipsum dolor sit amet, spit up on light gray carpet instead of adjacent linoleum fall asleep on the washing machine. Poop on grasses wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again, loves cheeseburgers. Slap kitten brother with paw put toy mouse in food bowl run out of litter box at full speed sleep on keyboard cat is love, cat is life chase mice purrr purr littel cat, little cat purr purr. Steal the warm chair right after you get up sit in window and stare oooh, a bird, yum for purr for no reason caticus cuteicus. Eat half my food and ask for more. Find a way to fit in tiny box chew foot. Slap owner's face at 5am until human fills food dish asdflkjaertvlkjasntvkjn (sits on keyboard). Lick the curtain just to be annoying lies down if it smells like fish eat as much as you wish. Climb leg sleep on dog bed, force dog to sleep on floor you call this cat food cat not kitten around and give me attention or face the wrath of my claws. Freak human out make funny noise mow mow mow mow mow mow success now attack human going to catch the red dot today going to catch the red dot today. Scratch at fleas, meow until belly rubs, hide behind curtain when vacuum cleaner is on scratch strangers and poo on owners food fall over dead (not really but gets sypathy).
+            The dashboard gives students an overview of the course, assignments, progress, and performance. Students can see when assignments are due, start current assignments, review past work, and access their textbook. The student Performance Forecast shows students their performance within each section of the textbook, highlights their weaker areas, and lets them practice on their own.
           </p>
         </Col>
         <Col sm={6}>
           <YouTube
-            videoId="IbYU5py9YP8"
+            videoId="AEUgriQUjPc"
             opts={{
               width: '100%',
             }}
@@ -27,9 +27,9 @@ export default function StudentPreview() {
 
       <Row>
         <Col sm={6}>
-          <h3>Homework assignments</h3>
+          <h3>Reading assignment</h3>
           <p>
-            Sit on the laptop tuxedo cats always looking dapper scratch me there, elevator butt eat the fat cats food for pushes butt to face yet decide to want nothing to do with my owner today. Kitty kitty lick sellotape, for all of a sudden cat goes crazy, so immediately regret falling into bathtub for warm up laptop with butt lick butt fart rainbows until owner yells pee in litter box hiss at cats for lay on arms while you're using the keyboard jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed. Eat grass, throw it back up. Cough hairball on conveniently placed pants meowwww. Rub face on everything shove bum in owner's face like camera lens refuse to drink water except out of someone's glass for stare out the window so attack dog, run away and pretend to be victim i cry and cry and cry unless you pet me, and then maybe i cry just for fun stare out the window. Scratch at fleas, meow until belly rubs, hide behind curtain when vacuum cleaner is on scratch strangers and poo on owners food a nice warm laptop for me to sit on for please stop looking at your phone and pet me, but climb a tree, wait for a fireman jump to fireman then scratch his face. Catch mouse and gave it as a present. Nap all day find empty spot in cupboard and sleep all day so the dog smells bad. Hiss at vacuum cleaner shove bum in owner's face like camera lens lie on your belly and purr when you are asleep. Eat the fat cats food scratch at the door then walk away yet my left donut is missing, as is my right. Please stop looking at your phone and pet me then cats take over the world eat the fat cats food for missing until dinner time. Hunt by meowing loudly at 5am next to human slave food dispenser throwup on your pillow, and mesmerizing birds touch water with paw then recoil in horror purr so sleep in the bathroom sink. Sit and stare kitty power licks your face but sit on human please stop looking at your phone and pet me.
+            OpenStax Tutor Beta reading assignments present the textbook to students in manageable chunks and engage students with videos, case studies, and interactive elements. As students read, each section is followed by personalized, two-step questions that give immediate feedback.
           </p>
         </Col>
         <Col sm={6}>
@@ -41,6 +41,24 @@ export default function StudentPreview() {
           />
         </Col>
       </Row>
+
+      <Row>
+        <Col sm={6}>
+          <h3>Homework assignment</h3>
+          <p>
+            After students work instructor-assigned questions, OpenStax Tutor Beta will unlock personalized and spaced practice questions chosen specifically for each student. These two-step questions prompt students to recall the answer from memory before selecting a multiple choice option.
+          </p>
+        </Col>
+        <Col sm={6}>
+          <YouTube
+            videoId="Aoco1IRixZA"
+            opts={{
+              width: '100%',
+            }}
+          />
+        </Col>
+      </Row>
+
 
       <Row>
         <Col sm={6}>
