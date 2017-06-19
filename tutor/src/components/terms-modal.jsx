c34f3570fa7b7eb54e3773f165ac2e0f07684446
Nathan Stitt <nathan@stitt.org>
2017-06-19 11:30:17 -0500

    include branding on terms header

--- tutor/src/components/terms-modal.jsx
+++ tutor/src/components/terms-modal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { observer, PropTypes as MobxPropTypes } from 'mobx-react';
 import { action, computed } from 'mobx';
-
+import Branding from './branding/course';
 import User from '../models/user';
 import { Modal, Button } from 'react-bootstrap';
 import { map, isEmpty } from 'lodash';
@@ -33,7 +33,7 @@ export default class TermsModal extends React.PureComponent {
         className="user-terms"
       >
         <Modal.Header>
-          {this.title}
+          <Branding isBeta={true} /> {this.title}
         </Modal.Header>
         <Modal.Body>
           {map(User.unsignedTerms, (t) =>
