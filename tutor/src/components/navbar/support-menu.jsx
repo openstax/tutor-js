825d9322a35dd4828275ed72eea143d39411da11
Nathan Stitt <nathan@stitt.org>
2017-06-20 11:56:26 -0500

    update chat styles

--- tutor/src/components/navbar/support-menu.jsx
+++ tutor/src/components/navbar/support-menu.jsx
@@ -61,7 +61,7 @@ export default class SupportMenu extends React.PureComponent {
         className="chat disabled"
         ref={opt => this.chatDisabled = opt}
       >
-        <Icon type='comment-o' /><span>Chat with Support</span>
+        <Icon type='comments-o' /><span>Chat with Support</span>
       </MenuItem>,
     ];
   }
