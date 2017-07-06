ce36c886b271557f039ff327194e52fe3493fd41
amanda <pandafulmanda@gmail.com>
2017-07-05 19:10:03 -0500

    create a course training view now closes on blur

---
+++ tutor/src/components/tours/custom/viewed-on-blur.jsx
@@ -0,0 +1,46 @@
+import React        from 'react';
+import ReactDOM     from 'react-dom';
+import { Tooltip }  from 'react-joyride';
+import { computed } from 'mobx';
+
+import { first }    from 'lodash';
+import classnames   from 'classnames';
+
+export default class ViewedOnBlur extends React.PureComponent {
+  className = 'viewed-on-blur-tooltip'
+
+  constructor(props) {
+    super(props)
+
+    this.checkIsBlur = this.checkIsBlur.bind(this);
+  }
+
+  componentDidMount() {
+    this.tooltipEl = ReactDOM.findDOMNode(this.tooltip);
+
+    document.addEventListener('click', this.checkIsBlur);
+    document.addEventListener('focus', this.checkIsBlur);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.checkIsBlur);
+    document.removeEventListener('focus', this.checkIsBlur);
+  }
+
+  checkIsBlur (focusEvent) {
+    if(focusEvent.target !== this.tooltipEl && !this.tooltipEl.contains(focusEvent.target)) {
+      this.props.step.joyrideRef.next();
+    }
+  }
+
+  render () {
+    const className = classnames(this.className,  this.props.className);
+
+    return (
+      <Tooltip
+        {...this.props}
+        className={className}
+        ref={tooltip => this.tooltip = tooltip}/>
+    );
+  }
+}
