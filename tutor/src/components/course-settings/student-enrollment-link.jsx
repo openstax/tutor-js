1eeb6f777c9edfbebfb7dbb9798a56c9fcab3b18
Nathan Stitt <nathan@stitt.org>
2017-07-14 15:48:04 -0500

    hide enrollment link for fall 2017 courses

--- tutor/src/components/course-settings/student-enrollment-link.jsx
+++ tutor/src/components/course-settings/student-enrollment-link.jsx
@@ -4,10 +4,29 @@ import { action } from 'mobx';
 import { get }    from 'lodash';
 import Course from '../../models/course';
 import TourAnchor from '../tours/anchor';
+import CopyOnFocusInput from '../copy-on-focus-input';
 
 // Approx how wide each char is in the text input
 // it's width will be set to this * link's character count
 const CHAR_WIDTH = 7;
+
+const Input = ({ period }) =>
+  <CopyOnFocusInput value={period.enrollment_url} readOnly />;
+
+const Preview = () =>
+  <span className="faux-disabled-input">No enrollment URL in preview.</span>;
+
+const NotYet = () =>
+  <span className="faux-disabled-input">Available August 1</span>;
+
+
+const EnrollmentLink = ({ course, period }) => {
+  if (course.is_preview) return <Preview />;
+  if (course.term == 'fall' && course.year == 2017 && !course.is_concept_coach) {
+    return <NotYet />;
+  }
+  return <Input period={period} />;
+};
 
 export default class StudentEnrollmentLink extends React.PureComponent {
 
@@ -18,28 +37,6 @@ export default class StudentEnrollmentLink extends React.PureComponent {
     course: React.PropTypes.instanceOf(Course).isRequired,
   }
 
-  @action.bound
-  selectText() {
-    this.input.select();
-  }
-
-  renderInput() {
-    return (
-      <input
-        type="text"
-        ref={ref => (this.input = ref)}
-        style={{ width: get(this.props.period.enrollment_url, 'length', 20) * CHAR_WIDTH }}
-        value={this.props.period.enrollment_url}
-        readOnly={true}
-      />
-    );
-  }
-
-  renderPreview() {
-    return (
-      <span className="disabled-for-preview">No enrollment URL in preview.</span>
-    );
-  }
 
   render() {
     return (
@@ -52,7 +49,7 @@ export default class StudentEnrollmentLink extends React.PureComponent {
         <span className="title">
           Student Enrollment URL:
         </span>
-        {this.props.course.is_preview ? this.renderPreview() : this.renderInput()}
+        <EnrollmentLink period={this.props.period} course={this.props.course} />
       </TourAnchor>
     );
   }
