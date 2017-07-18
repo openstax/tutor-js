27897dece9cb85ee4cf0952e301bc2804de22305
Nathan Stitt <nathan@stitt.org>
2017-07-18 12:26:03 -0500

    spec for purchase model

---
+++ tutor/specs/models/purchases/purchase.spec.js
@@ -0,0 +1,28 @@
+import Courses from '../../../src/models/courses-map';
+import { bootstrapCoursesList } from '../../courses-test-data';
+import Purchase from '../../../src/models/purchases/purchase';
+
+jest.mock('../../../src/flux/time', () => ({
+  TimeStore: {
+    getNow: jest.fn(() => new Date('2000-01-01')),
+  },
+}));
+
+describe('Purchase Model', () => {
+  let course, purchase;
+
+  beforeEach(() => {
+    course = bootstrapCoursesList().get('1');
+    purchase = new Purchase({ product_instance_uuid: course.userStudentRecord.uuid });
+  });
+
+  test('#course', () => {
+    expect(purchase.course).toBe(course);
+  });
+
+  test('#onRefunded', () => {
+    purchase.onRefunded();
+    expect(Courses.get(course.id)).toBeUndefined();
+  });
+
+});
