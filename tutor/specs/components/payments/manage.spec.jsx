1b2e40e0938cbe9803eb03fc7350b0381d72b850
Nathan Stitt <nathan@stitt.org>
2017-07-28 12:41:55 -0500

    Use fixed date for testing so snapshot is stable

--- tutor/specs/components/payments/manage.spec.jsx
+++ tutor/specs/components/payments/manage.spec.jsx
@@ -1,16 +1,30 @@
 import { Wrapper, SnapShot } from '../helpers/component-testing';
+import moment from 'moment';
 import Payments from '../../../src/components/payments/manage';
 import Purchases from '../../../src/models/purchases';
 import Router from '../../../src/helpers/router';
-
+import PaymentModel from '../../../src/models/payments';
 import mockData from '../../../api/purchases.json';
+import { TimeStore } from '../../../src/flux/time';
+import TimeHelper from '../../../src/helpers/time';
 
 jest.mock('../../../src/helpers/router');
+jest.mock('../../../src/models/payments');
+jest.mock('../../../src/flux/time', () => ({
+  TimeStore: {
+    getNow: () => new Date('2017-07-01'),
+  },
+}));
 
 describe('Student Payments Management', () => {
   beforeEach(() => {
+    PaymentModel.config = {
+      is_enabled: true,
+      base_url: 'url-for-test',
+    };
     Router.currentParams.mockReturnValue({ courseId: '2' });
     Router.makePathname.mockImplementation(() => '/foo');
+    mockData.orders.forEach((o) => o.purchased_at = '2017-06-25T00:00:00.000Z');
     Purchases.onLoaded({ data: mockData });
   });
 
