3ef40102362b3b122ec7f2d6322988b3bd394f65
Nathan Stitt <nathan@stitt.org>
2017-08-11 11:28:32 -0500

    Fix "canWork" to it only looks at completed_count if deleted

---
+++ tutor/specs/models/student/task.spec.js
@@ -0,0 +1,31 @@
+import Task from '../../../src/models/student/task';
+import moment from 'moment';
+import COURSE_1_DATA from '../../../api/courses/1/dashboard.json';
+
+const TASK = COURSE_1_DATA.tasks[0];
+jest.mock('../../../src/flux/time', () => ({
+  TimeStore: {
+    getNow: jest.fn(() => new Date()),
+  },
+}));
+
+describe('Student Task Model', () => {
+
+  let task;
+
+  beforeEach(() => {
+    task = new Task();
+  });
+
+  it('#canWork', () => {
+    task.opens_at = moment().add(1, 'day');
+    expect(task.canWork).toBe(false);
+    task.opens_at = moment().subtract(1, 'day');
+    expect(task.canWork).toBe(true);
+    task.is_deleted = true;
+    expect(task.canWork).toBe(true);
+    task.complete_exercise_count = 0;
+    expect(task.canWork).toBe(false);
+  });
+
+});
