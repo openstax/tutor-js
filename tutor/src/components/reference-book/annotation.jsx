8f05d6e4edb0cd68fba39433ae98fe832d96e477
Roy Johnson <roy.e.johnson@rice.edu>
2017-11-07 16:11:05 -0600

    Added sidebar bubbles

--- tutor/src/components/reference-book/annotation.jsx
+++ tutor/src/components/reference-book/annotation.jsx
@@ -7,8 +7,6 @@ import _ from 'underscore';
 
 const highlighter = new TextHighlighter(document.body);
 
-let preload;
-
 function dbEntryToPreload(entry) {
   return {
     savedId: entry.id,
@@ -17,8 +15,23 @@ function dbEntryToPreload(entry) {
   };
 }
 
+function getSelectionRect(selection) {
+  const rect = selection.getRangeAt(0).getBoundingClientRect();
+  const wLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  const wTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  return {
+    bottom: rect.bottom + wTop,
+    top: rect.top + wTop,
+    left: rect.left + wLeft,
+    right: rect.right + wLeft
+  }
+}
+
 function highlightEntry(entry) {
-  serializeSelection.restore(entry.selection);
+  const selection = serializeSelection.restore(entry.selection);
+
+  entry.rect = getSelectionRect(selection);
   highlighter.doHighlight();
 }
 
@@ -32,6 +45,15 @@ function unhighlightEntry(entry) {
     highlighter.removeHighlights(el);
   }
 }
+
+const HighlightWidget = ({style, annotate, highlight}) => (
+  style ?
+    <div className="widget arrow_box" style={style}>
+      <i className="fa fa-comment" role="button" alt="annotate" onClick={annotate}></i>
+      <i className="fa fa-pencil" role="button" alt="highlight" onClick={highlight}></i>
+    </div>
+  : null
+);
 
 const EditBox = (props) => {
   return (
@@ -47,18 +69,38 @@ const EditBox = (props) => {
           </button>
         </div>
         <div className="button-group">
-          <button aria-label="previous annotation">
+          <button aria-label="previous annotation" onClick={props.previous}>
             <i className="fa fa-chevron-up" role="button" ></i>
           </button>
           <button aria-label="next annotation">
-            <i className="fa fa-chevron-down" role="button" ></i>
+            <i className="fa fa-chevron-down" role="button" onClick={props.next}></i>
           </button>
           <button>See all</button>
         </div>
       </div>
     </div>
   );
-}
+};
+
+const SidebarButtons = ({items, offsetTop, onClick}) => (
+  <div>
+    {items.map((item) => {
+      const rect = item.rect;
+      const style = {
+        right: 0,
+        top: rect ? rect.top - offsetTop : 0,
+        position: 'absolute'
+      };
+
+      return item.annotation.length ?
+        <button className="sidebar-button" style={style} key={item.selection.start} onClick={() => onClick(item)}>
+          <i className="fa fa-comment"></i>
+        </button>
+        :
+        null;
+    })}
+  </div>
+);
 
 export default class AnnotationWidget extends React.PureComponent {
 
@@ -66,7 +108,8 @@ export default class AnnotationWidget extends React.PureComponent {
     super(props);
     this.state = {
       activeHighlight: null,
-      widgetStyle: {display: 'none'}
+      widgetStyle: {display: 'none'},
+      preload: []
     };
     // This is state that only affects post-rendering processes, so not proper state
     this.needToLoadAnnotations = true;
@@ -99,7 +142,7 @@ export default class AnnotationWidget extends React.PureComponent {
     const start = selection.start;
     const end = selection.end;
 
-    return preload.find((entry) => {
+    return this.state.preload.find((entry) => {
       const sel = entry.selection;
 
       return (sel.start >= start && sel.start <= end) ||
@@ -117,7 +160,7 @@ export default class AnnotationWidget extends React.PureComponent {
       this.savedSelection = null;
       const start = serializeSelection.save().start;
       this.setState({
-        activeHighlight: preload.find((entry) => {
+        activeHighlight: this.state.preload.find((entry) => {
           const sel = entry.selection;
 
           return sel.start <= start && sel.end >= start;
@@ -133,13 +176,13 @@ export default class AnnotationWidget extends React.PureComponent {
       this.setState({widgetStyle: null});
     } else {
       this.savedSelection = serializeSelection.save();
-      const rect = selection.getRangeAt(0).getBoundingClientRect();
-      const pwRect = document.querySelector('.paged-content .page-wrapper').getBoundingClientRect();
+      const rect = getSelectionRect(selection);
+      const pwRect = this.parentRect;
 
       this.setState({
         widgetStyle: {
           top: `${rect.bottom - pwRect.top}px`,
-          left: `${rect.left}px`
+          left: `${rect.left - pwRect.left}px`
         }
       });
     }
@@ -148,8 +191,8 @@ export default class AnnotationWidget extends React.PureComponent {
   restoreAnnotations() {
     if (this.needToLoadAnnotations) {
       hypothesisStore.fetch(this.props.documentId).then((response) => {
-        preload = response.rows.map(dbEntryToPreload);
-        for (const entry of preload) {
+        this.setState({preload: response.rows.map(dbEntryToPreload)});
+        for (const entry of this.state.preload) {
           highlightEntry(entry);
         }
       });
@@ -157,19 +200,10 @@ export default class AnnotationWidget extends React.PureComponent {
     }
   }
 
-  renderWidget() {
-    return (this.state.widgetStyle) ?
-      <div className="widget arrow_box" style={this.state.widgetStyle}>
-        <i className="fa fa-comment" role="button" alt="annotate" onClick={this.openAnnotator.bind(this)}></i>
-        <i className="fa fa-pencil" role="button" alt="highlight" onClick={this.highlightAndClose.bind(this)}></i>
-      </div>
-    : null;
-  }
-
   highlightAndClose() {
-    highlighter.doHighlight(true);
     return this.saveAnnotation().then((response) => {
       this.setState({widgetStyle: null});
+      highlightEntry(response);
       return response;
     });
   }
@@ -187,24 +221,25 @@ export default class AnnotationWidget extends React.PureComponent {
       activeHighlight: Object.assign(
         {},
         this.state.activeHighlight,
-        {
-          annotation: newValue
-        }
+        {annotation: newValue}
       )
     });
   }
 
   updateAnnotation() {
-    const oldEntry = preload.find((e) => e.savedId === this.state.activeHighlight.savedId);
+    const oldEntry = this.state.preload.find((e) => e.savedId === this.state.activeHighlight.savedId);
 
     this.setState({activeHighlight: null});
     window.getSelection().empty();
     if (oldEntry.annotation !== this.state.activeHighlight.annotation) {
       hypothesisStore.update(this.state.activeHighlight.savedId, this.state.activeHighlight.annotation).then((response) => {
-        const oldEntry = preload.find((e) => e.savedId === response.id);
+        const newPreload = this.state.preload.slice();
+        const oldEntry = newPreload.find((e) => e.savedId === response.id);
 
         Object.assign(oldEntry, dbEntryToPreload(response));
-        console.debug("Updated values:", oldEntry);
+        this.setState({
+          preload: newPreload
+        });
         return oldEntry;
       });
     }
@@ -216,21 +251,26 @@ export default class AnnotationWidget extends React.PureComponent {
     return hypothesisStore.save(this.props.documentId, this.savedSelection, '')
       .then((response) => {
         const newEntry = dbEntryToPreload(response);
+        const newPreload = this.state.preload.slice();
 
-        console.debug("SAVED", newEntry);
-        preload.push(newEntry);
+        newPreload.push(newEntry);
+        this.setState({
+          preload: newPreload
+        });
         return newEntry;
       });
   }
 
   deleteEntry() {
-    console.debug("Delete active", this.state.activeHighlight);
-    // Ensure it's the last thing highlighted
     unhighlightEntry(this.state.activeHighlight);
     hypothesisStore.delete(this.state.activeHighlight.savedId).then((response) => {
-      const oldIndex = preload.findIndex((e) => e.savedId === response.id);
+      const oldIndex = this.state.preload.findIndex((e) => e.savedId === response.id);
+      const newPreload = this.state.preload.slice();
 
-      preload.splice(oldIndex, 1);
+      newPreload.splice(oldIndex, 1);
+      this.setState({
+        preload: newPreload
+      });
     });
     this.setState({
       activeHighlight: null
@@ -238,17 +278,66 @@ export default class AnnotationWidget extends React.PureComponent {
     window.getSelection().empty();
   }
 
+  nextAnnotationInSortedList(entries) {
+    const nextIndex = 1 + entries.findIndex((e) => e.selection.start === this.state.activeHighlight.selection.start);
+
+    if (nextIndex < entries.length) {
+      this.setState({activeHighlight: entries[nextIndex]});
+      serializeSelection.restore(entries[nextIndex].selection);
+    }
+  }
+
+  nextAnnotation() {
+    const entries = this.state.preload.sort((a, b) => a.selection.start - b.selection.start);
+
+    this.nextAnnotationInSortedList(entries);
+  }
+
+  previousAnnotation() {
+    // The trick is that the sort is reversed
+    const entries = this.state.preload.sort((a, b) => b.selection.start - a.selection.start);
+
+    this.nextAnnotationInSortedList(entries);
+  }
+
+  getParentRect(el) {
+    if (el) {
+      const wLeft = window.pageXOffset || document.documentElement.scrollLeft;
+      const wTop = window.pageYOffset || document.documentElement.scrollTop;
+      const parentRect = el.parentNode.getBoundingClientRect();
+
+      this.parentRect = {
+        bottom: wTop + parentRect.bottom,
+        left: wLeft + parentRect.left,
+        right: wLeft + parentRect.right,
+        top: wTop + parentRect.top
+      }
+    }
+  }
+
   render() {
-    return <div className="annotater">
-      {this.renderWidget()}
-      <EditBox
-        show={this.state.activeHighlight ? 'open' : 'closed'}
-        annotation={this.state.activeHighlight ? this.state.activeHighlight.annotation : ''}
-        updateAnnotation={this.updateActiveAnnotation.bind(this)}
-        save={this.updateAnnotation.bind(this)}
-        delete={this.deleteEntry.bind(this)}
-      />
-    </div>;
+    return (
+      <div className="annotater" ref={this.getParentRect.bind(this)}>
+        <HighlightWidget
+          style={this.state.widgetStyle}
+          annotate={this.openAnnotator.bind(this)}
+          highlight={this.highlightAndClose.bind(this)}
+        />
+        <EditBox
+          show={this.state.activeHighlight ? 'open' : 'closed'}
+          annotation={this.state.activeHighlight ? this.state.activeHighlight.annotation : ''}
+          updateAnnotation={this.updateActiveAnnotation.bind(this)}
+          save={this.updateAnnotation.bind(this)}
+          delete={this.deleteEntry.bind(this)}
+          next={this.nextAnnotation.bind(this)}
+          previous={this.previousAnnotation.bind(this)}
+        />
+        <SidebarButtons items={this.state.preload}
+          offsetTop={this.parentRect ? this.parentRect.top : 0}
+          onClick={(item) => this.setState({activeHighlight: item})}
+        />
+      </div>
+    );
   }
 
 }
