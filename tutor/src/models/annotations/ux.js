b6b379a5aa539bf7a55d913f7c0013604efa60ce
Nathan Stitt <nathan@stitt.org>
2017-12-01 11:32:48 -0600

    add status message with type and autohide

--- tutor/src/models/annotations/ux.js
+++ tutor/src/models/annotations/ux.js
@@ -1,4 +1,46 @@
-import { observable, action } from 'mobx';
+import { observable, action, computed } from 'mobx';
+import { readonly } from 'core-decorators';
+
+const ERROR_DISPLAY_TIMEOUT = 1000 * 2;
+
+class StatusMessage {
+
+  @observable display = false;
+  @observable type;
+  @observable message;
+  @observable hideTimer;
+
+  @computed get icon() {
+    if (this.type === 'error') { return 'exclamation-triangle'; }
+    return 'info-circle';
+  }
+
+  @action.bound hide() {
+    this.display = false;
+    this.hideTimer = null;
+  }
+
+  @action.bound hideStatusMessage() {
+  }
+
+  show({ message, type = 'error', autoHide = false }) {
+    if (this.hideTimer) {
+      clearTimeout(this.hideTimer);
+      this.hideTimer = null;
+    }
+
+    Object.assign(this, {
+      display: true, message, type,
+    });
+
+    if (autoHide) {
+      this.hideTimer = setTimeout(
+        this.hide, ERROR_DISPLAY_TIMEOUT
+      );
+    }
+  }
+
+}
 
 export default class AnnotatorUX {
 
@@ -7,4 +49,7 @@ export default class AnnotatorUX {
   @action.bound toggleSummary() {
     this.isSummaryVisible = !this.isSummaryVisible;
   }
+
+  @readonly statusMessage = new StatusMessage();
+
 }
