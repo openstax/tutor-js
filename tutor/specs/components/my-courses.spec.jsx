070d51455d6dc34e1b53d7070f7e30c8e5428939
amanda <pandafulmanda@gmail.com>
2017-12-04 20:20:44 -0600

    make tours sticky

--- tutor/specs/components/my-courses.spec.jsx
+++ tutor/specs/components/my-courses.spec.jsx
@@ -6,9 +6,7 @@ import Courses from '../../src/models/courses-map';
 import User from '../../src/models/user';
 import Offerings from '../../src/models/course/offerings/index';
 import OFFERINGS from '../../api/offerings';
-import TourContext from '../../src/models/tour/context';
 import moment from 'moment';
-import { Provider } from 'mobx-react';
 jest.mock('../../src/models/chat');
 
 import { bootstrapCoursesList, STUDENT_COURSE_ONE_MODEL, TEACHER_COURSE_TWO_MODEL, TEACHER_AND_STUDENT_COURSE_THREE_MODEL, MASTER_COURSES_LIST, TUTOR_HELP, CONCEPT_COACH_HELP, STUDENT_ARCHIVED_COURSE, TEACHER_PAST_COURSE, STUDENT_PAST_COURSE } from '../courses-test-data';
@@ -21,12 +19,7 @@ const loadOfferings = () => {
 
 describe('My Courses Component', function() {
 
-  let context;
-
-  beforeEach(() => {
-    bootstrapCoursesList();
-    context = new TourContext({ isEnabled: true });
-  });
+  beforeEach(bootstrapCoursesList);
 
   afterEach(() => {
     Courses.clear();
@@ -44,9 +37,7 @@ describe('My Courses Component', function() {
   });
 
   it('renders the listing sorted', function() {
-    const wrapper = mount(<Provider tourContext={context}>
-      <CourseListing />
-    </Provider>, EnzymeContext.withDnD());
+    const wrapper = mount(<CourseListing />, EnzymeContext.withDnD());
     for (let i = 0; i < MASTER_COURSES_LIST.length; i++) {
       const course = MASTER_COURSES_LIST[i];
       expect(wrapper).toHaveRendered(`.my-courses [data-course-id='${course.id}']`);
@@ -56,18 +47,14 @@ describe('My Courses Component', function() {
 
   it('renders the listing without archived courses', function() {
     Courses.bootstrap(flatten([MASTER_COURSES_LIST, STUDENT_ARCHIVED_COURSE]), { clear: true });
-    const wrapper = mount(<Provider tourContext={context}>
-      <CourseListing />
-    </Provider>, EnzymeContext.withDnD());
+    const wrapper = shallow(<CourseListing />, EnzymeContext.withDnD());
     expect(wrapper).not.toHaveRendered(`CourseLink[courseId='${STUDENT_ARCHIVED_COURSE.id}']`);
     wrapper.unmount();
   });
 
   it('renders add course action if user is teacher', function() {
     loadTeacherUser();
-    const wrapper = mount(<Provider tourContext={context}>
-      <CourseListing />
-    </Provider>, EnzymeContext.withDnD());
+    const wrapper = mount(<CourseListing />, EnzymeContext.withDnD());
     expect(User.isConfirmedFaculty).toBeTruthy();
     expect(wrapper).toHaveRendered('.my-courses-add-zone');
     wrapper.unmount();
@@ -75,9 +62,7 @@ describe('My Courses Component', function() {
 
   it('renders controls for course if user is teacher of course', function() {
     loadTeacherUser();
-    const wrapper = mount(<Provider tourContext={context}>
-      <CourseListing />
-    </Provider>, EnzymeContext.withDnD());
+    const wrapper = mount(<CourseListing />, EnzymeContext.withDnD());
     for (let i = 0; i < MASTER_COURSES_LIST.length; i++) {
       const course = MASTER_COURSES_LIST[i];
       if (Courses.get(course.id).isTeacher) {
@@ -89,9 +74,7 @@ describe('My Courses Component', function() {
 
   it('does not render controls for course if user is student of course', function() {
     loadTeacherUser();
-    const wrapper = mount(<Provider tourContext={context}>
-      <CourseListing />
-    </Provider>, EnzymeContext.withDnD());
+    const wrapper = mount(<CourseListing />, EnzymeContext.withDnD());
     for (let i = 0; i < MASTER_COURSES_LIST.length; i++) {
       const course = MASTER_COURSES_LIST[i];
       if (!Courses.get(course.id).isTeacher) {
@@ -104,25 +87,19 @@ describe('My Courses Component', function() {
   it('renders past courses in past courses listing', function() {
     loadTeacherUser();
     Courses.bootstrap([TEACHER_PAST_COURSE, STUDENT_PAST_COURSE], { clear: true });
-    const wrapper = mount(<Provider tourContext={context}>
-      <CourseListing />
-    </Provider>, EnzymeContext.withDnD());
+    const wrapper = mount(<CourseListing />, EnzymeContext.withDnD());
     expect(wrapper).toHaveRendered(`.my-courses-past [data-course-id='${TEACHER_PAST_COURSE.id}']`);
     expect(wrapper).toHaveRendered(`.my-courses-past [data-course-id='${STUDENT_PAST_COURSE.id}']`);
   });
 
   it('renders empty courses if course list is empty', function() {
     Courses.clear();
-    const wrapper = mount(<Provider tourContext={context}>
-      <CourseListing />
-    </Provider>, EnzymeContext.withDnD());
+    const wrapper = shallow(<CourseListing />, EnzymeContext.withDnD());
     expect(wrapper).toHaveRendered('EmptyCourses');
   });
 
   it('renders course appropriate flag', function() {
-    const wrapper = mount(<Provider tourContext={context}>
-      <CourseListing />
-    </Provider>, EnzymeContext.withDnD());
+    const wrapper = mount(<CourseListing />, EnzymeContext.withDnD());
     for (let i = 0; i < MASTER_COURSES_LIST.length; i++) {
       const course = MASTER_COURSES_LIST[i];
       expect(
@@ -137,19 +114,14 @@ describe('My Courses Component', function() {
     const c = Courses.get(STUDENT_COURSE_ONE_MODEL.id);
     c.ends_at = moment().add(1, 'week');
     c.starts_at = moment().subtract(1, 'week');
-    const wrapper = mount(<Provider tourContext={context}>
-      <CourseListing />
-    </Provider>, EnzymeContext.withDnD());
-    expect(wrapper).toHaveRendered('Redirect');
-    expect(wrapper.find('Redirect').props().to).equal('/course/1');
+    const wrapper = shallow(<CourseListing />, EnzymeContext.withDnD());
+    expect(wrapper).toHaveRendered('Redirect[to="/course/1"]');
   });
 
   it('displays pending screen', () => {
     Courses.clear();
     User.self_reported_role = 'instructor';
-    const wrapper = mount(<Provider tourContext={context}>
-      <CourseListing />
-    </Provider>, EnzymeContext.withDnD());
+    const wrapper = shallow(<CourseListing />, EnzymeContext.withDnD());
     expect(wrapper).toHaveRendered('PendingVerification');
     wrapper.unmount();
   });
@@ -158,9 +130,7 @@ describe('My Courses Component', function() {
     Courses.clear();
     loadTeacherUser();
     loadOfferings();
-    const wrapper = mount(<Provider tourContext={context}>
-      <CourseListing />
-    </Provider>, EnzymeContext.withDnD());
+    const wrapper = mount(<CourseListing />, EnzymeContext.withDnD());
     expect(wrapper).toHaveRendered('[data-tour-anchor-id="explore-a-preview-zone"]');
     expect(wrapper).toHaveRendered('[data-tour-anchor-id="create-course-zone"]');
     wrapper.unmount();
--- tutor/src/components/my-courses/index.jsx
+++ tutor/src/components/my-courses/index.jsx
@@ -1,54 +1,21 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { computed, action } from 'mobx';
-import { observer, inject } from 'mobx-react';
+import { computed } from 'mobx';
+import { observer } from 'mobx-react';
 import User from '../../models/user';
 import Router from '../../helpers/router';
 import Courses from '../../models/courses-map';
 import EmptyCourses from './empty';
 import TourRegion from '../tours/region';
 import PendingVerification from './pending-verification';
-import TourContext from '../../models/tour/context';
-import { includes } from 'lodash';
 
 import { MyCoursesPast, MyCoursesCurrent, MyCoursesPreview } from './listings';
 
-@inject((allStores, props) => ({
-  tourContext: ( props.tourContext || allStores.tourContext ),
-}))
 @observer
 export default class MyCourses extends React.PureComponent {
 
-  static propTypes = {
-    tourContext: React.PropTypes.instanceOf(TourContext),
-  }
-
-  @computed get stubbornTour() {
-    return this.props.tourContext.eligibleTours.find((tour) => (
-        includes([
-          'explore-a-preview',
-          'create-a-course',
-        ], tour.id)
-      ));
-  }
-
   componentWillMount() {
     User.logEvent({ category: 'onboarding', code: 'arrived_my_courses' });
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.forceTours();
-  }
-
-  @action.bound
-  forceTours() {
-    if (!this.props.tourContext.tour) {
-      const { stubbornTour } = this;
-
-      if (stubbornTour) {
-        stubbornTour.isEnabled = true;
-      }
-    }
   }
 
   @computed get firstCourse() {
