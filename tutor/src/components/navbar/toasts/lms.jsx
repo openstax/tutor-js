69856957ad87da9372d0959185b467460e28db25
Nathan Stitt <nathan@stitt.org>
2017-12-11 15:47:12 -0600

    update success/failure wording and style toasts

--- tutor/src/components/navbar/toasts/lms.jsx
+++ tutor/src/components/navbar/toasts/lms.jsx
@@ -197,11 +197,13 @@ export class Failure extends React.Component {
 
     return (
       <div className="toast scores failure">
-        <div className="heading">
+        <div className="title">
           {num_callbacks ? 'Some scores not sent' : 'Scores not sent'}
           <Icon type="close" onClick={this.props.dismiss} />
         </div>
-        <Button bsStyle="link" onClick={this.onShowDetails}>Details</Button>
+        <div className="body">
+          <Button bsStyle="link" onClick={this.onShowDetails}>Details</Button>
+        </div>
       </div>
     );
   }
@@ -210,7 +212,8 @@ export class Failure extends React.Component {
 export function Success() {
   return (
     <div className="toast success">
-      Scores successfully sent
+      <div className="title">Scores sent successfully</div>
+      <div className="body">Check your LMS gradebook for the updates</div>
     </div>
   );
 }
