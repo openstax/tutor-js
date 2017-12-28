6d66bc0f8ea70b236d4ae755248735f1deb6cd6e
Nathan Stitt <nathan@stitt.org>
2017-12-28 15:35:47 -0600

    add class to read window size and update only when it's observed

---
+++ tutor/src/models/window-size.js
@@ -0,0 +1,40 @@
+import { computed } from 'mobx';
+import { fromResource } from 'mobx-utils';
+import { debounce } from 'lodash';
+
+export default class WindowSize {
+
+  constructor(windowImpl = window) {
+    this.size = fromResource(
+      (sink) => {
+        sink(this.readSize(windowImpl));
+        this.updater = debounce(() => sink(this.readSize(windowImpl)), 200);
+        windowImpl.addEventListener('resize', this.updater);
+      },
+      () => {
+        windowImpl.removeEventListener('resize', this.updater);
+        this.updater = null;
+      }
+    );
+  }
+
+  @computed get current() {
+    return this.size.current();
+  }
+
+  @computed get width() {
+    return this.current.width;
+  }
+
+  @computed get height() {
+    return this.current.height;
+  }
+
+  readSize(windowImpl = window) {
+    return {
+      width: windowImpl.innerWidth,
+      height: windowImpl.innerHeight,
+    };
+  }
+
+}
