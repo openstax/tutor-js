b3eb3c136246d2f2db4c1ca581e1b526610a2ee7
Nathan Stitt <nathan@stitt.org>
2018-01-19 15:34:34 -0600

    add helper for getting inner modal instance

---
+++ tutor/specs/helpers/modal-wrapper.js
@@ -0,0 +1,13 @@
+import { ReactWrapper } from 'enzyme';
+import { Modal } from 'react-bootstrap';
+
+export function getModalInstance(component) {
+  const modal = component.find(Modal);
+  return modal.node._modal.dialog;
+}
+
+export function wrapModalContents(component) {
+  return new ReactWrapper(
+    getModalInstance(component), true
+  );
+}
