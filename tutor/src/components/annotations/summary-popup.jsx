d4b74f9dedd0459d3b9f642632a84704d2d30b03
m1yag1 <mike.arbelaez@rice.edu>
2018-01-29 10:58:10 -0600

    adding the summary-popup.jsx file to vcs

---
+++ tutor/src/components/annotations/summary-popup.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import {observer} from 'mobx-react';
+import {action, observable, computed} from 'mobx';
+import {Button} from 'react-bootstrap';
+import Icon from '../icon';
+import PopoutWindow from 'shared/src/components/popout-window';
+import {map} from "lodash";
+
+@observer
+export default class SummaryPopup extends React.Component {
+
+  static propTypes = {
+    windowImpl: React.PropTypes.shape({
+      open: React.PropTypes.func,
+    }),
+    annotations: React.PropTypes.object.isRequired
+  }
+
+  @observable isOpen = false;
+
+  static defaultProps = {
+    windowImpl: window,
+  }
+
+  @action.bound onSummaryWindowClose() {
+    this.isOpen = false;
+  }
+
+  @action.bound openSummaryWindow() {
+    this.isOpen = true;
+    this.popup.open();
+    console.log(this.popup)
+  }
+
+  render() {
+    return (
+      <Button
+        className="print-btn pull-right"
+        onClick={this.openSummaryWindow}
+      >
+        <PopoutWindow
+          title="Annotation Summary Print Page"
+          ref={pw => (this.popup = pw)}
+          windowImpl={this.props.windowImpl}
+          onClose={this.onSummaryWindowClose}
+          options={{
+            height: 500,
+            width: 700,
+          }}
+        >
+          <div className="summary-preview summary-popup">
+            <div className="annotations">
+              {map(this.props.annotations, (notes, ch) =>
+                <div key={ch}>
+                  <h2>{notes[0].formattedChapterSection} {notes[0].title}</h2>
+                  {map(notes, (annotation) => (
+                    <div key={annotation.id}>
+                    <p style={{fontStyle: 'italic'}}>
+                      {annotation.selection.content}
+                    </p>
+                    <p>
+                    {annotation.text}
+                    </p>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </PopoutWindow>
+        <Icon type="print"/>
+        Print this page
+      </Button>
+    );
+  }
+}
