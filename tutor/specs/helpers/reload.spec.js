c21420b1069ef7cd2bc25184d2e70de68dec12d5
Nathan Stitt <nathan@stitt.org>
2018-02-14 15:48:54 -0600

    use pushState to change url to have "reloaded" then forceReload

--- tutor/specs/helpers/reload.spec.js
+++ tutor/specs/helpers/reload.spec.js
@@ -12,6 +12,14 @@ describe('Reloading window', () => {
         writable: true,
         value: jest.fn(),
       },
+      href: {
+        writable: true,
+        value: 'https://tutor.org/dashboard',
+      },
+    });
+    Object.defineProperty(window.history, 'pushState', {
+      writable: true,
+      value: jest.fn(),
     });
   });
 
@@ -25,14 +33,22 @@ describe('Reloading window', () => {
   it('reloads page once', () => {
     expect(isReloaded()).toEqual(false);
     reloadOnce();
-    expect(isReloaded()).toEqual(true);
+    expect(window.history.pushState).toHaveBeenCalledWith('', '', `${window.location.href}?reloaded`);
+    expect(window.location.reload).toHaveBeenCalledWith(true); // true == no-cache
+  });
+
+  it('retains existing query params', () => {
+    window.location.href = 'http://openstax.org/test?test=true';
+    reloadOnce();
+    expect(window.history.pushState).toHaveBeenCalledWith('', '', 'http://openstax.org/test?test=true&reloaded');
+    expect(window.location.reload).toHaveBeenCalledWith(true);
   });
 
   it('can force a page reload', () => {
     expect(isReloaded()).toEqual(false);
     forceReload();
     expect(isReloaded()).toEqual(false); // force reload doesn't change the search, it just reloads
-    expect(window.location.reload).toHaveBeenCalledWith(true); // true == no-cache
+    expect(window.location.reload).toHaveBeenCalledWith(true);
   });
 
 });
