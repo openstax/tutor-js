7d38b49613f9c7088e75f491d8b39fbdd3861f25
Philip Schatz <phil@cnx.org>
2018-03-07 20:30:27 -0600

    add failing test

--- tutor/specs/components/plan-stats/index.spec.jsx
+++ tutor/specs/components/plan-stats/index.spec.jsx
@@ -28,11 +28,12 @@ describe('TaskPlan stats progress bar', function() {
     expect(tree).toMatchSnapshot();
   });
 
-  it('deals with missing stats for a period', () => {
+  it('deals with missing stats for a period', async () => {
     course.periods.push({ id: 99, name: 'testing', num_enrolled_students: 42 });
-    const stats = mount(<Stats {...props} />, EnzymeContext.build());
-    stats.find('.nav-tabs li a').last().simulate('click');
-    expect(stats.find('.text-not-started').text()).toEqual('0'); // not NaN
+    const wrapper = mount(<Stats {...props} />, EnzymeContext.build());
+    expect(await axe(wrapper.html())).toHaveNoViolations();
+    wrapper.find('.nav-tabs li a').last().simulate('click');
+    expect(wrapper.find('.text-not-started').text()).toEqual('0'); // not NaN
 
     const component = SnapShot.create(<Stats {...props} />);
     const tree = component.toJSON();
