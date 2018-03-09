c160b4ec525c252fd72b39fe0cd7614ebea2a622
Nathan Stitt <nathan@stitt.org>
2018-03-09 09:49:50 -0600

    change window param so it's a property of object
    This way the bare method can be passed to a onClick callback and doesn't get confused by the event property

--- tutor/specs/helpers/reload.spec.js
+++ tutor/specs/helpers/reload.spec.js
@@ -2,35 +2,35 @@ import { isReloaded, reloadOnce, forceReload } from '../../src/helpers/reload';
 import FakeWindow from 'shared/specs/helpers/fake-window';
 
 describe('Reloading window', () => {
-  let win;
-  beforeEach(() => win = new FakeWindow());
+  let windowImpl;
+  beforeEach(() => windowImpl = new FakeWindow());
 
   it('detects if page is reloaded', () => {
-    expect(isReloaded(win)).toEqual(false);
-    win.location.search = '?foo&reloaded';
-    expect(isReloaded(win)).toEqual(true);
+    expect(isReloaded({ windowImpl })).toEqual(false);
+    windowImpl.location.search = '?foo&reloaded';
+    expect(isReloaded({ windowImpl} )).toEqual(true);
   });
 
   it('reloads page once', () => {
-    expect(win.location.reload).not.toHaveBeenCalled();
-    expect(isReloaded(win)).toEqual(false);
-    reloadOnce(win);
-    expect(win.history.pushState).toHaveBeenCalledWith('', '', `${win.location.href}?reloaded`);
-    expect(win.location.reload).toHaveBeenCalledWith(true); // true == no-cache
+    expect(windowImpl.location.reload).not.toHaveBeenCalled();
+    expect(isReloaded({ windowImpl} )).toEqual(false);
+    reloadOnce({ windowImpl });
+    expect(windowImpl.history.pushState).toHaveBeenCalledWith('', '', `${windowImpl.location.href}?reloaded`);
+    expect(windowImpl.location.reload).toHaveBeenCalledWith(true); // true == no-cache
   });
 
   it('retains existing query params', () => {
-    win.location.href = 'http://openstax.org/test?test=true';
-    reloadOnce(win);
-    expect(win.history.pushState).toHaveBeenCalledWith('', '', 'http://openstax.org/test?test=true&reloaded');
-    expect(win.location.reload).toHaveBeenCalledWith(true);
+    windowImpl.location.href = 'http://openstax.org/test?test=true';
+    reloadOnce({ windowImpl });
+    expect(windowImpl.history.pushState).toHaveBeenCalledWith('', '', 'http://openstax.org/test?test=true&reloaded');
+    expect(windowImpl.location.reload).toHaveBeenCalledWith(true);
   });
 
   it('can force a page reload', () => {
-    expect(isReloaded(win)).toEqual(false);
-    forceReload(win);
-    expect(isReloaded(win)).toEqual(false); // force reload doesn't change the search, it just reloads
-    expect(win.location.reload).toHaveBeenCalledWith(true);
+    expect(isReloaded({ windowImpl })).toEqual(false);
+    forceReload({ windowImpl });
+    expect(isReloaded({ windowImpl })).toEqual(false); // force reload doesn't change the search, it just reloads
+    expect(windowImpl.location.reload).toHaveBeenCalledWith(true);
   });
 
 });
