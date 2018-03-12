69ba63e3a18710e6ab75d874270fe7274d25958a
Nathan Stitt <nathan@stitt.org>
2018-03-12 09:34:15 -0500

    take snapshots of student dashboard

---
+++ tutor/specs/acceptance/dashboard.spec.js
@@ -0,0 +1,27 @@
+import { openPage, snapshotPage, setRole } from './helpers';
+
+describe('Course Dashboard', () => {
+  let page;
+
+  let role = 'student';
+
+  beforeEach(() => openPage('/dashboard', { role }).then((pg) => page = pg));
+
+  afterEach(() => page.close());
+
+  it(`matches welcome snapshot for ${role}`, async () => {
+    page.click('.my-courses-item-wrapper a');
+    await page.waitForSelector('.progress-guide');
+    let tip = await page.$('.joyride-tooltip__button--primary');
+    let i = 1;
+    while(tip) {
+      await snapshotPage(page, `dashboard-tip-${i}`);
+      i += 1;
+      await page.click('.joyride-tooltip__button--primary');
+      tip = await page.$('.joyride-tooltip__button--primary');
+    }
+    const snapshot = await snapshotPage(page, 'dashboard');
+    expect(snapshot).toMatchPreviousPage();
+  });
+
+});
