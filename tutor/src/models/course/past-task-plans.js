565a5e286fea2db30c793759d1bb12a785d4b2fa
Nathan Stitt <nathan@stitt.org>
2018-04-11 13:04:48 -0500

    replace past task plans with mobx store

---
+++ tutor/src/models/course/past-task-plans.js
@@ -0,0 +1,27 @@
+import { computed, observable, action } from 'mobx';
+import { find } from 'lodash';
+import Map from 'shared/model/map';
+import TaskPlan from '../task-plan/teacher';
+
+export default class CoursePastTaskPlans extends Map {
+
+  @observable course;
+
+  constructor(attrs) {
+    super();
+    this.course = attrs.course;
+  }
+
+  // called from api
+  fetch() {
+    if (!this.course.isCloned) { return 'ABORT'; }
+    return { id: this.course.cloned_from_id };
+  }
+  onLoaded({ data: { plans } }) {
+    plans.forEach(plan => {
+      const tp = this.get(plan.id);
+      tp ? tp.update(plan) : this.set(plan.id, new TaskPlan(plan));
+    });
+  }
+
+}
