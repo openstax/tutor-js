bf4e238b09ada4c4cabb647b0cb1c4af94891bff
Nathan Stitt <nathan@stitt.org>
2018-04-16 14:46:36 -0500

    remove axe tests from cc dashboard since it's deprecated
    It's planned to remove it soon

--- tutor/specs/components/cc-dashboard/dashboard.spec.jsx
+++ tutor/specs/components/cc-dashboard/dashboard.spec.jsx
@@ -38,7 +38,7 @@ describe('Concept Coach Dashboard', function() {
   });
 
   describe('Dashboard', function() {
-    it('shows the help page for blank periods', async function() {
+    it('shows the help page for blank periods', function() {
       const wrapper = shallow(<Dashboard {...props} />, Context.build());
       expect(wrapper).toHaveRendered(`CCDashboardEmptyPeriod[courseId=\"${props.courseId}\"]`);
     });
@@ -56,13 +56,12 @@ describe('Concept Coach Dashboard', function() {
   });
 
   describe('Chapter', () =>
-    it('shows the right amount of sections for chapters', async function() {
+    it('shows the right amount of sections for chapters', function() {
       const chapters = CCDashboardStore.chaptersForDisplay(COURSE_ID,
         BaseModel.course.periods[ACTIVE_PERIOD].id
       );
       for(const chapter of chapters) {
         const wrapper = shallow(<Chapter {...props} chapter={chapter} />);
-        expect(await axe(wrapper.html())).toHaveNoViolations()
         Array.from(chapter.valid_sections).map((section) =>
           expect(wrapper).toHaveRendered(`Section[id=\"${section.id}\"]`));
       }
@@ -80,16 +79,14 @@ describe('Concept Coach Dashboard', function() {
       };
     });
 
-    it('shows a section without spaced practice', async function() {
+    it('shows a section without spaced practice', function() {
       const wrapper = shallow(<Section {...props} />);
-      expect(await axe(wrapper.html())).toHaveNoViolations()
       expect(wrapper).toHaveRendered('.empty-spaced-practice');
     });
 
-    it('shows a section with spaced practice', async function() {
+    it('shows a section with spaced practice', function() {
       props.section.spaced_practice_performance = 0.5;
       const wrapper = shallow(<Section {...props} />);
-      expect(await axe(wrapper.html())).toHaveNoViolations()
       expect(wrapper).not.toHaveRendered('.empty-spaced-practice');
     });
   });
@@ -104,38 +101,33 @@ describe('Concept Coach Dashboard', function() {
       };
     });
 
-    it('displays as 100%', async function() {
+    it('displays as 100%', function() {
       const wrapper = shallow(<SectionProgress {...props} />);
-      expect(await axe(wrapper.html())).toHaveNoViolations()
       expect(wrapper).toHaveRendered('ProgressBar[now=100][label="100% completed"]');
     });
 
-    it('hides complete progress bar when 0% complete', async function() {
+    it('hides complete progress bar when 0% complete', function() {
       props.section.completed_percentage = 0.0;
       const wrapper = shallow(<SectionProgress {...props} />);
-      expect(await axe(wrapper.html())).toHaveNoViolations()
       expect(wrapper).toHaveRendered('ProgressBar.none-completed');
     });
   });
 
   describe('Section Performance Bars', function() {
-    it('hides incorrect progress bar when all correct', async function() {
+    it('hides incorrect progress bar when all correct', function() {
       const wrapper = shallow(<SectionPerformance performance={1.0} />);
-      expect(await axe(wrapper.html())).toHaveNoViolations()
       expect(wrapper).toHaveRendered('ProgressBar[now=100][label="100% correct"]');
       expect(wrapper).not.toHaveRendered('ProgressBar.progress-bar-incorrect');
     });
 
-    it('hides correct progress bar when all incorrect', async function() {
+    it('hides correct progress bar when all incorrect', function() {
       const wrapper = shallow(<SectionPerformance performance={0} />);
-      expect(await axe(wrapper.html())).toHaveNoViolations()
       expect(wrapper).not.toHaveRendered('ProgressBar.progress-bar-correct');
       expect(wrapper).toHaveRendered('ProgressBar[now=100][label="100% incorrect"]');
     });
 
-    it('renders both when a mix of of correct/incorrect', async function() {
+    it('renders both when a mix of of correct/incorrect', function() {
       const wrapper = shallow(<SectionPerformance performance={0.3} />);
-      expect(await axe(wrapper.html())).toHaveNoViolations()
       expect(wrapper).toHaveRendered('ProgressBar.progress-bar-correct[now=30][label="30%"]');
       expect(wrapper).toHaveRendered('ProgressBar.progress-bar-incorrect[now=70][label=""]');
     });
