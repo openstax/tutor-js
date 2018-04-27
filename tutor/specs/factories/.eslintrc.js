d6971b776269a8530be3a11652d86e74b5799b93
Nathan Stitt <nathan@stitt.org>
2018-04-27 14:35:48 -0500

    allow ommitting semi colons for fixtures

--- tutor/specs/factories/.eslintrc.js
+++ tutor/specs/factories/.eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  "globals": {
-    "require": false,
+  globals: {
+    require: false,
+  },
+  rules: {
+    semi: "off", // it's nice to be able to not have to put semi after long factory def, which is safe because the factories won't be minimized
   }
 }
