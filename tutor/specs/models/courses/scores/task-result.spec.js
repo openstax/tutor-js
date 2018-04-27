1d621b3dfedc8163968eff1bf8db7759345ba91b
Nathan Stitt <nathan@stitt.org>
2018-04-27 16:38:01 -0500

    test accepting while values are n/a

--- tutor/specs/models/courses/scores/task-result.spec.js
+++ tutor/specs/models/courses/scores/task-result.spec.js
@@ -1,3 +1,4 @@
+import { filter } from 'lodash';
 import Factory from '../../../factories';
 
 describe('scores store task results', () => {
@@ -8,11 +9,89 @@ describe('scores store task results', () => {
     course = Factory.course({ num_periods: 1 });
     Factory.scores({ course });
     scores = course.scores;
+
     course.update({
       homework_score_weight: 50.0,
       homework_progress_weight: 10.0,
       reading_score_weight: 20.0,
       reading_progress_weight: 20.0,
+    });
+  });
+
+
+  describe('with n/a values', () => {
+    let task;
+    let tests;
+    let period;
+
+    beforeEach(() => {
+      period = scores.periods.values()[0];
+      filter(period.data_headings, { type: 'reading' }).forEach(reading =>
+        period.data_headings.remove(reading),
+      );
+      period.overall_course_average = undefined;
+      period.overall_reading_score = undefined;
+      period.overall_reading_progress = undefined;
+
+      period.students.forEach((s) => {
+        filter(s.data, { type: 'reading' }).forEach(reading =>
+          s.data.remove(reading),
+        );
+      });
+      task = scores.getTask(1882); // a homework
+      tests = [
+        {
+          object: task, property: 'completed_accepted_late_exercise_count',
+          from: 0, to: 3,
+        }, {
+          object: task, property: 'student.homework_progress',
+          precision: 5, from: 0.142857, to: 0.25,
+        }, {
+          object: task, property: 'student.homework_score',
+          precision: 5, from: 0.107142, to: 0.142857,
+        }, {
+          object: task, property: 'student.course_average',
+          precision: 5, from: 0.067857142, to: 0.0964285,
+        }, {
+          object: task, property: 'period.overall_homework_progress',
+          precision: 5, from: 0.0714285714285714, to: 0.125,
+        }, {
+          object: task, property: 'period.overall_homework_score',
+          precision: 5, from: 0.0535714, to: 0.07142857142,
+        }, {
+          object: task, property: 'reportHeading.average_score',
+          precision: 5, from: 0.107142857, to: 0.142857142,
+        }, {
+          object: task, property: 'reportHeading.average_progress',
+          precision: 5, from: 0.142857142, to: 0.25,
+        },
+      ];
+    });
+
+    it ('has no readings', () => {
+      expect(scores.getTask(1883)).toBeNull();
+    });
+
+    it('matches expected values', () => {
+      expect(
+        () => task.onLateWorkAccepted()
+      ).toHaveChanged(tests);
+      // it shouldn't update reading or course avg
+      expect(period.overall_course_average).toBeUndefined();
+      expect(period.overall_reading_score).toBeUndefined();
+      expect(period.overall_reading_progress).toBeUndefined();
+    });
+
+    it('onLateWorkRejected matches expected values', () => {
+      task.onLateWorkAccepted();
+      tests.forEach((t) => {
+        const { from, to } = t;
+        t.from = to;
+        t.to = from;
+      });
+      expect(
+        () => task.onLateWorkRejected()
+      ).toHaveChanged(tests);
     });
   });
 
