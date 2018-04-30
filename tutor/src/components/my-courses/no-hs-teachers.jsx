d518debdd59409acad01020b8238af162696f299
Nathan Stitt <nathan@stitt.org>
2018-04-30 10:39:18 -0500

    update copy for no-hs teachers message

--- tutor/src/components/my-courses/no-hs-teachers.jsx
+++ tutor/src/components/my-courses/no-hs-teachers.jsx
@@ -6,13 +6,13 @@ export default function NoHSTeachers() {
   return (
     <Panel className="no-hs-teachers">
       <p>
-        Thanks for your interest in OpenStax Tutor Beta!  Unfortunately, we're not able to offer OpenStax Tutor to high school instructors and students at this time. We're working on this and we hope to make it available for you in the future.
+        Thanks for your interest in OpenStax Tutor Beta! At this time, OpenStax Tutor Beta is only available to instructors and students at 2- and 4-year institutions in the United States. Unfortunately, we’re not able to offer OpenStax Tutor Beta to high school, home school, corporate or international users at this time.
       </p>
       <p>
         In the meantime, we have over 50 online homework and ancillary partners that have created low-cost tools that integrate with our books. You can find those resources at <a href="https://openstax.org/partners">openstax.org/partners</a>.
       </p>
       <p>
-        If you believe you’re seeing this message in error, please <SupportLink label="contact Support" />.
+        If you think you’ve reached this page in error, please contact <SupportLink displayEmail />.
       </p>
     </Panel>
   );
