feba8732f10fb2e1a02c29d3fa6adf00c926294f
Nathan Stitt <nathan@stitt.org>
2018-06-21 14:31:29 -0500

    test title

--- tutor/specs/screens/new-course/bio2e.spec.jsx
+++ tutor/specs/screens/new-course/bio2e.spec.jsx
@@ -9,6 +9,9 @@ describe('Bio2e unavailable', () => {
   });
 
   it('matches snapshot', function() {
+    const title = SnapShot.create(<Wrapper _wrapped_component={Bio2eUnavail.title} {...props} />);
+    expect(title.toJSON()).toMatchSnapshot();
+
     const component = SnapShot.create(<Wrapper _wrapped_component={Bio2eUnavail} {...props} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
