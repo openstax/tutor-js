9819ab12aeffb747f32f73fb45051deb74c9e71a
Nathan Stitt <nathan@stitt.org>
2018-07-05 13:13:25 -0500

    formats are per-question and not set on the exercise

--- shared/src/model/exercise.js
+++ shared/src/model/exercise.js
@@ -23,7 +23,7 @@ export default class Exercise extends BaseModel {
   @field version;
   @field nickname;
   @field({ type: 'array' }) versions;
-  @field({ type: 'array' }) formats;
+
   @field is_vocab;
   @field number;
   @field stimulus_html;
@@ -44,10 +44,6 @@ export default class Exercise extends BaseModel {
       },
     },
   }) tags;
-
-  @computed get hasFreeResponse() {
-    return this.formats.includes('free-response');
-  }
 
   @computed get pool_types() {
     return [];
