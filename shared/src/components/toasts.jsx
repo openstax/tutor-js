ba46e1f20689c7776d2516102ccacf2836e9084b
Nathan Stitt <nathan@stitt.org>
2018-09-12 18:16:20 -0500

    move toasts framework to shared components
    This way exercises can also use it

---
+++ shared/src/components/toasts.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { observer, PropTypes as mobxPropTypes } from 'mobx-react';
+import { observable, autorun, action } from 'mobx';
+import { isEmpty } from 'lodash';
+import { Store } from '../model/toasts';
+
+const REMOVE_AFTER = 1000 * 7;
+
+@observer
+export default class Toasts extends React.Component {
+
+  static propTypes = {
+    toasts: mobxPropTypes.observableArray,
+  }
+
+  static defaultProps = {
+    toasts: Store,
+  }
+
+  queuePopperStop = autorun(() => {
+    if (!this.currentToast && !isEmpty(this.props.toasts)) {
+      this.currentToast = this.props.toasts.shift();
+      if (this.currentToast.isOk) {
+        this.pendingRemoval = setTimeout(this.removeToast, REMOVE_AFTER);
+      }
+    }
+  })
+
+  @observable currentToast;
+  @observable pendingRemoval;
+
+  @action.bound removeToast() {
+    this.currentToast = null;
+  }
+
+  componentWillUnmount() {
+    this.queuePopperStop();
+    if (this.pendingRemoval) {
+      clearTimeout(this.pendingRemoval);
+    }
+  }
+
+  render() {
+    if (!this.currentToast) { return null; }
+    const Toast = this.currentToast.component;
+
+    return (
+      <div className="toast-notification">
+        <Toast toast={this.currentToast} dismiss={this.removeToast} />
+      </div>
+    );
+  }
+
+}
--- tutor/src/models/toasts.js
+++ tutor/src/models/toasts.js
@@ -1,11 +1,5 @@
-import { readonly } from 'core-decorators';
-import { createCollection } from 'mobx-decorated-models';
-import { uniqueId } from 'lodash';
 import invariant from 'invariant';
-import {  computed } from 'mobx';
-import {
-  BaseModel, identifiedBy, session,
-} from 'shared/model';
+import { Store, setHandlers } from 'shared/model/toasts';
 
 import * as lms from '../components/toasts/lms';
 import * as scores from '../components/toasts/scores';
@@ -13,7 +7,7 @@ import Reload from '../components/toasts/reload';
 
 const JobToasts = { lms, scores };
 
-const Handlers  = {
+setHandlers({
   job(toast) {
     invariant(['ok', 'failed'].includes(toast.status), 'job status must be ok or failed');
     return JobToasts[toast.type][toast.status == 'ok' ? 'Success' : 'Failure'];
@@ -21,29 +15,8 @@ const Handlers  = {
   reload() {
     return Reload;
   },
-};
+});
 
-@identifiedBy('toast')
-export class ToastModel extends BaseModel {
+export { Toast } from 'shared/model/toasts';
 
-  @readonly id = uniqueId('toast-');
-  @session handler;
-  @session status;
-  @session type;
-  @session({ type: 'object' }) info;
-
-  @computed get isOk() {
-    return 'ok' === this.status;
-  }
-
-  @computed get component() {
-    const handler = Handlers[this.handler];
-    invariant(handler, `Handler type for '${this.handler}' was not found`);
-    return handler(this);
-  }
-}
-
-
-const ToastsStore = createCollection({ model: ToastModel });
-
-export default ToastsStore;
+export default Store;
