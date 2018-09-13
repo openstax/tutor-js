0121eedaf1543d189526396b3bc748b289479243
Nathan Stitt <nathan@stitt.org>
2018-09-13 12:25:51 -0500

    appease coverage by add spec for publish

--- exercises/specs/components/exercise/controls.spec.js
+++ exercises/specs/components/exercise/controls.spec.js
@@ -3,6 +3,11 @@ import { MemoryRouter } from 'react-router-dom';
 import Factory from '../../factories';
 import ExerciseControls from '../../../src/components/exercise/controls';
 import EnzymeContext from '../../../../tutor/specs/components/helpers/enzyme-context';
+import ToastsStore from '../../../src/models/toasts';
+
+jest.mock('../../../src/models/toasts', () => ({
+  push: jest.fn(),
+}));
 
 describe('Exercise controls component', function() {
   let exercise;
@@ -37,6 +42,44 @@ describe('Exercise controls component', function() {
     expect(controls.find('button.draft').props().disabled).toBe(false);
     expect(controls.find('button.publish').props().disabled).toBe(true);
     controls.unmount();
+  });
+
+  it('adds a toast when saved', () => {
+    const controls = mount(<ExerciseControls {...props} />, EnzymeContext.build());
+    exercise.published_at = new Date();
+    props.exercises.saveDraft = jest.fn(() => Promise.resolve());
+    controls.find('button.draft').simulate('click');
+    expect(props.exercises.saveDraft).toHaveBeenCalled();
+    controls.unmount();
+    return new Promise((done) => {
+      setTimeout(() => {
+        expect(ToastsStore.push).toHaveBeenCalledWith({
+          handler: 'published', status: 'ok',
+          info: { isDraft: true, exercise },
+        });
+        done();
+      });
+    });
+  });
+
+  it('adds a toast when published', () => {
+    const controls = mount(<ExerciseControls {...props} />, EnzymeContext.build());
+    exercise.published_at = new Date();
+    props.exercises.publish = jest.fn(() => Promise.resolve());
+    controls.find('button.publish').simulate('click');
+    controls.instance().publishExercise();
+    expect(props.exercises.publish).toHaveBeenCalled();
+    controls.unmount();
+    return new Promise((done) => {
+      setTimeout(() => {
+        expect(ToastsStore.push).toHaveBeenCalledWith({
+          handler: 'published', status: 'ok',
+          info: { exercise },
+        });
+        expect(props.history.push).toHaveBeenCalledWith('/search');
+        done();
+      });
+    });
   });
 
 });
