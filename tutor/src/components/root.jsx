5bab0572ceaa613150c10dd69d8f8bb7bf263776
Nathan Stitt <nathan@stitt.org>
2018-11-11 22:43:51 -0600

    add theme with colors for styled components

--- tutor/src/components/root.jsx
+++ tutor/src/components/root.jsx
@@ -1,16 +1,20 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from './app';
+import { ThemeProvider } from 'styled-components';
+import TutorTheme from '../theme.js';
 
 export default function TutorRoot() {
   return (
     <BrowserRouter>
-      <div className="tutor-root openstax">
-        <Route
-          path="/"
-          render={props => <App {...props} />}
-        />
-      </div>
+      <ThemeProvider theme={TutorTheme}>
+        <div className="tutor-root openstax">
+          <Route
+            path="/"
+            render={props => <App {...props} />}
+          />
+        </div>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
---
+++ tutor/src/theme.js
@@ -0,0 +1,44 @@
+// Shared with scss in styles/variables
+
+
+const neutral = {
+  lightest: '#f9f9f9', // nearly white
+  bright:   '#f5f5f5', // bright gray
+  cool:     '#f6f7f8', // cool gray
+  lighter:  '#f1f1f1', // light gray
+  light:    '#e4e4e4', // light gray
+  lite:     '#999999', // a gray that's darker than "light"
+  std:      '#818181', // gray
+  thin:     '#6f6f6f',
+  gray:     '#5e5e5e',
+  dark:     '#5f6163', // dark gray
+  darker:   '#424242', // very dark gray
+};
+
+const TutorTheme = {
+
+  colors: {
+    primary:       '#f47641', // orange
+    secondary:     '#63a524', // green
+    tertiary:      '#233066', // dark blue
+    quaternary:    '#f4d019', // yellow
+    blue_control:  '#337eb5', // 337eb5
+    black:         'black',
+    white:         'white',
+
+    neutral,
+
+    controls: {
+      active: '#337eb5',
+    },
+
+  },
+
+
+  borders: {
+    box: `1px solid ${neutral.lighter}`,
+  },
+
+};
+
+export default TutorTheme;
