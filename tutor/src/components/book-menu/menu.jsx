bf61fd1c7b21a359816a2316435427816d19821f
Nathan Stitt <nathan@stitt.org>
2018-12-03 17:15:14 -0600

    hide chapter_section and no longer link to chapters

--- tutor/src/components/book-menu/menu.jsx
+++ tutor/src/components/book-menu/menu.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { observer, propTypes as mobxPropTypes } from 'mobx-react';
+import { computed } from 'mobx';
 import { map, partial } from 'lodash';
 import cn from 'classnames';
 import TutorLink from '../link';
@@ -14,20 +15,33 @@ class BookMenuTocSection extends React.Component {
     section: PropTypes.object,
   };
 
-  renderSection(section) {
+  @computed get title() {
+    const { section } = this.props;
     return (
-      <React.Fragment>
-        <span className="section-number">
-          {section.chapter_section.asString}
-        </span>
-        {section.title}
-      </React.Fragment>
+      <div className="chapter-section-title">
+        {!section.isChapterSectionHidden &&
+          <span className="section-number">
+            {section.displayedChapterSection.asString}
+          </span>}
+        <span key="title">{section.title}</span>
+      </div>
     );
   }
 
-  renderBakedSection(section) {
-    return <span dangerouslySetInnerHTML={{ __html: section.title }} />
+  @computed get linkedTitle() {
+    const { ux, section } = this.props;
+    return (
+      <TutorLink
+        {...ux.sectionLinkProps(section)}
+        tabIndex={ux.isMenuVisible ? 0 : -1}
+        className={section === ux.page ? 'active' : ''}
+        onClick={partial(ux.onMenuSelection, section.chapter_section.asString)}
+      >
+        {this.title}
+      </TutorLink>
+    );
   }
+
 
   render() {
     const { ux, section } = this.props;
@@ -38,15 +52,7 @@ class BookMenuTocSection extends React.Component {
         data-depth={section.depth}
       >
         <li data-section={section.chapter_section.asString}>
-          <TutorLink
-            {...ux.sectionLinkProps(section)}
-            tabIndex={ux.isMenuVisible ? 0 : -1}
-            className={section === ux.page ? 'active' : ''}
-            onClick={partial(ux.onMenuSelection, section.chapter_section.asString)}
-          >
-            {(ux.isCollated ? this.renderBakedSection : this.renderSection)(section)}
-
-          </TutorLink>
+          {section.hasContent ? this.linkedTitle : this.title}
         </li>
         {map(this.props.section.children, child => (
           <li key={child.id} data-section={child.chapter_section.asString}>
