2a2fe18d2cf02c40b4eca61c4fb37400453c103c
Nathan Stitt <nathan@stitt.org>
2018-12-20 12:55:08 -0700

    add trouble icon component

---
+++ tutor/specs/components/icons/trouble.spec.js
@@ -0,0 +1,23 @@
+import TroubleIcon from '../../../src/components/icons/trouble';
+import Plan from '../../../src/models/task-plan/teacher';
+
+describe('Student Preview Builder', () => {
+
+  let props;
+
+  beforeEach(() => {
+    props = { plan: new Plan({ is_trouble: true }) };
+  });
+
+  it('only renders if plan is in trouble', () => {
+    props.plan.is_trouble = false;
+    const plan = mount(<TroubleIcon {...props} />);
+    expect(plan.html()).toBeNull();
+  });
+
+  it('matches snapshot', () => {
+    const plan = mount(<TroubleIcon {...props} />);
+    expect.snapshot(plan.debug()).toMatchSnapshot();
+  });
+
+});
