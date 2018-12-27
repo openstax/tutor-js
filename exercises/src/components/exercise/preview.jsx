1dc9bf2a353ee831493c5ad28dad03571ab08686
Nathan Stitt <nathan@stitt.org>
2018-12-27 15:57:46 -0600

    set margin on a wrapper so it also applies to corner wrapper

--- exercises/src/components/exercise/preview.jsx
+++ exercises/src/components/exercise/preview.jsx
@@ -1,12 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { find } from 'lodash';
+import styled from 'styled-components';
 import { ExercisePreview } from 'shared';
 import { Link } from 'react-router-dom';
 import { computed } from 'mobx';
 import { observer } from 'mobx-react';
 import { CornerRibbon } from 'shared';
 import CurrentUser from '../../models/user';
+
+
+const Preview = styled.div`
+  margin-top: 1.5rem;
+`;
 
 export default
 @observer
@@ -29,28 +35,30 @@ class ExerciseEditingPreview extends React.Component {
     const { showEdit, exercise, ...previewProps } = this.props;
 
     return (
-      <CornerRibbon
-        shadow
-        color="navy"
-        position="topRight"
-        label={
-          [<div key="ro">READ ONLY</div>,<div key="n">Author: {exercise.authors.names().join(',')}</div>]
-        }
-        hidden={this.isAuthor}
-      >
-        <ExercisePreview
-          className='exercise-editing-preview'
-          isInteractive
-          displayAllTags
-          displayFeedback
-          displayFormats
-          displayNickname
-          exercise={exercise}
-          {...previewProps}
+      <Preview>
+        <CornerRibbon
+          shadow
+          color="navy"
+          position="topRight"
+          label={[
+            <div key="ro">READ ONLY</div>,
+            <div key="n">Author: {exercise.authors.names().join(',')}</div>,
+          ]}
         >
-          {showEdit && this.isAuthor && <Link className="btn" to={`/exercise/${exercise.uid}`}>EDIT</Link>}
-        </ExercisePreview>
-      </CornerRibbon>
+          <ExercisePreview
+            className='exercise-editing-preview'
+            isInteractive
+            displayAllTags
+            displayFeedback
+            displayFormats
+            displayNickname
+            exercise={exercise}
+            {...previewProps}
+          >
+            {showEdit && this.isAuthor && <Link className="btn" to={`/exercise/${exercise.uid}`}>EDIT</Link>}
+          </ExercisePreview>
+        </CornerRibbon>
+      </Preview>
     );
   }
 
