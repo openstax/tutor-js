4ee5703fb4395b4fb71a3deac442c69afe119fd1
Nathan Stitt <nathan@stitt.org>
2018-12-26 17:27:45 -0700

    cleanup messages and correctly use propTypes

--- tutor/src/screens/performance-forecast/info-link.js
+++ tutor/src/screens/performance-forecast/info-link.js
@@ -1,60 +1,50 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import createReactClass from 'create-react-class';
 import { Icon } from 'shared';
 
 const MESSAGES = {
-  student:
-  <div>
-    <p key="s1">
+  student: (
+    <div>
+      <p key="s1">
         The performance forecast is an estimate of your understanding of a topic.
-    </p>
-    <p key="s2">
-      {`\
-  It is personalized display based on your answers to reading questions,
-  homework problems, and previous practices.\
-  `}
-    </p>
-  </div>,
-
-  teacher:
-  <div>
-    <p key="s1">
+      </p>
+      <p key="s2">
+        It is personalized display based on your answers to reading questions,
+        homework problems, and previous practices.
+      </p>
+    </div>
+  ),
+  teacher: (
+    <div>
+      <p key="s1">
         The performance forecast is an estimate of each group’s understanding of a topic.
-    </p>
-    <p key="s2">
-      {`\
-  It is personalized display based on their answers to reading questions,
-  homework problems, and previous practices.\
-  `}
-    </p>
-  </div>,
-
-  teacher_student:
-  <div>
-    <p key="st1">
+      </p>
+      <p key="s2">
+        It is personalized display based on their answers to reading questions,
+        homework problems, and previous practices.
+      </p>
+    </div>
+  ),
+  teacher_student: (
+    <div>
+      <p key="st1">
         The performance forecast is an estimate of each student’s understanding of a topic.
-    </p>
-    <p key="st2">
-      {`\
-  It is personalized display based on their answers to reading questions,
-  homework problems, and previous practices.\
-  `}
-    </p>
-  </div>,
+      </p>
+      <p key="st2">
+        It is personalized display based on their answers to reading questions,
+        homework problems, and previous practices.
+      </p>
+    </div>
+  ),
 };
 
-export default createReactClass({
-  displayName: 'PerformanceForecastInfoLink',
-  type: PropTypes.oneOf(['student', 'teacher', 'teacher_student']).isRequired,
+export default function InfoLink({ type }) {
 
-  render() {
-    return (
-      <Icon
-        className="info-link"
-        type="info-circle"
-        tooltipProps={{ placement: 'right' }}
-        tooltip={MESSAGES[this.props.type]} />
-    );
-  },
-});
+  return (
+    <Icon className="info-link" type="info-circle" tooltip={MESSAGES[type]} />
+  );
+}
+
+InfoLink.propTypes = {
+  type: PropTypes.oneOf(['student', 'teacher', 'teacher_student']).isRequired,
+};
