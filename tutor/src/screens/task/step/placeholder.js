6642a8b1fe439d51f3cf56451c08cba9ff7c6d24
Nathan Stitt <nathan@stitt.org>
2019-04-04 18:56:28 -0500

    remove unused vars

--- tutor/src/screens/task/step/placeholder.js
+++ tutor/src/screens/task/step/placeholder.js
@@ -1,27 +1,12 @@
-import { React, PropTypes, observer, styled } from '../../../helpers/react';
-import ContentLoader from 'react-content-loader';
+import { React, PropTypes, observer } from '../../../helpers/react';
 import UX from '../ux';
 import { StepCard } from './card';
-import { ArbitraryHtmlAndMath } from 'shared';
 import ContinueBtn from './continue-btn';
 import Step from '../../../models/student-tasks/step';
-
-const HtmlContent = styled(StepCard)`
-.phet-explorations-embedded { margin: 40px; }
-`;
-
-const Loader = () => (
-  <ContentLoader>
-    <rect x="0" y="0" rx="3" ry="3" width="250" height="10" />
-    <rect x="0" y="70" rx="5" ry="5" width="400" height="400" />
-  </ContentLoader>
-);
 
 export default
 @observer
 class PlaceHolderTaskStep extends React.Component {
-
-  static Loader = Loader;
 
   static propTypes = {
     ux: PropTypes.instanceOf(UX).isRequired,
