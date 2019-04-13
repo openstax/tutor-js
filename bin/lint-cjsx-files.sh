39a50d099fd4dfaf5a7c8c9768587ad1fb3cdc0b
Nathan Stitt <nathan@stitt.org>
2019-04-13 16:40:49 -0500

    replace old cjsx lint with eslint script

--- bin/lint-cjsx-files.sh
+++
@@ -1,6 +0,0 @@
-for FILE in $(find ./shared/src/ ./tutor/src/ ./exercises/src/ -name '*.cjsx')
-do
-  $(npm bin)/cjsx-transform ${FILE} | $(npm bin)/coffee --compile --stdio > ${FILE}.js
-  $(npm bin)/eslint --no-ignore ${FILE}.js
-  rm ${FILE}.js
-done
