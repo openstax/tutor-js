98b6af5d14f2103a5693944383baa5bf38cd8e55
Nathan Stitt <nathan@stitt.org>
2019-04-29 14:08:28 -0500

    add a reload page button

---
+++ tutor/src/components/buttons/reload-page.js
@@ -0,0 +1,43 @@
+import { React, PropTypes, cn, observer, action, observable } from '../../helpers/react';
+import { forceReload } from '../../helpers/reload';
+import { AsyncButton } from 'shared';
+
+@observer
+class ReloadPageButton extends React.Component {
+
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    windowImpl: PropTypes.object,
+  }
+
+  static defaultProps = {
+    children: 'Reload Page',
+  }
+
+  @observable isReloading = false;
+
+  @action.bound onReload() {
+    this.isReloading = true;
+    const { windowImpl } = this.props;
+    forceReload({ windowImpl });
+  }
+
+  render() {
+    return (
+      <AsyncButton
+        className={cn('reload-page', this.props.className)}
+        isWaiting={this.isReloading}
+        variant="primary"
+        size="small"
+        waitingText="Reloading…"
+        onClick={this.onReload}
+      >
+        {this.props.children}
+      </AsyncButton>
+    );
+  }
+
+}
+
+export default ReloadPageButton;
