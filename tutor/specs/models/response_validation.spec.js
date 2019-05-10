d1803892eba9b8d7ea8c7ec734665f52098935ce
Nathan Stitt <nathan@stitt.org>
2019-05-10 16:03:26 -0500

    add request spec

--- tutor/specs/models/response_validation.spec.js
+++ tutor/specs/models/response_validation.spec.js
@@ -1,8 +1,16 @@
 import ResponseValidation from '../../src/models/response_validation';
 
 describe(ResponseValidation, () => {
+  let rv;
+
+  beforeEach(() => {
+    rv = new ResponseValidation();
+    rv.config.url = 'http://test.com';
+    rv.config.is_enabled = true;
+  });
+
   it('does not request info when not configured', () => {
-    let rv = new ResponseValidation();
+    rv.config.url = '';
     expect(rv.validate({ response: 'test' })).toEqual('ABORT');
 
     rv = new ResponseValidation();
@@ -10,4 +18,13 @@ describe(ResponseValidation, () => {
     rv.config.is_enabled = false;
     expect(rv.validate({ response: 'test' })).toEqual('ABORT');
   });
+  it('makes request', () => {
+    expect(
+      rv.validate({ response: 'this is a test', uid: '123@4' }),
+    ).toEqual({
+      url: rv.config.url ,
+      query: { uid: '123@4', response: 'this is a test' },
+    });
+  });
+
 });
