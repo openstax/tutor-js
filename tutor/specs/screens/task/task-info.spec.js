82be34e95ddd4979ad39d67a5f1bed8a7c8a9b87
Nathan Stitt <nathan@stitt.org>
2019-05-31 18:03:37 -0500

    do not show due date on tasks that lack it
    tasks such as practice lack a due date which caused momentjs to
    default to "now"

---
+++ tutor/specs/screens/task/task-info.spec.js
@@ -0,0 +1,25 @@
+import { TaskInfo } from '../../../src/screens/task/task-info';
+import { Factory, TimeMock } from '../../helpers';
+
+describe('Tasks Info', () => {
+  let props;
+
+  TimeMock.setTo('2017-10-14T12:00:00.000Z');
+
+  beforeEach(() => {
+    const task = Factory.studentTask({ stepCount: 1, type: 'reading' });
+    props = { task };
+  });
+
+  it('matches snapshot', () => {
+    expect(<TaskInfo {...props} />).toMatchSnapshot();
+  });
+
+  it('hides due date if task has none', () => {
+    props.task.due_at = null;
+    const taskInfo = mount(<TaskInfo {...props} />);
+    expect(taskInfo).not.toHaveRendered('DueDate');
+    taskInfo.unmount();
+  });
+
+});
