d94a9681b54e388d2801d4cecc0b740beb404703
Nathan Stitt <nathan@stitt.org>
2019-08-05 10:14:16 -0500

    add feature flag for pulseinsights

--- tutor/src/models/app/pulse-insights.js
+++ tutor/src/models/app/pulse-insights.js
@@ -1,5 +1,6 @@
 import loadjs from 'loadjs';
 import Raven from './raven';
+import Flags from '../feature_flags';
 
 function onLoadFailure() {
   Raven.log('Failed to load PulseInsights script');
@@ -14,6 +15,9 @@ class PI {
 
 export default {
   boot() {
+    if (!Flags.pulse_insights) {
+      return Promise.resolve({});
+    }
     window.pi = new PI();
     return loadjs('//js.pulseinsights.com/surveys.js', {
       error: onLoadFailure,
