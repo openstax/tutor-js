02e58f91b12ab6f018b8d17011e1ae35ceeaef2d
Nathan Stitt <nathan@stitt.org>
2019-10-15 12:01:59 -0500

    use sample code from PI

--- tutor/src/models/app/pulse-insights.js
+++ tutor/src/models/app/pulse-insights.js
@@ -2,25 +2,23 @@ import loadjs from 'loadjs';
 import Raven from './raven';
 import Flags from '../feature_flags';
 
-function onLoadFailure() {
-  Raven.log('Failed to load PulseInsights script');
-}
-
-class PI {
-  commands = [
-    ['identify', 'PI-16384954'],
-    ['get', 'surveys'],
-  ];
-}
-
 export default {
   boot() {
     if (!Flags.pulse_insights) {
       return Promise.resolve({});
     }
-    window.pi = new PI();
+    const w = window;
+    w.pi = function() {
+      w.pi.commands = (w.pi.commands || []);
+      w.pi.commands.push(arguments);
+    };
+    w.pi('identify', 'PI-16384954');
+    w.pi('get', 'surveys');
+
     return loadjs('//js.pulseinsights.com/surveys.js', {
-      error: onLoadFailure,
+      async: true,
+      returnPromise: true,
+      error: () => Raven.log('Failed to load PulseInsights script'),
     });
   },
 };
