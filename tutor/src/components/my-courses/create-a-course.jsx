617aa8cc1ab1fdbd6c87219d488f726bc295607f
Nathan Stitt <nathan@stitt.org>
2019-10-29 16:48:22 -0500

    Remove drag-n-drop from courses
    At one time it was a barely supported feature that you
    could drag-n-drop a course onto the "Create a Course"
    card to start a course clone.
    
    It wasn't working right so we just removed it

--- tutor/src/components/my-courses/create-a-course.jsx
+++ tutor/src/components/my-courses/create-a-course.jsx
@@ -4,20 +4,17 @@ import { withRouter } from 'react-router-dom';
 import { observer } from 'mobx-react';
 import classnames from 'classnames';
 import Router from '../../helpers/router';
-import { wrapCourseDropComponent } from './course-dnd';
 import TutorLink from '../link';
 import IconAdd from '../icons/add';
 import TourAnchor from '../tours/anchor';
 
 export default
 @withRouter
-@wrapCourseDropComponent
 @observer
 class CreateACourse extends React.Component {
 
   static propTypes = {
     isHovering: PropTypes.bool,
-    connectDropTarget: PropTypes.func.isRequired,
     history: PropTypes.object.isRequired,
   }
 
@@ -28,20 +25,18 @@ class CreateACourse extends React.Component {
 
   renderAddZone() {
     return (
-      this.props.connectDropTarget(
-        <div className="my-courses-add-zone">
-          <TutorLink
-            to="createNewCourse"
-            className={classnames({ 'is-hovering': this.props.isHovering })}>
-            <div>
-              <IconAdd />
-              <span>
-                CREATE A COURSE
-              </span>
-            </div>
-          </TutorLink>
-        </div>
-      )
+      <div className="my-courses-add-zone">
+        <TutorLink
+          to="createNewCourse"
+          className={classnames({ 'is-hovering': this.props.isHovering })}>
+          <div>
+            <IconAdd />
+            <span>
+              CREATE A COURSE
+            </span>
+          </div>
+        </TutorLink>
+      </div>
     );
   }
 
