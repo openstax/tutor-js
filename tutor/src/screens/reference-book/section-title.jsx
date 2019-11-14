6dcf3913e11e12726c817ee35e6ccfa3efaf7918
Nathan Stitt <nathan@stitt.org>
2019-11-14 11:25:09 -0600

    display title using component, add height

--- tutor/src/screens/reference-book/section-title.jsx
+++ tutor/src/screens/reference-book/section-title.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import ChapterSection from '../../components/chapter-section';
+import BookPartTitle from '../../components/book-part-title';
 
-const SectionTitle = observer(({ ux }) => (
-  <React.Fragment>
-    <span>
-      <ChapterSection chapterSection={ux.page.chapter_section} />
-    </span>
-    <span className="title">
-      {ux.page.title}
-    </span>
-  </React.Fragment>
-));
-
-const BakedSectionTitle = observer(({ ux }) => (
-  <span dangerouslySetInnerHTML={{ __html: ux.page.title }} />
-));
-
-const SectionTitleWrapper = observer(({ ux }) => {
-  if (!ux.page) { return null; }
-
-  const Inner = ux.isCollated ? BakedSectionTitle : SectionTitle;
+const SectionTitleWrapper = observer(({ ux: { page } }) => {
+  if (!page) { return null; }
 
   return (
     <div className="section-title">
-      <Inner ux={ux} />
+      <ChapterSection chapterSection={page.chapter_section} />
+      <BookPartTitle className="title" title={page.title} />
     </div>
   );
 });
