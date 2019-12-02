ed61d8a39b56d94147272d7ee7067624a96b294f
Nathan Stitt <nathan@stitt.org>
2019-12-02 10:02:49 -0600

    Add pagination to exercises search

--- exercises/src/components/search.jsx
+++ exercises/src/components/search.jsx
@@ -1,11 +1,21 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
 import Preview from './exercise/preview';
 import Clause from './search/clause';
 import Controls from './search/controls';
 import { observer, inject } from 'mobx-react';
+import BSPagination from 'shared/components/pagination';
+import Loading from 'shared/components/loading-animation';
+
 import { action } from 'mobx';
 import UX from '../ux';
+
+const Pagination = styled(BSPagination)`
+  justify-content: center;
+  margin-top: 2rem;
+
+`;
 
 @inject('ux')
 @observer
@@ -30,15 +40,16 @@ class Search extends React.Component {
   }
 
   render() {
-
-    const { clauses, exercises } = this.search;
+    const { clauses, exercises, pagination, api } = this.search;
+    const body = api.isPending ?
+      <Loading message="Searching…" /> :
+      exercises.map((e) => <Preview key={e.uuid} exercise={e} showEdit />);
 
     return (
       <div className="search">
         {clauses.map((c, i) => <Clause key={i} clause={c} />)}
-        {exercises.map((e) => (
-          <Preview key={e.uuid} exercise={e} showEdit />
-        ))}
+        {pagination && <Pagination hideFirstAndLastPageLinks {...pagination} />}
+        {body}
       </div>
     );
   }
---
+++ shared/src/components/pagination.js
@@ -0,0 +1,64 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import cn from 'classnames';
+import { createUltimatePagination, ITEM_TYPES } from 'react-ultimate-pagination';
+
+// mostly taken from https://github.com/ultimate-pagination/react-ultimate-pagination-bootstrap-4/blob/master/src/react-ultimate-pagination-bootstrap-4.js#L40
+
+const WrapperComponent = ({ children, className }) => (
+  <ul className={cn('pagination', className)}>{children}</ul>
+);
+
+const withPreventDefault = (fn) => (event) => {
+  event.preventDefault();
+  fn();
+};
+
+const Page = ({ value, isActive, onClick }) => (
+  <li className={isActive ? 'page-item active' : 'page-item'}>
+    <a className="page-link" href="#" onClick={withPreventDefault(onClick)}>{value}</a>
+  </li>
+);
+
+const Ellipsis = ({ onClick }) => (
+  <li className="page-item">
+    <a className="page-link" href="#" onClick={withPreventDefault(onClick)}>...</a>
+  </li>
+);
+
+const FirstPageLink = ({ onClick }) => (
+  <li className="page-item">
+    <a className="page-link" href="#" onClick={withPreventDefault(onClick)}>&laquo;</a>
+  </li>
+);
+
+const PreviousPageLink = ({ onClick }) => (
+  <li className="page-item">
+    <a className="page-link" href="#" onClick={withPreventDefault(onClick)}>&lsaquo;</a>
+  </li>
+);
+
+const NextPageLink = ({ onClick }) => (
+  <li className="page-item">
+    <a className="page-link" href="#" onClick={withPreventDefault(onClick)}>&rsaquo;</a>
+  </li>
+);
+
+const LastPageLink = ({ onClick }) => (
+  <li className="page-item">
+    <a className="page-link" href="#" onClick={withPreventDefault(onClick)}>&raquo;</a>
+  </li>
+);
+
+const itemTypeToComponent = {
+  [ITEM_TYPES.PAGE]: Page,
+  [ITEM_TYPES.ELLIPSIS]: Ellipsis,
+  [ITEM_TYPES.FIRST_PAGE_LINK]: FirstPageLink,
+  [ITEM_TYPES.PREVIOUS_PAGE_LINK]: PreviousPageLink,
+  [ITEM_TYPES.NEXT_PAGE_LINK]: NextPageLink,
+  [ITEM_TYPES.LAST_PAGE_LINK]: LastPageLink,
+};
+
+const Pagination = createUltimatePagination({ itemTypeToComponent, WrapperComponent });
+
+export default Pagination;
