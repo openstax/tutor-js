4fe62275c1ab16a7d19f8e8cccc45e03d2a72775
Josiah Ivey <josiah.ivey@gmail.com>
2019-12-06 12:31:43 -0800

    fix indentation and unused import

--- tutor/src/components/notes/inline-controls.jsx
+++ tutor/src/components/notes/inline-controls.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { observer } from 'mobx-react';
-import { Icon } from 'shared';
 import HighlightIcon from './highlight-icon';
 import AnnotateIcon from './annotate-icon';
 import getRangeRect from './getRangeRect';
