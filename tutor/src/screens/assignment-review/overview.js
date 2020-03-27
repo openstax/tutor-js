f2ff0803f7b928cda6916e87d4187534b33d61b1
Nathan Stitt <nathan@stitt.org>
2020-03-27 09:31:27 -0500

    pull in changes from WRM branch

--- tutor/src/screens/assignment-review/overview.js
+++ tutor/src/screens/assignment-review/overview.js
@@ -1,5 +1,6 @@
 import { React, PropTypes, styled, useObserver } from 'vendor';
 import { StickyTable, Row, Cell } from 'react-sticky-table';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import TutorLink from '../../components/link';
 import S from '../../helpers/string';
 import { Icon } from 'shared';
@@ -118,57 +119,95 @@ const GradeButton = styled(TutorLink).attrs({
   }
 `;
 
-const Overview = ({ ux, ux: { planId, scores } }) => useObserver(() => (
-  <Wrapper data-test-id="overview">
-    <Toolbar>
-      <Center>
-        This assignment is now open for grading.
-      </Center>
-      <Right>
-        <GradeButton
-          params={{ id: planId, courseId: ux.course.id }}
-        >
-          <span className="flag">72 New</span>
-          <span>Grade answers</span>
-        </GradeButton>
-      </Right>
-    </Toolbar>
-    <StyledStickyTable>
-      <Row>
-        <Header>Question Number</Header>
-        {scores.question_headings.map((h, i) => <Header key={i} center={true}>{h.title}</Header>)}
-      </Row>
-      <Row>
-        <Header>Question Type</Header>
-        {scores.question_headings.map((h, i) => <Cell key={i}>{h.type}</Cell>)}
-      </Row>
-      <Row>
-        <Header>Available Points</Header>
-        {scores.question_headings.map((h, i) => <Cell key={i}>{S.numberWithOneDecimalPlace(h.points)}</Cell>)}
-      </Row>
-      <Row>
-        <Header>Correct Responses</Header>
-        {scores.question_headings.map((h, i) => <Cell key={i}>{h.responseStats.points} / {h.responseStats.totalPoints}</Cell>)}
-      </Row>
-    </StyledStickyTable>
-    <Legend>
-      MCQ: Multiple Choice Question (auto-graded);
-      WRQ: Written Response Question (manually-graded);
-      Tutor: Personalized questions assigned by OpenStax Tutor (MCQs & auto-graded)
-    </Legend>
+const StyledTooltip = styled(Tooltip)`
+  max-width: 30rem;
+  &.tooltip.show { opacity: 1; }
 
-    {ux.isExercisesReady ? (
-      <HomeworkQuestions
-        questionsInfo={scores.questionsInfo}
-        questionType="teacher-review"
-        headerContentRenderer={(props) => <QuestionHeader ux={ux} {...props} />}
-        questionInfoRenderer={(props) => <QuestionFreeResponse ux={ux} {...props} />}
-        styleVariant="submission"
-      />) : <Loading message="Loading Questions…"/>}
+  .tooltip-inner {
+    padding: 2.2rem 1.6rem;
+    text-align: left;
+  }
+`;
 
-  </Wrapper>
-));
+const AvailablePoints = ({ value }) => {
+  if (!value) {
+    return (
+      <OverlayTrigger
+        placement="right"
+        overlay={
+          <StyledTooltip>
+            Students received different numbers of Tutor-selected questions. This can happen when questions aren’t
+            available, a student works an assignment late, or a student hasn’t started the assignment.
+          </StyledTooltip>
+        }
+      >
+        <Icon variant="infoCircle" />
+      </OverlayTrigger>
+    );
+  }
+  return (
+    <strong>({S.numberWithOneDecimalPlace(value)})</strong>
+  );
+};
+AvailablePoints.propTypes = {
+  value: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]),
+};
 
+const Overview = ({ ux, ux: { scores } }) => {
+  return useObserver(() => (
+    <Wrapper data-test-id="overview">
+      <Toolbar>
+        <Center>
+          This assignment is now open for grading.
+        </Center>
+        <Right>
+          <GradeButton variant="primary" className="btn-new-flag">
+            <span className="flag">72 New</span>
+            <span>Grade answers</span>
+          </GradeButton>
+        </Right>
+      </Toolbar>
+      <StyledStickyTable>
+        <Row>
+          <Header>Question Number</Header>
+          {scores.question_headings.map((h, i) => <Header key={i} center={true}>{h.title}</Header>)}
+        </Row>
+        <Row>
+          <Header>Question Type</Header>
+          {scores.question_headings.map((h, i) => <Cell key={i}>{h.type}</Cell>)}
+        </Row>
+        <Row>
+          <Header>
+            Available Points <AvailablePoints value={scores.hasEqualTutorQuestions && scores.questionsInfo.totalPoints} />
+          </Header>
+          {scores.question_headings.map((h, i) => <Cell key={i}>{S.numberWithOneDecimalPlace(h.points)}</Cell>)}
+        </Row>
+        <Row>
+          <Header>Correct Responses</Header>
+          {scores.question_headings.map((h, i) => <Cell key={i}>{h.responseStats.points} / {h.responseStats.totalPoints}</Cell>)}
+        </Row>
+      </StyledStickyTable>
+      <Legend>
+        MCQ: Multiple Choice Question (auto-graded);
+        WRQ: Written Response Question (manually-graded);
+        Tutor: Personalized questions assigned by OpenStax Tutor (MCQs & auto-graded)
+      </Legend>
+
+      {ux.isExercisesReady ? (
+        <HomeworkQuestions
+          questionsInfo={scores.questionsInfo}
+          questionType="teacher-review"
+          headerContentRenderer={(props) => <QuestionHeader ux={ux} {...props} />}
+          questionInfoRenderer={(props) => <QuestionFreeResponse ux={ux} {...props} />}
+          styleVariant="submission"
+        />) : <Loading message="Loading Questions…"/>}
+
+    </Wrapper>
+  ));
+};
 Overview.title = 'Submission Overview';
 Overview.propTypes = {
   ux: PropTypes.object.isRequired,
