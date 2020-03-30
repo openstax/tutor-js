b388712ac9283743869749b3175a0695f9a65455
Nathan Stitt <nathan@stitt.org>
2020-03-30 15:22:09 -0500

    add a "primitives" file for extracting small styled things to

---
+++ tutor/src/primitives.js
@@ -0,0 +1,11 @@
+import { styled } from 'vendor';
+import { colors } from 'theme';
+import { Button } from 'react-bootstrap';
+
+// This file is intended to hold very lightweight wrappers around other components
+
+export const ToolbarButton = styled(Button).attrs({
+  variant: 'plain',
+})`
+  && { border: 1px solid ${colors.neutral.pale}; }
+`;
