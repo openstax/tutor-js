3a1a0d3d5e8cbbea416480b4bc7dd543664371e4
Nathan Stitt <nathan@stitt.org>
2020-05-11 16:38:34 -0500

    save scores after they're entered
    also adds rudimentary support for saving and returning them in the mock
    data.  Once the UI is capable of showing already graded questions,
    we should be able to test that they're saved

---
+++ tutor/src/models/task-plans/teacher/grade.js
@@ -0,0 +1,40 @@
+import {
+  BaseModel, identifiedBy, field, belongsTo,
+} from 'shared/model';
+import { pick } from 'lodash';
+import TeacherTaskPlan from './plan';
+
+export default
+@identifiedBy('task-plans/teacher/grade')
+class TeacherTaskGrade extends BaseModel {
+
+  @belongsTo({ model: TeacherTaskPlan }) taskPlan;
+
+  constructor({ question, points, comment }) {
+    super();
+    this.question = question;
+    this.grader_points = points;
+    this.grader_comments = comment;
+  // }, task_step_id: question.task_step_id, grader_points: points, grader_comments: comment,
+  }
+
+  @field grader_points;
+  @field grader_comments;
+
+  save() {
+    return {
+      task_step_id: this.question.task_step_id,
+      data: pick(this, 'grader_points', 'grader_comments'),
+    };
+  }
+
+  onGraded({ data }) {
+    this.update(data);
+    this.question.update(data);
+  }
+  // not clear how these are populated
+  @field answer_id;
+  @field free_response;
+  @field response_validation;
+  @field last_graded_at;
+}
