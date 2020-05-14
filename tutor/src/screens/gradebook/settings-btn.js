a6cd3ecfafe10738eecdc805d1abdbf3cc298fcf
Alexander Cam Liu <acamliu89@gmail.com>
2020-05-14 17:48:20 -0400

    styling settings grabebook checkboxes

--- tutor/src/screens/gradebook/settings-btn.js
+++ tutor/src/screens/gradebook/settings-btn.js
@@ -1,11 +1,56 @@
-import { React, PropTypes, useObserver, styled } from 'vendor';
+import { React, PropTypes, useObserver, styled, observable } from 'vendor';
 import { createRef } from 'react';
 import { Button, Popover, Overlay } from 'react-bootstrap';
 import { observer } from 'mobx-react';
 import { Icon } from 'shared';
+import { colors } from 'theme';
 import UX from './ux';
-import { observable } from 'mobx';
 
+/** https://www.w3schools.com/howto/howto_css_custom_checkbox.asp  */
+const StyledCheckbox = styled.label`
+  display: block;
+  position: relative;
+  padding-left: 30px;
+  margin-bottom: 10px;
+  cursor: pointer;
+  font-size: 14px;
+
+/* Hide default checkbox */
+& input {
+  position: absolute;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+.check {
+  position: absolute;
+  top: 2px;
+  left: 0;
+  height: 20px;
+  width: 20px;
+  background-color: ${colors.white};
+  border: 2px solid ${colors.neutral.pale};
+}
+
+& input:checked ~ .check:after {
+  display: block;
+}
+
+/* Style the check */
+& .check:after {
+  content: "";
+  position: absolute;
+  display: none;
+  left: 5px;
+  top: 2px;
+  width: 6px;
+  height: 10px;
+  border: solid ${colors.neutral.dark};
+  border-width: 0 3px 3px 0;
+  transform: rotate(45deg);
+}
+`;
 
 const Toggles = styled(Popover.Content)`
   padding: 1rem;
@@ -18,14 +63,15 @@ const Toggles = styled(Popover.Content)`
 `;
 
 const SettingsCheckbox = ({ ux, title, property }) => useObserver(() => (
-  <label>
+  <StyledCheckbox>
+    {title}
     <input
       type="checkbox"
       checked={ux[property]}
       onChange={({ target }) => ux[property] = target.checked}
     />
-    {title}
-  </label>
+    <span class="check"></span>
+  </StyledCheckbox>
 ));
 SettingsCheckbox.propTypes ={
   ux: PropTypes.instanceOf(UX).isRequired,
