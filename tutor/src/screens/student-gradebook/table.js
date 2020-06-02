471930fc32b92e21b3baa7febb73b193953b5b58
Alexander Cam Liu <acamliu89@gmail.com>
2020-06-02 11:36:00 -0400

    delete console log

--- tutor/src/screens/student-gradebook/table.js
+++ tutor/src/screens/student-gradebook/table.js
@@ -169,7 +169,6 @@ const GradebookTable = observer((
         </thead>
         <tbody>
           {studentData.map((sd,i) => {
-            console.log(sd);
             return (<tr key={i}>
               <td
                 className={`border-${sd.reportHeading.type}`}
