6ce5bc60e969d9855f32a31f6f520a627195a636
Dante Soares <dante.m.soares@rice.edu>
2020-07-10 18:24:37 -0500

    Use should_reuse_preview to determine when to update preview courses Added specs

---
+++ tutor/specs/models/courses/offerings/previews.spec.js
@@ -0,0 +1,139 @@
+import OfferingsMap from '../../../../src/models/course/offerings';
+import CoursesMap from '../../../../src/models/courses-map';
+import Previews from '../../../../src/models/course/offerings/previews';
+
+describe('Offering Previews Model', () => {
+  describe('when the offering has previews disabled', () => {
+    const OFFERINGDATA = { id: 42, is_preview_available: false }
+    const COURSEDATA = {
+      id: 21,
+      offering_id: OFFERINGDATA.id,
+      is_active: true,
+      is_preview: true,
+      should_reuse_preview: true,
+      roles: [ { type: 'teacher' } ]
+    }
+    beforeAll(
+      () => {
+        OfferingsMap.onLoaded({ data: { items: [ OFFERINGDATA ] } });
+        CoursesMap.onLoaded({ data: [ COURSEDATA ] });
+      }
+    );
+
+    it('returns no preview offerings', () => {
+      expect(Previews.all).toEqual([]);
+    });
+  });
+
+  describe('when the offering has previews enabled', () => {
+    const OFFERINGDATA = { id: 42, is_preview_available: true }
+    beforeAll(() => OfferingsMap.onLoaded({ data: { items: [ OFFERINGDATA ] } }));
+
+    describe('when the user has no courses', () => {
+      beforeAll(() => CoursesMap.reset());
+
+      it('returns a preview offering with no existing courses', () => {
+        expect(Previews.all[0].previewCourse).toBeUndefined();
+      });
+    });
+
+    describe('when the course has the wrong offering_id', () => {
+      const COURSEDATA = {
+        id: 21,
+        offering_id: 84,
+        is_active: true,
+        is_preview: true,
+        should_reuse_preview: true,
+        roles: [ { type: 'teacher' } ]
+      }
+      beforeAll(() => CoursesMap.onLoaded({ data: [ COURSEDATA ] }));
+
+      it('returns a preview offering with no existing courses', () => {
+        expect(Previews.all[0].previewCourse).toBeUndefined();
+      });
+    });
+
+    describe('when the course is not a preview course', () => {
+      const COURSEDATA = {
+        id: 21,
+        offering_id: OFFERINGDATA.id,
+        is_active: true,
+        is_preview: false,
+        should_reuse_preview: true,
+        roles: [ { type: 'teacher' } ]
+      }
+      beforeAll(() => CoursesMap.onLoaded({ data: [ COURSEDATA ] }));
+
+      it('returns a preview offering with no existing courses', () => {
+        expect(Previews.all[0].previewCourse).toBeUndefined();
+      });
+    });
+
+    describe('when the course is not reusable', () => {
+      const COURSEDATA = {
+        id: 21,
+        offering_id: OFFERINGDATA.id,
+        is_active: true,
+        is_preview: true,
+        should_reuse_preview: false,
+        roles: [ { type: 'teacher' } ]
+      }
+      beforeAll(() => CoursesMap.onLoaded({ data: [ COURSEDATA ] }));
+
+      it('returns a preview offering with no existing courses', () => {
+        expect(Previews.all[0].previewCourse).toBeUndefined();
+      });
+    });
+
+    describe('when the course is not active', () => {
+      const COURSEDATA = {
+        id: 21,
+        offering_id: OFFERINGDATA.id,
+        is_active: false,
+        is_preview: true,
+        should_reuse_preview: true,
+        roles: [ { type: 'teacher' } ]
+      }
+      beforeAll(() => CoursesMap.onLoaded({ data: [ COURSEDATA ] }));
+
+      it('returns a preview offering with no existing courses', () => {
+        expect(Previews.all[0].previewCourse).toBeUndefined();
+      });
+    });
+
+    describe('when the user is not teaching the course', () => {
+      const COURSEDATA = {
+        id: 21,
+        offering_id: OFFERINGDATA.id,
+        is_active: true,
+        is_preview: true,
+        should_reuse_preview: true,
+        roles: [ { type: 'teacher_student' } ]
+      }
+      beforeAll(() => CoursesMap.onLoaded({ data: [ COURSEDATA ] }));
+
+      it('returns a preview offering with no existing courses', () => {
+        expect(Previews.all[0].previewCourse).toBeUndefined();
+      });
+    });
+
+    describe(
+      'when the user is teaching an active reusable preview course with the correct offering_id',
+      () => {
+        const COURSEDATA = {
+          id: 21,
+          offering_id: OFFERINGDATA.id,
+          is_active: true,
+          is_preview: true,
+          should_reuse_preview: true,
+          roles: [ { type: 'teacher' } ]
+        }
+        beforeAll(() => CoursesMap.onLoaded({ data: [ COURSEDATA ] }));
+
+        it('returns the existing preview course', () => {
+          expect(Previews.all[0].previewCourse.id).toBe(COURSEDATA.id);
+        });
+      }
+    );
+  });
+});
