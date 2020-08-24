845a9d96b8a6b6e399dae83153fde32e505ce9e7
Nathan Stitt <nathan@stitt.org>
2020-08-24 16:20:44 -0500

    exercise cards no longer use `panel` class

--- tutor/src/models/tour/actions/hover-exercise.js
+++ tutor/src/models/tour/actions/hover-exercise.js
@@ -5,11 +5,11 @@ export default
 class Reposition extends BaseAction {
 
   beforeStep() {
-    this.$('.panel-body').classList.add('on-demo-hover');
+    this.$('.card-body').classList.add('on-demo-hover');
   }
 
   afterStep() {
-    this.$('.panel-body').classList.remove('on-demo-hover');
+    this.$('.card-body').classList.remove('on-demo-hover');
   }
 
 }
