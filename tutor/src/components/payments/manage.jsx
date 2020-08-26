5a4fbc324e49a8d2db5022f597e3dcc1bb424c97
Alexander Cam Liu <acamliu89@gmail.com>
2020-08-25 20:12:09 -0500

    adding responsive design for manage payment screen

--- tutor/src/components/payments/manage.jsx
+++ tutor/src/components/payments/manage.jsx
@@ -2,13 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { observer } from 'mobx-react';
 import { observable, action } from 'mobx';
-import { Container, Table, Button } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import moment from 'moment';
 import { map, extend, isFunction } from 'lodash';
+import styled from 'styled-components';
 import cn from 'classnames';
 import Router from '../../helpers/router';
 import backInfo from '../../helpers/backInfo';
-import BackButton from '../buttons/back-button';
 import Purchases from '../../models/purchases';
 import OXFancyLoader from 'shared/components/staxly-animation';
 import { AsyncButton } from 'shared';
@@ -16,6 +16,92 @@ import NewTabLink from '../new-tab-link';
 import UserMenu from '../../models/user/menu';
 import RefundModal from './refund-modal';
 import Header from '../header';
+import Responsive from '../responsive';
+import { colors, breakpoint } from 'theme';
+
+const StyledContainer = styled.div`
+  padding: 4rem 10rem 50rem;
+  background-color: white;
+  max-width: 1000px;
+  margin: 0 auto;
+  table {
+    font-size: 1.4rem;
+    thead th, tbody td {
+      padding: 1rem;
+    }
+    tbody {
+      td {
+        vertical-align: middle;
+      }
+      .refunded .total {
+        color: #489200;
+      }
+    }
+  }
+  .footer {
+    margin: 10px 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    > * {
+      margin-top: 10px;
+    }
+  }
+
+  ${breakpoint.tablet`
+    padding: 2rem ${breakpoint.margins.tablet};
+  `}
+
+  ${breakpoint.mobile`
+    padding: 1rem ${breakpoint.margins.mobile};
+  `}
+
+
+`;
+
+const PaymentInfoRows = styled.div` 
+  .manage-payment-info-row:nth-child(odd) {
+    background-color: ${colors.neutral.bright};
+  }
+
+  .manage-payment-info-row:last-child {
+    border-bottom: 1px solid ${colors.neutral.pale};
+  }
+
+  .manage-payment-info-row {
+    padding: 0 10px 10px;
+    width: 100%;
+    border-top: 1px solid ${colors.neutral.pale};
+
+    .manage-payment-info {
+      display: flex;
+      margin-top: 10px;
+      flex-wrap: wrap;
+
+      > .refunded {
+        color: #489200 !important;
+      }
+
+      > span:first-child {
+        flex: 0 1 35%;
+        font-size: 1.2rem;
+        color: ${props => props.theme.colors.neutral.thin};
+      }
+
+      > span:last-child {
+        flex: 0 1 50%;
+        align-self: flex-end;
+        font-size: 1.3rem;
+        color: ${props => props.theme.colors.neutral.darker};
+      }
+    }
+
+    button {
+      margin: 10px 0;
+      font-size: 1.4rem;
+    }
+  }
+`;
 
 const defaultOptions = {
   toolbar: 'no',
@@ -83,18 +169,14 @@ class ManagePayments extends React.Component {
   renderRefundCell(purchase) {
     if (purchase && purchase.isRefundable) {
       return (
-        <td className="refund">
-          <AsyncButton
-            data-identifier={purchase.identifier}
-            isWaiting={purchase.api.isPending}
-            onClick={this.onRequestRefund}
-          >
+        <AsyncButton
+          data-identifier={purchase.identifier}
+          isWaiting={purchase.api.isPending}
+          onClick={this.onRequestRefund}
+        >
             Request Refund
-          </AsyncButton>
-        </td>
+        </AsyncButton>
       );
-    } else if (Purchases.isAnyRefundable) {
-      return <td></td>;
     } else {
       return null;
     }
@@ -104,13 +186,13 @@ class ManagePayments extends React.Component {
   renderInvoiceButton(purchase) {
     if (purchase.is_refund_record) { return null; }
     return (
-      <Button variant="link"
+      <a
         data-identifier={purchase.identifier}
         onClick={this.onShowInvoiceClick}
         href={purchase.invoiceURL}
       >
         Invoice
-      </Button>
+      </a>
     );
   }
 
@@ -124,8 +206,9 @@ class ManagePayments extends React.Component {
             <th>Item</th>
             <th>Transaction date</th>
             <th>Order number</th>
-            <th className="right">Amount</th>
-            {this.renderRefundCell()}
+            <th>Amount</th>
+            <th>Invoice</th>
+            {Purchases.isAnyRefundable && <th></th>}
           </tr>
         </thead>
         <tbody>
@@ -134,17 +217,58 @@ class ManagePayments extends React.Component {
               <td>{purchase.product.name}</td>
               <td>{moment(purchase.purchased_at).format('MMMM Do YYYY')}</td>
               <td>{purchase.identifier}</td>
-              <td className="right total">
+              <td className="total">
                 {purchase.formattedTotal}
               </td>
-              {this.renderRefundCell(purchase)}
-              <td>
-                {this.renderInvoiceButton(purchase)}
-              </td>
+              <td>{this.renderInvoiceButton(purchase)}</td>
+              {Purchases.isAnyRefundable && <td className="refund">{this.renderRefundCell(purchase)}</td>}
             </tr>
           )}
         </tbody>
       </Table>
+    );
+  }
+
+  renderList() {
+    if (Purchases.isEmpty) { return this.renderEmpty(); }
+    return (
+      <PaymentInfoRows>
+        {Purchases.withRefunds.map(purchase =>
+          <div className="manage-payment-info-row" key={purchase.identifier}>
+            <div className="manage-payment-info">
+              <span>Item</span> 
+              <span>
+                {purchase.product.name}
+              </span>
+            </div>
+            <div className="manage-payment-info">
+              <span>Transaction date</span> 
+              <span>
+                {moment(purchase.purchased_at).format('MMMM Do YYYY')}
+              </span>
+            </div>
+            <div className="manage-payment-info">
+              <span>Order number</span> 
+              <span>
+                {purchase.identifier}
+              </span>
+            </div>
+            <div className="manage-payment-info">
+              <span>Amount</span> 
+              <span className={cn({ refunded: purchase.is_refund_record })}>
+                {purchase.formattedTotal}
+              </span>
+            </div>
+            <div className="manage-payment-info">
+              <span>Invoice</span> 
+              <span>
+                {this.renderInvoiceButton(purchase)}
+              </span>
+            </div>
+            {this.renderRefundCell(purchase)}
+          </div>
+        )}
+      </PaymentInfoRows>
     );
   }
 
@@ -169,13 +293,16 @@ class ManagePayments extends React.Component {
           backTo={back.to || 'myCourses'}
           backToText={back.text || 'Back'}
         />
-        <Container className="manage-payments">
+        <StyledContainer className="manage-payments">
           <RefundModal
             purchase={this.refunding}
             onRefund={this.onRefundConfirm}
             onCancel={this.onRefundCancel}
           />
-          {Purchases.api.isPending ? <OXFancyLoader isLoading /> : this.renderTable()}
+          {Purchases.api.isPending
+            ? <OXFancyLoader isLoading />
+            : <Responsive mobile={this.renderList()} tablet={this.renderList()} desktop={this.renderTable()} /> 
+          }
           <div className="footer">
             <NewTabLink
               className="refund-policy"
@@ -187,7 +314,7 @@ class ManagePayments extends React.Component {
             Need help? <a href={`mailto:${UserMenu.supportEmail}`}>Contact Support</a>
             </div>
           </div>
-        </Container>
+        </StyledContainer>
       </>
     );
   }
