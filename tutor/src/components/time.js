1546fab79d379717cce827a1a932986cf4028f91
Josiah Ivey <josiah.ivey@gmail.com>
2020-08-28 20:06:57 +0100

    don't render abbreviated tz, show tz info on hover

--- tutor/src/components/time.js
+++ tutor/src/components/time.js
@@ -2,6 +2,8 @@ import TimeModel from '../models/time';
 import PropTypes from 'prop-types';
 import React from 'react';
 import TimeHelper from '../helpers/time';
+import { moment } from 'vendor';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 export default class Time extends React.Component {
   static defaultProps = {
@@ -18,20 +20,31 @@ export default class Time extends React.Component {
     format: PropTypes.string,
   };
 
+  dateFormat = (value) => { switch (value) {
+    case 'shortest': return 'M/D'; // 9/14
+    case 'short': return 'MMM DD, YYYY'; // Feb 14, 2010
+    case 'concise': return 'ddd, MMM DD[,] h:mma';
+    case 'medium': return 'dddd, MMMM Do YYYY, h:mma'; // Sunday, February 14th 2010, 3:25pm
+    case 'long': return 'dddd, MMMM Do YYYY, h:mm:ss a'; // Sunday, February 14th 2010, 3:25:50 pm
+    default: return value;
+  } };
+
   render() {
     let { format, date } = this.props;
-    format = (() => { switch (this.props.format) {
-      case 'shortest': return 'M/D'; // 9/14
-      case 'short': return 'MMM DD, YYYY'; // Feb 14, 2010
-      case 'concise': return 'ddd, MMM DD[,] h:mma z';
-      case 'long': return 'dddd, MMMM Do YYYY, h:mm:ss a'; // Sunday, February 14th 2010, 3:25:50 pm
-      default: return this.props.format;
-    } })();
 
     return (
-      <time>
-        {TimeHelper.momentInLocal(date).format(format)}
-      </time>
+      <OverlayTrigger
+        placement="right"
+        overlay={
+          <Tooltip>
+            {moment(date).format(this.dateFormat('medium'))} {TimeHelper.momentOffsetWithBrowserTzName(date)}
+          </Tooltip>
+        }
+      >
+        <time>
+          {moment(date).format(this.dateFormat(format))}
+        </time>
+      </OverlayTrigger>
     );
   }
 }
