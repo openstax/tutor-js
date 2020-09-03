0347d1cdf1ff37f9fe011780a4b82b67cd220de3
Nathan Stitt <nathan@stitt.org>
2020-09-03 15:53:45 -0500

    test for my-courses

--- tutor/scripts/health-checks/login.js
+++ tutor/scripts/health-checks/login.js
@@ -2,8 +2,7 @@ context('Login', () => {
   it('loads application', () => {
     cy.loginAccount();
     cy.checkLastPageLoadTime(5, 'seconds');
-    // enable once released and test element exists
-    // cy.getTestElement('my-courses').should('exist')
+    cy.getTestElement('my-courses')
   })
   
 })
