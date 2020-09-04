f8e617f3b9afb2513c2ce2b2343950f8cb289462
Nathan Stitt <nathan@stitt.org>
2020-09-03 19:12:57 -0500

    display 2nd toolbar with pay now timer+btn on mobile

---
+++ tutor/src/components/navbar/student-payment-bar.js
@@ -0,0 +1,48 @@
+import { React, PropTypes, observer, styled } from 'vendor';
+import Theme from 'theme';
+import WindowSize from '../../models/window-size';
+import { Nav } from './nav';
+import { willDisplayPayment, StudentPayNowBtn } from './student-pay-now-btn';
+import Course from '../../models/course';
+
+const PaymentBar = styled(Nav)`
+  top: ${Theme.navbars.top.height};
+  .student-pay-now {
+    align-items: center;
+    font-size: 1.4rem;
+    .btn {
+      font-size: 1.2rem;
+      padding: 0.5rem;
+      white-space: nowrap;
+      margin-left: 0.5rem;
+    }
+  }
+`;
+
+
+@observer
+class MobilePaymentBar extends React.Component {
+
+  static propTypes = {
+    course: PropTypes.instanceOf(Course),
+  }
+
+  windowSize = new WindowSize()
+
+  render() {
+    const { course } = this.props;
+
+    if (!(this.windowSize.isMobile && willDisplayPayment(course))) {
+      return null;
+    }
+
+    return (
+      <PaymentBar area="header" className="mobile-payment-bar">
+        <StudentPayNowBtn course={course}/>
+      </PaymentBar>
+    );
+  }
+
+}
+
+export { MobilePaymentBar };
