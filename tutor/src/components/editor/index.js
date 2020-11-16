fab91e6678e4476693fffa1904557ad0e5ffea4c
Nathan Stitt <nathan@stitt.org>
2020-11-16 12:08:24 -0600

    transistion

--- tutor/src/components/editor/index.js
+++ tutor/src/components/editor/index.js
@@ -1,74 +1,137 @@
-import { React, useState, useEffect, useRef } from 'vendor';
-import {EditorState} from 'prosemirror-state'
-import {EditorView} from 'prosemirror-view'
-import {Schema, DOMParser} from 'prosemirror-model'
-import {schema} from 'prosemirror-schema-basic'
-import {addListNodes} from 'prosemirror-schema-list'
-import {exampleSetup} from 'prosemirror-example-setup';
-import {tableEditing, columnResizing, tableNodes, fixTables} from 'prosemirror-tables';
-import Wrapper from './wrapper.js';
-import ImgNodeSpec from './img-node-spec'
-import ImgNodeView from './image-node-view'
+import { React, useState, useMemo } from 'vendor';
+import { ArbitraryHtmlAndMath as HTML } from 'shared';
+import { Editor, convertFromHTML, Foo as PWFoo, UICommand } from 'perry-white'
+import { last } from 'lodash'
+import { Foo as FormialFoo } from 'formial';
+// import { Foo } from 'perry-white'
 
-function bindNodeView(NodeView) {
-  return (node, view, getPos, decorations) => {
-    return new NodeView(node, view, getPos, decorations);
-  };
+
+// import 'perry-white/dist/styles.css'
+
+class A extends FormialFoo {
+
 }
 
-const mySchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block')
-    .append(tableNodes({
-      tableGroup: 'block',
-      cellContent: 'block+',
-      cellAttributes: {
-        background: {
-          default: null,
-          getFromDOM(dom) { return dom.style.backgroundColor || null },
-          setDOMAttr(value, attrs) { if (value) attrs.style = (attrs.style || '') + `background-color: ${value};` }
-        },
-      },
-    })).append({
-      image: ImgNodeSpec,
-    }),
-  marks: schema.spec.marks,
-});
+class B extends PWFoo {
 
-
-const nodeViews = {
-  'image': (...args) => new ImgNodeView(...args),
 }
 
 
-const parser = DOMParser.fromSchema(mySchema);
+console.log(
+  "FORMIAL", new A(),
+)
+
+console.log(
+  "PW",  new B(),
+)
 
 
-export const Editor = ({ content }) => {
-  const [editor, setEditor] = useState();
-  const wrapperRef = useRef();
-  const contentRef = useRef();
-  useEffect(() => {
-    if (wrapperRef.current) {
-      contentRef.current.innerHTML = content;
+class SaveCommand extends Foo {
 
-      const ed = new EditorView(wrapperRef.current, {
-        nodeViews: nodeViews,
-        state: EditorState.create({
-          doc: parser.parse(contentRef.current),
-          plugins: exampleSetup({ schema: mySchema }),
-        }),
-      })
-      setEditor(ed)
+  constructor() {
+    super()
+  }
+
+    //isEnabled() { return true }
+
+    isActive = (state) => {
+        return true
     }
-  }, []);
+    isEnabled = (state) => {
+        return true
+    }
+
+    execute = () => {
+        debugger
+    }
+
+//    isActive() { return true }
+}
+
+// debugger
+// const sv = new SaveCommand()
+// console.log(sv)
+
+
+class EditorRuntime {
+
+  // Image Upload
+  canUploadImage() {
+    return true
+  }
+
+  uploadImage(blob) {
+    let img
+    // Note: While looking at the uploadImage() function, it is found that a promise is resolved blindly after 3 seconds. Is it a
+    // requirement? If not, then I think it causes two issues, 1. Even if an image upload finishes in 700ms, it will take 3s for
+    // resolving the promise. 2. If the image upload takes more than 3s, then the promise will be incorrectly resolved before
+    // completing the upload.
+    // The following structure may be good to solve the issue.
+    return new Promise((resolve, reject) => {
+      // Use uploaded image URL.
+      const url = window.location.protocol + '//' + window.location.hostname + ':3004/saveimage?fn=' + blob.name
+
+      // POST(url, blob, 'application/octet-stream').then(data => {
+      //   img = JSON.parse(data)
+      //   resolve(img)
+      // }, err => {
+      //   img = {
+      //     id: '',
+      //     width: 0,
+      //     height: 0,
+      //     src: '',
+      //   }
+      //   resolve(img)
+      // })
+    })
+  }
+
+  filterCommandGroups(groups) {
+
+
+    last(groups)['[save] Save:'] = (new SaveCommand())
+    // groups.forEach(g => {
+    //     delete g['[font_download] Font Type']
+    // })
+    console.log("FILTER", groups)
+    return groups
+  }
+}
+
+
+const Editing = ({ html }) => {
+
+  const defaultEditorState = useMemo(() => convertFromHTML(html, null, null), [html]);
+  const runtime = useMemo(() => new EditorRuntime(), [])
 
   return (
-    <div>
-      <h1>ProseMirror</h1>
-      <div style={{ display: 'none' }} ref={contentRef} />
-      <Wrapper ref={wrapperRef} />
+    <div style={{ margin: '100px auto', maxWidth: '1100px', height: '500px' }}>
+
+      <Editor
+        defaultEditorState={defaultEditorState}
+        height="100%" width="100%"
+        runtime={runtime}
+        fitToContent
+      />
+
     </div>
   );
-
 };
+
+
+const ClickToEdit = ({ html }) => {
+  return <p>hi</p>
+
+  const [isEditing, setEditing] = useState(true)
+  const [currentHTML, setHTML] = useState(html)
+
+  if (isEditing) {
+    return <Editing html={currentHTML} />
+  }
+  return <HTML html={currentHTML} onClick={() => setEditing(true)}/>
+
+}
+
+
+export { ClickToEdit }
 
--- tutor/src/screens/demo/editor.js
+++ tutor/src/screens/demo/editor.js
@@ -1,33 +1,24 @@
 import { React } from 'vendor';
-import { asyncComponent } from '../../helpers/async-component';
-// import { Editor } from '../../components/editor';
-//import { Editor, COMMAND_GROUPS, TABLE_COMMANDS_GROUP } from 'pmrosemirror-plus'
 
-import {Editor} from 'perry-white'
-//import 'perry-white/dist/styles.css'
-// import convertFromHTML from 'prosemirror-plus/dist/convertFromHTML'
-
-import 'perry-white/dist/styles.css';
-
-console.log(Editor)
-
-
-// import {COMMAND_GROUPS} from 'prosemirror-plus/dist/ui/EditorToolbarConfig';
-
-const docJSON = { 'type': 'doc', 'attrs': { 'layout': null, 'padding': null, 'width': null }, 'content': [{ 'type': 'paragraph', 'attrs': { 'align': null, 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null }, 'content': [{ 'type': 'text', 'marks': [{ 'type': 'mark-font-type', 'attrs': { 'name': 'Arial Black' } }], 'text': 'First line Arial black' }] }, { 'type': 'ordered_list', 'attrs': { 'id': null, 'counterReset': null, 'indent': 0, 'following': null, 'listStyleType': null, 'name': null, 'start': 1 }, 'content': [{ 'type': 'list_item', 'attrs': { 'align': null }, 'content': [{ 'type': 'paragraph', 'attrs': { 'align': null, 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null }, 'content': [{ 'type': 'text', 'text': 'List 1' }] }] }] }, { 'type': 'ordered_list', 'attrs': { 'id': null, 'counterReset': null, 'indent': 1, 'following': null, 'listStyleType': null, 'name': null, 'start': 1 }, 'content': [{ 'type': 'list_item', 'attrs': { 'align': null }, 'content': [{ 'type': 'paragraph', 'attrs': { 'align': null, 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null }, 'content': [{ 'type': 'text', 'text': 'Child' }] }] }] }, { 'type': 'ordered_list', 'attrs': { 'id': null, 'counterReset': 'none', 'indent': 0, 'following': null, 'listStyleType': null, 'name': null, 'start': 1 }, 'content': [{ 'type': 'list_item', 'attrs': { 'align': null }, 'content': [{ 'type': 'paragraph', 'attrs': { 'align': null, 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null }, 'content': [{ 'type': 'text', 'text': 'List 2' }] }] }] }, { 'type': 'paragraph', 'attrs': { 'align': 'center', 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null }, 'content': [{ 'type': 'text', 'text': 'Align' }] }, { 'type': 'paragraph', 'attrs': { 'align': null, 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null }, 'content': [{ 'type': 'text', 'marks': [{ 'type': 'mark-text-color', 'attrs': { 'color': '#f20d96' } }], 'text': 'Font' }, { 'type': 'text', 'text': ' ' }, { 'type': 'text', 'marks': [{ 'type': 'mark-text-highlight', 'attrs': { 'highlightColor': '#e5e5e5' } }], 'text': 'Color ' }, { 'type': 'text', 'marks': [{ 'type': 'strong' }], 'text': 'align ' }, { 'type': 'text', 'marks': [{ 'type': 'link', 'attrs': { 'href': 'http://www.google.com', 'rel': 'noopener noreferrer nofollow', 'target': 'blank', 'title': null } }, { 'type': 'em' }], 'text': 'Link to google' }, { 'type': 'text', 'marks': [{ 'type': 'em' }], 'text': ' ' }, { 'type': 'text', 'marks': [{ 'type': 'underline' }], 'text': 'underline ' }, { 'type': 'text', 'marks': [{ 'type': 'em' }, { 'type': 'strong' }, { 'type': 'mark-text-color', 'attrs': { 'color': '#e5e5e5' } }, { 'type': 'mark-text-highlight', 'attrs': { 'highlightColor': '#979797' } }, { 'type': 'underline' }], 'text': 'combined' }] }, { 'type': 'heading', 'attrs': { 'align': null, 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null, 'level': 1 }, 'content': [{ 'type': 'text', 'text': 'Header 1' }] }, { 'type': 'paragraph', 'attrs': { 'align': null, 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null } }, { 'type': 'table', 'attrs': { 'marginLeft': null }, 'content': [{ 'type': 'table_row', 'content': [{ 'type': 'table_cell', 'attrs': { 'colspan': 1, 'rowspan': 1, 'colwidth': null, 'borderColor': null, 'background': null }, 'content': [{ 'type': 'paragraph', 'attrs': { 'align': null, 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null }, 'content': [{ 'type': 'text', 'marks': [{ 'type': 'strong' }], 'text': 'Cell 1' }] }] }, { 'type': 'table_cell', 'attrs': { 'colspan': 1, 'rowspan': 1, 'colwidth': null, 'borderColor': null, 'background': null }, 'content': [{ 'type': 'paragraph', 'attrs': { 'align': null, 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null }, 'content': [{ 'type': 'text', 'text': 'Cell 2' }] }] }] }] }, { 'type': 'paragraph', 'attrs': { 'align': null, 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null } }, { 'type': 'paragraph', 'attrs': { 'align': null, 'color': null, 'id': null, 'indent': null, 'lineSpacing': null, 'paddingBottom': null, 'paddingTop': null }, 'content': [{ 'type': 'text', 'text': 'Subscript ' }, { 'type': 'text', 'marks': [{ 'type': 'super' }], 'text': '2 ' }] }] };
-
-// const AtlaskitEditor = asyncComponent(() => import(/* webpackChunkName: 'atlaskit' */ './atlaskit'));
-// import AtlaskitEditor from './atlaskit';
-//const TrixEditor = asyncComponent(() => import(/* webpackChunkName: 'trix' */ './editor/trix'));
-//const ProseMirrorEditor = asyncComponent(() => import(/* webpackChunkName: 'prosemirror' */ './editor/prosemirror'));
-// import ProseMirrorEditor from './prosemirror';
-
+import { ClickToEdit } from '../../components/editor'
 
 const fakeExercise = `
 <div>
+<p>
+
+
+How many <span data-math="\\text{km/h}"></span> are equal to <span data-math="1.0\\,\\text{m/s}">1.0\\,\\text{m/s}</span>? Hint: Carry out the explicit steps involved in the conversion.
+
+The speed limit on some interstate highways is roughly <span data-math="100,\\text{km/h}\">100\\,\\text{km/h}</span>. How many miles per hour is this if <span data-math=\"1.0\\,\\text{mile}\">1.0\\,\\text{mile}</span> is about <span data-math="1.609,\\text{km}">1.609,\text{km}</span>?
+
+          <img src="https://s3-us-west-2.amazonaws.com/openstax-assets/Ss2Xg59OLfjbgarp-prodtutor/exercises/attachments/large_aeb10979cb1265ae1f928677f271c8e4f0f6154a26723b80e40ca3db913579d3.png" alt="The x-axis is temperature in Celsius degrees, ranging from 0 to 70 degrees in increments of 10 degrees.  The y-axis is rate of reaction and is not numbered.  The curve begins at 0 degrees, where the rate of the reaction is close to zero.  The curve proceeds upwards with a sharp slope until peaking at about 38 degrees, where an indicator line is labeled “optimal temperature.”  The curve is relatively flat for another ten degrees and then drops precipitously  from 50 to 60 degrees, hitting zero again at about 61 degrees." />
+
+</p>
+
+
           The graph shows what effect temperature has on the functioning of enzymes.
           What conclusion can be drawn from these data?
-          <img src="https://s3-us-west-2.amazonaws.com/openstax-assets/Ss2Xg59OLfjbgarp-prodtutor/exercises/attachments/large_aeb10979cb1265ae1f928677f271c8e4f0f6154a26723b80e40ca3db913579d3.png" alt="The x-axis is temperature in Celsius degrees, ranging from 0 to 70 degrees in increments of 10 degrees.  The y-axis is rate of reaction and is not numbered.  The curve begins at 0 degrees, where the rate of the reaction is close to zero.  The curve proceeds upwards with a sharp slope until peaking at about 38 degrees, where an indicator line is labeled “optimal temperature.”  The curve is relatively flat for another ten degrees and then drops precipitously  from 50 to 60 degrees, hitting zero again at about 61 degrees." />
+          
 
           <p>
             An experiment investigating the effects of water and fertilizer on plant growth was conducted. Each plant was given the same amount of light per day and was kept at the same temperature. The results are listed in the table.
@@ -69,43 +60,6 @@ const fakeExercise = `
 </div>
 `;
 
-      // <h1>Trix</h1>
-      // <TrixEditor content={fakeExercise} />
-      // <h1>Quill</h1>
-      // <InlineEditor content={fakeExercise} />
-
-const EditorDemo = () => {
-  const [editorView, setEditorView] = React.useState()
-  const [editorState, setEditorState] = React.useState(
-
-  )
-  const onReady = React.useCallback((ev) => {
-    setEditorView(ev)
-  })
-  const onChange = React.useCallback(({ state, transaction }) => {
-
-    // const { data: { state, transaction }
-    //if (transaction.docChanged) {
-    setEditorState(state.apply(transaction))
-    //}
-  })
-
-  return (
-    <div style={{ margin: '100px auto', maxWidth: '1100px', height: '500px' }}>
-
-      <Editor
-        onReady={onReady}
-        editorState={editorState}
-        onChange={onChange}
-        height="100%" width="100%"
-        readOnly={false} disabled={false}
-        data={docJSON} embedded={false}
-        fitToContent
-        defaultHTML={fakeExercise}
-      />
-
-    </div>
-  );
-};
+const EditorDemo = () => <ClickToEdit html={fakeExercise} />
 
 export default EditorDemo;
