6ff880b24228d79c0694542464693d542be5bab9
Alexander Cam Liu <acamliu89@gmail.com>
2021-01-22 16:33:26 -0600

    change the header style in course settings

--- tutor/src/screens/course-settings/index.jsx
+++ tutor/src/screens/course-settings/index.jsx
@@ -2,16 +2,27 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { observable, computed, action } from 'mobx';
 import { observer } from 'mobx-react';
+import moment from 'moment-timezone';
+import styled from 'styled-components';
 import Courses from '../../models/courses-map';
 import CoursePage from '../../components/course-page';
 import Tabs from '../../components/tabs';
+import CourseBreadcrumb from '../../components/course-breadcrumb';
 import StudentAccess from './student-access';
 import RenameCourseLink from './rename-course';
 import Timezone from './timezone';
-import moment from 'moment-timezone';
 import './styles.scss';
 
 const df = (d) => moment(d).format('MM/DD/YYYY');
+
+const StyledCourseSettings = styled(CoursePage)`
+  &&& .body-wrapper {
+    padding: 0 40px;
+    .body {
+      padding-top: 10px;
+    }
+  }
+`;
 
 export default
 @observer
@@ -57,27 +68,34 @@ class CourseSettings extends React.Component {
     );
   }
 
+  titleBreadcrumbs(course) {
+    return <CourseBreadcrumb course={course} currentTitle="Course Settings" noBottomMargin />;
+  }
+
   render() {
     const { course, tabIndex } = this;
     return (
-      <CoursePage
+      <StyledCourseSettings
         className="settings"
-        title="Course settings"
+        title=""
         course={course}
+        titleBreadcrumbs={this.titleBreadcrumbs(course)}
+        titleAppearance="light"
+        controlBackgroundColor='white'
       >
-        <div className="course-settings-title">
+        {/* <div className="course-settings-title">
           {course.name}
           <RenameCourseLink course={this.course} />
         </div>
         <h4 className="course-settings-term">
           {course.termFull}
-        </h4>
+        </h4> */}
         <Tabs
           tabs={['STUDENT ACCESS', 'DATES AND TIME']}
           onSelect={this.onTabSelect}
         />
         {tabIndex ? this.renderDates() : this.renderAccess()}
-      </CoursePage>
+      </StyledCourseSettings>
     );
   }
-};
+}
