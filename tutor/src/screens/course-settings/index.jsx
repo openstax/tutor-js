03f9269863c82bf61f6dd5792265faf4cd314e73
Alexander Cam Liu <acamliu89@gmail.com>
2021-01-25 22:09:19 -0600

    show save changes button if there are any changes in courseName, courseCode or timezone. Disabled the save changes button if courseName is empty

--- tutor/src/screens/course-settings/index.jsx
+++ tutor/src/screens/course-settings/index.jsx
@@ -4,8 +4,10 @@ import { observable, computed, action } from 'mobx';
 import { observer } from 'mobx-react';
 import moment from 'moment-timezone';
 import styled from 'styled-components';
-import { Form, Row, Col } from 'react-bootstrap';
+import { Form, Row, Col, Button } from 'react-bootstrap';
 import { Formik } from 'formik';
+import { isEmpty } from 'lodash';
+import cn from 'classnames';
 import Courses from '../../models/courses-map';
 import CoursePage from '../../components/course-page';
 import Tabs from '../../components/tabs';
@@ -27,43 +29,73 @@ const StyledCourseSettings = styled(CoursePage)`
   }
 
   &&& .course-details {
-    .form-group {
-      margin-bottom: 2.4rem;
-      label {
-        font-weight: 700;
-        color: ${colors.darker};
-        font-size: 1.4rem;
-        line-height: 2rem;
-        padding: 10px 0;
+    form {
+      .form-controls {
+        display: flex;
+        justify-content: space-between;
+        width: 75%;
+        .btn.btn-primary {
+          padding: 0.75rem 2rem;
+          &.hidden {
+            display: none;
+          }
+        }
+        .btn.btn-link {
+          margin-left: -15px;
+          padding-top: 15px;
+        }
       }
-      .form-control {
-        height: 45px;
-        color: ${colors.darker};
-        font-size: 1.6rem;
-        width: 25%;
-        &#course-name {
-          width: 60%;
+      .form-group {
+        margin-bottom: 2.4rem;
+        &.row {
+          margin-left: 0;
+        }
+        span.error-message {
+          color: red;
+          margin-top: 2px;
+        }
+        label {
+          font-weight: 700;
+          color: ${colors.darker};
+          font-size: 1.4rem;
+          line-height: 2rem;
+          padding: 10px 0;
+        }
+        .form-control {
+          height: 45px;
+          color: ${colors.darker};
+          font-size: 1.6rem;
+          width: 25%;
+          &.error-input {
+            background: #fbe7ea;
+            color: red;
+            border-color: #f4c0c5;
+            border-width: 2px;
+          }
+          &#course-name {
+            width: 60%;
+            ${breakpoint.tablet`
+              width: 100%;
+            `}
+          }
+          &#endDate, 
+          &#startDate {
+            width: 57%;
+            ${breakpoint.tablet`
+              width: 100%;
+            `}
+          }
           ${breakpoint.tablet`
             width: 100%;
           `}
         }
-        &#endDate, 
-        &#startDate {
-          width: 57%;
-          ${breakpoint.tablet`
-            width: 100%;
-          `}
-        }
-        ${breakpoint.tablet`
-          width: 100%;
-        `}
-      }
-      &.dates {
-        .end-date-label {
-          margin-left: -15rem;
-          ${breakpoint.tablet`
-            margin-left: 0;
-          `}
+        &.dates {
+          .end-date-label {
+            margin-left: -15rem;
+            ${breakpoint.tablet`
+              margin-left: 0;
+            `}
+          }
         }
       }
     }
@@ -100,7 +132,6 @@ class CourseSettings extends React.Component {
     return Courses.get(this.props.params.courseId);
   }
 
-
   @action.bound onTabSelect(tabIndex) {
     this.tabIndex = tabIndex;
   }
@@ -117,7 +148,7 @@ class CourseSettings extends React.Component {
 
   renderCourseDetails() {
     const { course } = this;
-    const values = {
+    const initialValues = {
       courseName: course.name,
       courseCode: '',
       term: course.termFull,
@@ -125,14 +156,30 @@ class CourseSettings extends React.Component {
       endDate: course.bounds.end,
       timezone: course.timezone,
     };
+
+    const hasChanges = (courseName, courseCode, timezone) => {
+      const hasCourseNameChanged = initialValues.courseName !== courseName.trim();
+      const hasCourseCodeChanged = initialValues.courseCode !== courseCode.trim();
+      const hasTimezoneChanged = initialValues.timezone !== timezone;
+      return hasCourseNameChanged || hasCourseCodeChanged || hasTimezoneChanged;
+    };
+
+    const validate = (values) => {
+      const errors = {};
+      if(!values.courseName.trim())
+        errors.courseName = 'Required';
+      return errors;
+    };
+
     return (
       <div className="course-details">
-        <Formik initialValues={values}>
+        <Formik initialValues={initialValues} validate={validate}>
           {({
             values,
             handleChange,
+            errors,
           }) => (
-            <>
+            <Form>
               <Form.Group as={Row} controlId="course-name">
                 <Form.Label column sm="2" md="1">
               Course name
@@ -143,7 +190,9 @@ class CourseSettings extends React.Component {
                     type="text"
                     name="courseName"
                     placeholder="Course Name"
+                    className={cn({ 'error-input': errors && errors.courseName })}
                     onChange={handleChange} />
+                  {errors && errors.courseName && <span className="error-message">{errors.courseName}</span>}
                 </Col>
               </Form.Group>
               <Form.Group as={Row} controlId="course-code">
@@ -164,7 +213,7 @@ class CourseSettings extends React.Component {
             Term
                 </Form.Label>
                 <Col sm="10" md="11">
-                  <Form.Control value={course.termFull} type="text" name="term" readOnly/>
+                  <Form.Control value={values.term} type="text" name="term" readOnly/>
                 </Col>
               </Form.Group>
               <Form.Group as={Row} className="dates">
@@ -174,7 +223,7 @@ class CourseSettings extends React.Component {
                 <Col sm="4" md="5">
                   <Form.Control
                     id="startDate"
-                    value={df(course.bounds.start)}
+                    value={df(values.startDate)}
                     type="text"
                     name="startDate"
                     readOnly/>
@@ -185,7 +234,7 @@ class CourseSettings extends React.Component {
                 <Col sm="4" md="5">
                   <Form.Control
                     id="endDate"
-                    defaultValue={df(course.bounds.end)}
+                    defaultValue={df(values.endDate)}
                     type="text"
                     name="endDate"
                     readOnly/>
@@ -196,12 +245,21 @@ class CourseSettings extends React.Component {
             Time zone
                 </Form.Label>
                 <Col sm="10" md="11">
-                  <Form.Control value={course.timezone} type="text" name="timezone" onChange={handleChange} />
+                  <Form.Control value={values.timezone} type="text" name="timezone" onChange={handleChange} />
                 </Col>
               </Form.Group>
               <hr />
-              <DeleteCourseModal course={course} history={this.props.history} />
-            </>
+              <div className="form-controls">
+                <DeleteCourseModal course={course} history={this.props.history} />
+                <Button
+                  variant="primary"
+                  className={cn({ 'hidden': !hasChanges(values.courseName, values.courseCode, values.timezone) })}
+                  disabled={!isEmpty(errors)}
+                  onClick={() => console.log('submit')}>
+            Save changes
+                </Button>
+              </div>
+            </Form>
           )}
         </Formik>
       </div>
