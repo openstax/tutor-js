e572bc05fd6dd144aa932fc0ccb8bfc70ccf20eb
Nathan Stitt <nathan@stitt.org>
2021-01-29 18:15:03 -0700

    add spec for switching courses display

--- tutor/src/screens/my-courses/index.tsx
+++ tutor/src/screens/my-courses/index.tsx
@@ -4,10 +4,10 @@ import { useHasAnyCourses } from '../../store/courses'
 const MyCourses: React.FC = () => {
     const hasCourses = useHasAnyCourses()
     if (hasCourses) {
-        return <h1>Courses Dashboard</h1>
+        return <h1 data-test-id="existing-teacher-screen">Courses Dashboard</h1>
     }
     return (
-        <h1>new user!</h1>
+        <h1 data-test-id="new-teacher-screen">new user!</h1>
     )
 }
 
