ef9abc48fc6fc14fc455c6962414fbdecdb3f3c7
Alexander Cam Liu <acamliu89@gmail.com>
2021-02-04 17:22:23 -0600

    style the preview course card

--- tutor/src/screens/my-courses/dashboard/preview-course.tsx
+++ tutor/src/screens/my-courses/dashboard/preview-course.tsx
@@ -1,44 +1,62 @@
-import { React, cn } from 'vendor';
+import { React, cn, styled } from 'vendor'
 import { last } from 'lodash'
-import Router from '../../../helpers/router';
-import { Icon } from 'shared';
-import OXFancyLoader from 'shared/components/staxly-animation';
+import Router from '../../../helpers/router'
+import OXFancyLoader from 'shared/components/staxly-animation'
 import { IOfferingWithCourses } from './index'
+import { colors } from 'theme'
+
+const StyledPreviewCourse = styled.div`
+  &&& {
+    a .preview-belt {
+      background: white;
+      box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.2);
+      bottom: 40%;
+      h4 {
+        font-size: 1.8rem;
+        line-height: 2rem;
+        font-weight: 600;
+      }
+      p {
+        color: ${colors.neutral.thin};
+      }
+    }
+  }
+`
 
 const CoursePreview = ({ offeringWithCourses, className } : {offeringWithCourses: IOfferingWithCourses, className: string}) => {
     
   const aCourse = last(offeringWithCourses.courses)
 
   const redirectToCourse = () => {
-    if ( !aCourse.previewCourse ) { return; }
+    if ( !aCourse.previewCourse ) { return }
     this.props.history.push(Router.makePathname(
       'dashboard', { courseId: aCourse.previewCourse.id },
-    ));
+    ))
   }
 
   const onClick = () => {
     if (aCourse.isCreated) {
-      this.redirectToCourse();
+      this.redirectToCourse()
     } else {
-      aCourse.build().then(redirectToCourse());
+      aCourse.build().then(redirectToCourse())
     }
   }
 
   const previewMessage = () => {
     if (aCourse.isBuilding) {
-      return <h4 key="title">Loading Preview</h4>;
+      return <h4 key="title">Loading Preview Course</h4>
     }
     return [
-      <h4 key="title"><Icon type="eye" /> Preview</h4>,
-      <p key="message">Check out a course with assignments and sample data</p>,
-    ];
+      <h4 key="title">Preview Course</h4>,
+      <p key="message">Create test assignments and view sample data</p>,
+    ]
   }
 
   const itemClasses = cn('my-courses-item', 'preview', className, {
   'is-building': aCourse.isBuilding,
-  });
+  })
   return (
-  <div className="my-courses-item-wrapper preview">
+  <StyledPreviewCourse className="my-courses-item-wrapper preview">
       <div
         data-appearance={offeringWithCourses.appearance_code}
         data-test-id="course-card"
@@ -57,8 +75,8 @@ const CoursePreview = ({ offeringWithCourses, className } : {offeringWithCourses
           </div>
       </a>
       </div>
-  </div>
-  );
+  </StyledPreviewCourse>
+  )
 }
 
-export default CoursePreview;
+export default CoursePreview
