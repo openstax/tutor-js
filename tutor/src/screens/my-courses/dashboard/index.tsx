c0d82a8ee3235995accf443c51d7686d2f600ea9
Alexander Cam Liu <acamliu89@gmail.com>
2021-02-04 16:34:58 -0600

    styling the offering courses box, adding the header and adding the button Manage subjects but not functional

--- tutor/src/screens/my-courses/dashboard/index.tsx
+++ tutor/src/screens/my-courses/dashboard/index.tsx
@@ -1,9 +1,11 @@
-import React, { useState, useMemo, ReactElement } from 'react'
+import React, { useState, useMemo } from 'react'
 import { connect } from 'react-redux'
+import { Button } from 'react-bootstrap'
 import { isEmpty, map, compact, filter } from 'lodash'
 import styled from 'styled-components'
 import moment from 'moment'
 import { colors } from 'theme'
+import { Icon } from 'shared';
 import Tabs from '../../../components/tabs'
 import CreateACourse from './create-course'
 import CoursePreview from './preview-course'
@@ -20,16 +22,40 @@ interface MyCoursesDashboardProps {
 }
 
 const StyledMyCoursesDashboard = styled.div`
+    background-color: white;
+    padding: 3.8rem 1.6rem;
+    .controls {
+        display: flex;
+        justify-content: flex-end;
+    }
     .offering-container {
-        margin: 1.6rem;
+        margin: 1.6rem 0 1.6rem 0;
         background-color: ${colors.neutral.bright};
         padding-top: 2.4rem;
-        padding-left: 3.2rem;
+        padding: 2.4rem 3.2rem 6.4rem;
+        min-height: 40rem;
         h2 {
             font-weight: 700;
             font-size: 2.4rem;
             line-height: 3rem;
             color: ${colors.neutral.std};
+        }
+        .course-cards {
+            --gap: 3.2rem;
+            display: inline-flex;
+            flex-wrap: wrap;
+            margin: calc(-1 * var(--gap)) 0 0 calc(-1 * var(--gap));
+            width: calc(100% + var(--gap));
+            > * {
+                margin: var(--gap) 0 0 var(--gap);
+                width: 260px;
+            }
+            .no-courses-message {
+                font-weight: 700;
+                font-size: 1.8rem;
+                color: ${colors.neutral.darker};
+                margin: 7rem auto;
+            }
         }
     }
 `
@@ -37,23 +63,27 @@ const StyledMyCoursesDashboard = styled.div`
 const isCourseCurrent = (course: Course) => moment().isBetween(course.starts_at, course.ends_at)
 const isCoursePast = (course: Course) => moment().isAfter(course.ends_at)
 
-const MyCourses = ({ courses }: {courses: Course[]}) => {
-    return (
-        <>
-        {
-            map(courses, c => (
-                <div key={c.id}><ViewCourse course={c} /></div>
-            ))
-        }
-        </>
-    )
+const PastCourses = ({ courses }: {courses: Course[]}) => {
+    if(courses.length === 0) {
+        return <p className="no-courses-message">No past courses found.</p>
+    }
+    return map(courses, c => (<ViewCourse course={c} key={c.id}/>))
 }
 
-const OfferingBlock = ({ offeringWithCourses }: {offeringWithCourses: IOfferingWithCourses}): ReactElement => {
+const CurrentCourses = ({ courses, offeringWithCourses }: {courses: Course[], offeringWithCourses: IOfferingWithCourses}) => (
+    <>
+        {map(courses, c => (<ViewCourse course={c} key={c.id}/>))}
+        <CoursePreview offeringWithCourses={offeringWithCourses} />
+        <CreateACourse />
+    </>
+)
+
+const OfferingBlock = ({ offeringWithCourses }: {offeringWithCourses: IOfferingWithCourses}) => {
     const [tabIndex, setTabIndex] = useState(0);
 
     const currentCourses = useMemo(() => filter(offeringWithCourses.courses, c => isCourseCurrent(c), offeringWithCourses))
     const pastCourses = useMemo(() => filter(offeringWithCourses.courses, c => isCoursePast(c), offeringWithCourses))
+    const isCurrent = tabIndex === 0
 
     return (
         <div className="offering-container">    
@@ -61,20 +91,20 @@ const OfferingBlock = ({ offeringWithCourses }: {offeringWithCourses: IOfferingW
             <Tabs
               tabs={['CURRENT', 'PAST']}
               onSelect={(a) => setTabIndex(a)}/>
-            <MyCourses courses={tabIndex === 0 ? currentCourses : pastCourses}/>
-            { tabIndex === 0 &&
-            <>
-                <CoursePreview offeringWithCourses={offeringWithCourses} />
-                <CreateACourse />
-            </>
-        }
+              <div className="course-cards">
+                { isCurrent ? <CurrentCourses courses={currentCourses} offeringWithCourses={offeringWithCourses} /> : <PastCourses courses={pastCourses} />}
+              </div>
         </div>
     )
 }
 
-export const MyCoursesDashboard = ({ offeringsWithCourses }: MyCoursesDashboardProps): ReactElement => {
+export const MyCoursesDashboard = ({ offeringsWithCourses }: MyCoursesDashboardProps) => {
     return (
         <StyledMyCoursesDashboard>
+            <h2>My Courses</h2>
+            <div className="controls">
+                <Button variant="link"><Icon type="cog" />Manage subjects</Button>
+            </div>
             { map(offeringsWithCourses, o => <OfferingBlock key={o.id} offeringWithCourses={o} /> )}
         </StyledMyCoursesDashboard>
     )
