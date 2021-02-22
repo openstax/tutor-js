66b96d4eab7303b5f31ade39b6d5532fac3fc6f1
Josiah Ivey <josiah.ivey@gmail.com>
2021-02-22 08:06:17 -0800

    update test id for new teacher screen

--- tutor/src/screens/my-courses/index.tsx
+++ tutor/src/screens/my-courses/index.tsx
@@ -15,7 +15,7 @@ const MyCourses: React.FC = (props) => {
     return <Dashboard />
   }
   return (
-    <NewUser history={props.history} />
+    <NewUser history={props.history} data-test-id="new-teacher-screen" />
   )
 }
 
