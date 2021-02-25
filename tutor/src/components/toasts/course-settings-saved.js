d8b081e9a1768b605d155d325ce89264aab41aab
Nathan Stitt <nathan@stitt.org>
2021-02-25 10:29:52 -0600

    fix indentation

--- tutor/src/components/toasts/course-settings-saved.js
+++ tutor/src/components/toasts/course-settings-saved.js
@@ -1,18 +1,18 @@
 import { React } from 'vendor';
 
 const CourseSettingsSaved = () => {
-  return (
-    <div className="toast success" data-test-id="course-settings-published">
-      <div className="title">
-        <span>Course Settings Saved</span>
-      </div>
-      <div className="body">
-        <p>
+    return (
+        <div className="toast success" data-test-id="course-settings-published">
+            <div className="title">
+                <span>Course Settings Saved</span>
+            </div>
+            <div className="body">
+                <p>
             Your course settings has been successfully saved.
-        </p>
-      </div>
-    </div>
-  );
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default CourseSettingsSaved;
--- tutor/src/screens/course-settings/course-details.js
+++ tutor/src/screens/course-settings/course-details.js
@@ -119,147 +119,147 @@ const StyledCourseDetails = styled.div`
 const df = (d) => moment(d).format('MMM DD, YYYY');
 
 const CourseDetails = observer(({ course, history }) => {
-  const [isSaving, setIsSaving] = useState(false);
+    const [isSaving, setIsSaving] = useState(false);
     
-  const initialValues = {
-    courseName: course.name,
-    courseCode: course.code,
-    term: course.termFull,
-    startDate: course.bounds.start,
-    endDate: course.bounds.end,
-    timezone: course.timezone,
-  };
+    const initialValues = {
+        courseName: course.name,
+        courseCode: course.code,
+        term: course.termFull,
+        startDate: course.bounds.start,
+        endDate: course.bounds.end,
+        timezone: course.timezone,
+    };
 
-  const hasChanges = ({ courseName, courseCode, timezone }) => {
-    const hasCourseNameChanged = initialValues.courseName !== courseName.trim();
-    const hasCourseCodeChanged = initialValues.courseCode !== courseCode.trim();
-    const hasTimezoneChanged = initialValues.timezone !== timezone;
-    return hasCourseNameChanged || hasCourseCodeChanged || hasTimezoneChanged || isSaving;
-  };
+    const hasChanges = ({ courseName, courseCode, timezone }) => {
+        const hasCourseNameChanged = initialValues.courseName !== courseName.trim();
+        const hasCourseCodeChanged = initialValues.courseCode !== courseCode.trim();
+        const hasTimezoneChanged = initialValues.timezone !== timezone;
+        return hasCourseNameChanged || hasCourseCodeChanged || hasTimezoneChanged || isSaving;
+    };
 
-  const validate = (values) => {
-    const errors = {};
-    if(!values.courseName.trim())
-      errors.courseName = 'Required';
-    return errors;
-  };
+    const validate = (values) => {
+        const errors = {};
+        if(!values.courseName.trim())
+            errors.courseName = 'Required';
+        return errors;
+    };
 
-  const onSubmit = ({ courseName, courseCode, timezone }) => {
-    setIsSaving(true);
-    course.name = courseName;
-    course.code = courseCode;
-    course.timezone = timezone;
-    course.save().then(() => {
-      setIsSaving(false);
-      Toasts.push({ handler: 'courseSettingsSaved', status: 'ok' });
-    });
-  };
+    const onSubmit = ({ courseName, courseCode, timezone }) => {
+        setIsSaving(true);
+        course.name = courseName;
+        course.code = courseCode;
+        course.timezone = timezone;
+        course.save().then(() => {
+            setIsSaving(false);
+            Toasts.push({ handler: 'courseSettingsSaved', status: 'ok' });
+        });
+    };
 
-  return (
-    <StyledCourseDetails>
-      <Formik initialValues={initialValues} validate={validate}>
-        {({
-          setFieldValue,
-          values,
-          handleChange,
-          errors,
-        }) => (
-          <Form className="course-detail-settings-form">
-            <Form.Group as={Row} controlId="course-name">
-              <Form.Label column sm="2" md="1">
+    return (
+        <StyledCourseDetails>
+            <Formik initialValues={initialValues} validate={validate}>
+                {({
+                    setFieldValue,
+                    values,
+                    handleChange,
+                    errors,
+                }) => (
+                    <Form className="course-detail-settings-form">
+                        <Form.Group as={Row} controlId="course-name">
+                            <Form.Label column sm="2" md="1">
               Course name
-              </Form.Label>
-              <Col sm="10" md="11">
-                <Form.Control
-                  value={values.courseName}
-                  type="text"
-                  name="courseName"
-                  placeholder="Course Name"
-                  className={cn({ 'error-input': errors && errors.courseName })}
-                  onChange={handleChange}
-                  disabled={isSaving} />
-                {errors && errors.courseName && <span className="error-message">{errors.courseName}</span>}
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId="course-code">
-              <Form.Label column sm="2" md="1">
+                            </Form.Label>
+                            <Col sm="10" md="11">
+                                <Form.Control
+                                    value={values.courseName}
+                                    type="text"
+                                    name="courseName"
+                                    placeholder="Course Name"
+                                    className={cn({ 'error-input': errors && errors.courseName })}
+                                    onChange={handleChange}
+                                    disabled={isSaving} />
+                                {errors && errors.courseName && <span className="error-message">{errors.courseName}</span>}
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="course-code">
+                            <Form.Label column sm="2" md="1">
             Course code
-              </Form.Label>
-              <Col sm="10" md="11">
-                <Form.Control
-                  value={values.courseCode}
-                  type="text"
-                  name="courseCode"
-                  placeholder="SOC-101. Optional"
-                  onChange={handleChange}
-                  disabled={isSaving} />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId="term">
-              <Form.Label column sm="2" md="1">
+                            </Form.Label>
+                            <Col sm="10" md="11">
+                                <Form.Control
+                                    value={values.courseCode}
+                                    type="text"
+                                    name="courseCode"
+                                    placeholder="SOC-101. Optional"
+                                    onChange={handleChange}
+                                    disabled={isSaving} />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="term">
+                            <Form.Label column sm="2" md="1">
             Term
-              </Form.Label>
-              <Col sm="10" md="11">
-                <Form.Control value={values.term} type="text" name="term" readOnly/>
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} className="dates">
-              <Form.Label column sm="2" md="1" htmlFor="startDate">
+                            </Form.Label>
+                            <Col sm="10" md="11">
+                                <Form.Control value={values.term} type="text" name="term" readOnly/>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="dates">
+                            <Form.Label column sm="2" md="1" htmlFor="startDate">
             Start date
-              </Form.Label>
-              <Col sm="4" md="5">
-                <Form.Control
-                  id="startDate"
-                  value={df(values.startDate)}
-                  type="text"
-                  name="startDate"
-                  readOnly/>
-              </Col>
-              <Form.Label column sm="2" md="1" className="end-date-label" htmlFor="endDate">
+                            </Form.Label>
+                            <Col sm="4" md="5">
+                                <Form.Control
+                                    id="startDate"
+                                    value={df(values.startDate)}
+                                    type="text"
+                                    name="startDate"
+                                    readOnly/>
+                            </Col>
+                            <Form.Label column sm="2" md="1" className="end-date-label" htmlFor="endDate">
             End date
-              </Form.Label>
-              <Col sm="4" md="5">
-                <Form.Control
-                  id="endDate"
-                  defaultValue={df(values.endDate)}
-                  type="text"
-                  name="endDate"
-                  readOnly/>
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId="time-zone">
-              <Col sm="2" md="1" className="timezone-label">
-                <span>Time zone</span>
-              </Col>
-              <Col sm="10" md="11">
-                <Timezone
-                  value={values.timezone}
-                  onChange={(timezone) => setFieldValue('timezone', timezone)}
-                  disabled={isSaving}/>
-              </Col>
-            </Form.Group>
-            <hr />
-            <div className="form-controls">
-              <DeleteCourseModal course={course} history={history} />
-              <AsyncButton
-                className={cn('save-changes-button', { 'hidden': !hasChanges(values) })}
-                disabled={!isEmpty(errors)}
-                onClick={() => onSubmit(values)}
-                isWaiting={isSaving}
-                waitingText="Saving..."
-              >
+                            </Form.Label>
+                            <Col sm="4" md="5">
+                                <Form.Control
+                                    id="endDate"
+                                    defaultValue={df(values.endDate)}
+                                    type="text"
+                                    name="endDate"
+                                    readOnly/>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="time-zone">
+                            <Col sm="2" md="1" className="timezone-label">
+                                <span>Time zone</span>
+                            </Col>
+                            <Col sm="10" md="11">
+                                <Timezone
+                                    value={values.timezone}
+                                    onChange={(timezone) => setFieldValue('timezone', timezone)}
+                                    disabled={isSaving}/>
+                            </Col>
+                        </Form.Group>
+                        <hr />
+                        <div className="form-controls">
+                            <DeleteCourseModal course={course} history={history} />
+                            <AsyncButton
+                                className={cn('save-changes-button', { 'hidden': !hasChanges(values) })}
+                                disabled={!isEmpty(errors)}
+                                onClick={() => onSubmit(values)}
+                                isWaiting={isSaving}
+                                waitingText="Saving..."
+                            >
                 Save changes
-              </AsyncButton>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </StyledCourseDetails>
-  );
+                            </AsyncButton>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </StyledCourseDetails>
+    );
 });
 CourseDetails.propTypes = {
-  course: PropTypes.instanceOf(Course).isRequired,
-  history: PropTypes.object.isRequired,
+    course: PropTypes.instanceOf(Course).isRequired,
+    history: PropTypes.object.isRequired,
 };
 
 export default CourseDetails;
