6c68ce1b2970c05cbab71dafede6542ee5bd534b
Alexander Cam Liu <acamliu89@gmail.com>
2021-03-04 16:29:25 -0600

    running the student dashboard test with the different screen size

--- tutor/specs/e2e/helpers.ts
+++ tutor/specs/e2e/helpers.ts
@@ -1,8 +1,7 @@
 import { Page } from 'playwright-core'
 import fetch from 'node-fetch'
-import { forEach } from 'lodash'
 
-const SCREENS = {
+export const SCREENS = {
     mobile: [375,667], // iphone
     tablet: [768,1024], // ipad
     desktop: [1280, 1024], // pretty much anything is larger than this
@@ -27,15 +26,6 @@ export const setTimeouts = async () => {
     const TIMEOUT = testConfig.DEBUG ? 90 : 30
     context.setDefaultTimeout(TIMEOUT * 1000)
 }
-
-export const withScreenSizes = (test: () => Promise<void>, sizes = SCREENS) => {
-    forEach(sizes, async size => {
-        const [w,h] = size;
-        await page.setViewportSize({ width: w, height: h })
-        await test()
-        await page.setViewportSize({ width: SCREENS.desktop[0], height: SCREENS.desktop[1] })
-    })
-};
 
 export const disableTours = async () => {
     await page.evaluate(() => {
--- tutor/specs/e2e/student-dashboard.e2e.ts
+++ tutor/specs/e2e/student-dashboard.e2e.ts
@@ -1,31 +1,5 @@
-import { visitPage, disableTours, setRole, setTimeouts } from './helpers'
-
-// const setupDashboard = () => {
-//     cy.setRole('student')
-//     cy.visit('/course/2');
-//     cy.disableTours();
-// }
-
-// context('Dashboard', () => {
-
-//     afterEach(() => {
-//         cy.setRole('teacher');
-//     })
-  
-//     it('switches tabs', () => {
-//         setupDashboard();
-//         cy.getTestElement('all-past-work-tab').click()
-//     });
-
-//     it('loads assignments', () => {
-//         withScreenSizes(() => {
-//             setupDashboard();
-//             cy.getTestElement('all-past-work-tab').click()
-//             cy.get('.task.homework').first().click()
-//             cy.getTestElement('student-task').should('exist')
-//         })
-//     })
-// });
+import { visitPage, disableTours, setRole, setTimeouts, SCREENS } from './helpers'
+import { forEach } from 'lodash'
 
 describe('Student Dashboard', () => {
     beforeEach(async () => {
@@ -39,15 +13,22 @@ describe('Student Dashboard', () => {
         await page.click('testEl=all-past-work-tab')
     });
 
-    it.only('loads assignments', async () => {
-        await page.setViewportSize({ width: 375, height: 667 })
-        await page.click('testEl=all-past-work-tab');
-        await page.click('.task.homework');
-        await expect(page).toHaveSelector('testEl=student-task')
-        // withScreenSizes(async () => {
-        //     await page.click('testEl=all-past-work-tab');
-        //     await page.click('.task.homework');
-        //     await expect(page).toHaveSelector('testEl=student-task')
-        // })
+    //https://github.com/facebook/jest/issues/1619
+    forEach(SCREENS, (screen, type) => {
+        const [width, height] = screen
+
+        it(`loads assignment with screen width: ${width} and height: ${height}`, async () => {
+            await page.setViewportSize({ width, height })
+            await page.click('testEl=all-past-work-tab');
+            if(type === 'mobile') {
+                await page.click(':nth-match(.task.homework a, 1)');
+            }
+            else {
+                await page.click(':nth-match(.task.homework, 1)');
+            }
+            await expect(page).toHaveSelector('testEl=student-task')
+        })
     })
+
+
 })
