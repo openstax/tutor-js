2533d47cc978e29717b21a3accc87610b5498162
Alexander Cam Liu <acamliu89@gmail.com>
2021-03-05 10:14:34 -0600

    add student-task playwright tests

--- configs/test/playwright.env.js
+++ configs/test/playwright.env.js
@@ -5,59 +5,60 @@ const chalk = require('chalk');
 
 const createDataTestIdEngine = () => {
 
-  const toTestSelector = (sel) => {
-    const quoted = sel.match(/^".*"$/) ? sel : `"${sel}"`;
-    return `[data-test-id=${quoted}]`;
-  };
+    const toTestSelector = (sel) => {
+        const quoted = sel.match(/^".*"$/) ? sel : `"${sel}"`;
+        console.log(quoted)
+        return `[data-test-id=${quoted}]`;
+    };
 
-  return {
+    return {
     // Creates a selector that matches given target when queried at the root.
     // Can return undefined if unable to create one.
-    create(root, target) {
-      const testId = target.getAttribute('data-test-id');
-      return (testId && root.querySelector(toTestSelector(testId)) === target) ? testId : undefined;
-    },
+        create(root, target) {
+            const testId = target.getAttribute('data-test-id');
+            return (testId && root.querySelector(toTestSelector(testId)) === target) ? testId : undefined;
+        },
 
-    // Returns the first element matching given selector in the root's subtree.
-    query(root, selector) {
-      return root.querySelector(toTestSelector(selector));
-    },
+        // Returns the first element matching given selector in the root's subtree.
+        query(root, selector) {
+            return root.querySelector(toTestSelector(selector));
+        },
 
-    // Returns all elements matching given selector in the root's subtree.
-    queryAll(root, selector) {
-      return Array.from(root.querySelectorAll(toTestSelector(selector)));
-    },
-  };
+        // Returns all elements matching given selector in the root's subtree.
+        queryAll(root, selector) {
+            return Array.from(root.querySelectorAll(toTestSelector(selector)));
+        },
+    };
 };
 
 class CustomEnvironment extends PlaywrightEnvironment {
 
-  async setup() {
-    await super.setup();
-    await playwright
-      .selectors
-      .register('testEl', createDataTestIdEngine)
-      .catch((e) => {
-        if (!e.toString().includes('has been already')) {
-          throw e;
-        }
-      });
-  }
-
-  async teardown() {
-    await super.teardown();
-  }
-
-  async handleTestEvent(event) {
-    if (event.name === 'test_done' && event.test.errors.length > 0) {
-      const parentName = event.test.parent.name.replace(/\W/g, '-');
-      const specName = event.test.name.replace(/\W/g, '-');
-      const path = `screenshots/tutor/${parentName}_${specName}.png`;
-      // eslint-disable-next-line no-console
-      console.log(chalk.red(`writing screenshot to: ${path}`));
-      await this.global.page.screenshot({ path });
+    async setup() {
+        await super.setup();
+        await playwright
+            .selectors
+            .register('testEl', createDataTestIdEngine)
+            .catch((e) => {
+                if (!e.toString().includes('has been already')) {
+                    throw e;
+                }
+            });
     }
-  }
+
+    async teardown() {
+        await super.teardown();
+    }
+
+    async handleTestEvent(event) {
+        if (event.name === 'test_done' && event.test.errors.length > 0) {
+            const parentName = event.test.parent.name.replace(/\W/g, '-');
+            const specName = event.test.name.replace(/\W/g, '-');
+            const path = `screenshots/tutor/${parentName}_${specName}.png`;
+            // eslint-disable-next-line no-console
+            console.log(chalk.red(`writing screenshot to: ${path}`));
+            await this.global.page.screenshot({ path });
+        }
+    }
 }
 
 
--- tutor/specs/e2e/student-task.e2e.ts
+++ tutor/specs/e2e/student-task.e2e.ts
@@ -8,39 +8,25 @@ describe('Student Tasks', () => {
         setRole('student')
     });
 
-    // eslint-disable-next-line
-//   const submitAnswer = () => {
-    //         cy.get('.exercise-step').then(st => {
-    //             const fr = st.find('[data-test-id="free-response-box"]')
-    //             if (fr.length > 0) {
-    //                 cy.wrap(fr).type('this is a answer answering and fully explaining my reasoning for the question')
-    //                 cy.getTestElement('submit-answer-btn').click()
-    //             }
-    //         })
-    //         cy.getTestElement('answer-choice-a').click()
-    //         cy.getTestElement('submit-answer-btn').click()
-    //         cy.getTestElement('continue-btn').click()
-    //     }
-
-    // const longFreeResponseAnswer = ' dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius' + 
-    // 'duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas' + 
-    // 'purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at varius vel pharetra vel turpis nunc eget lorem dolor sed viverra ipsum nunc' + 
-    // 'aliquet bibendum enim facilisis gravida neque convallis a cras semper auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor consequat id porta nibh venenatis cras sed' + 
-    // 'felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis' + 
-    // 'tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt' + 
-    // 'eget nullam non nisi est sit amet facilisis magna etiam tempor orci eu lobortis elementum nibh tellus molestie nunc non blandit massa enim nec dui nunc mattis enim ut tellus elementum sagittis' + 
-    // 'vitae et leo duis ut diam quam nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum' + 
-    // 'sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis' + 
-    // 'volutpat est velit egestas dui id ornare arcu odio ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris' + 
-    // 'commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor' + 
-    // 'lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sit amet aliquam' +
-    // 'id diam maecenas ultricies mi eget mauris pharetra et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna' + 
-    // 'neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh' + 
-    // 'cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut placerat' +
-    // 'orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque' +
-    // 'massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis' + 
-    // 'risus sed vulputate odio ut enim blandit volutpat maecenas olutpatv blandit aliquam etiam risus sed vulputate odio ut enim blandit volutpat maecenas olutpatv blandit aliquam etiam landit volutpat' +
-    // 'maecenas olutpatv blandit aliquam etiam maecenas olutpatv blandit aliquam etiam'
+    const longFreeResponseAnswer = ' dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius' + 
+    'duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas' + 
+    'purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at varius vel pharetra vel turpis nunc eget lorem dolor sed viverra ipsum nunc' + 
+    'aliquet bibendum enim facilisis gravida neque convallis a cras semper auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor consequat id porta nibh venenatis cras sed' + 
+    'felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis' + 
+    'tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt' + 
+    'eget nullam non nisi est sit amet facilisis magna etiam tempor orci eu lobortis elementum nibh tellus molestie nunc non blandit massa enim nec dui nunc mattis enim ut tellus elementum sagittis' + 
+    'vitae et leo duis ut diam quam nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum' + 
+    'sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis' + 
+    'volutpat est velit egestas dui id ornare arcu odio ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris' + 
+    'commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor' + 
+    'lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sit amet aliquam' +
+    'id diam maecenas ultricies mi eget mauris pharetra et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna' + 
+    'neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh' + 
+    'cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut placerat' +
+    'orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque' +
+    'massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis' + 
+    'risus sed vulputate odio ut enim blandit volutpat maecenas olutpatv blandit aliquam etiam risus sed vulputate odio ut enim blandit volutpat maecenas olutpatv blandit aliquam etiam landit volutpat' +
+    'maecenas olutpatv blandit aliquam etiam maecenas olutpatv blandit aliquam etiam'
 
     it('advances after answering a free-response only question', async () => {
         await visitPage(page, '/course/1/task/3') // task id 3 is a hardcoded WRM task
@@ -55,7 +41,7 @@ describe('Student Tasks', () => {
         
     })
 
-    it.only('can change and re-submit answers to questions', async () => {
+    it('can change and re-submit answers to questions', async () => {
         await visitPage(page, '/course/1/task/2')
         //Verify: after clicking on a two-step question, the two step question icon shows up, moving all the data-step-index by 1
         await page.click('.sticky-table [data-step-index="3"]')
@@ -88,63 +74,32 @@ describe('Student Tasks', () => {
         await page.click('testEl=continue-btn')
     })
   
-    // it('should show late clock icon and the late points info, if task step is late', () => {
-    //     cy.visit('/course/1/task/4')
-    //     cy.get('[data-test-id="late-icon"]').should('exist')
-    //     cy.get('.isLateCell').first().trigger('mouseover').then(() => {
-    //         cy.get('[data-test-id="late-info-points-table"]').should('exist')
-    //     })
-    // })
+    it('should show late clock icon and the late points info, if task step is late', async () => {
+        await visitPage(page, '/course/1/task/4')
+        await expect(page).toHaveSelector('testEl=late-icon')
+        await page.hover(':nth-match(.isLateCell, 1)')
+        await expect(page).toHaveSelector('testEl=late-info-points-table')
+    })
 
-    // it('should show word limit error message and disable submit button if response is over 500 words', () => {
-    //     cy.visit('/course/1/task/3')
-    //     cy.getTestElement('student-free-response').should('exist')
-    //     cy.getTestElement('free-response-box').type(longFreeResponseAnswer, { delay: 0 })
-    //     cy.get('.word-limit-error-info').should('exist')
-    //     cy.getTestElement('submit-answer-btn').should('be.disabled')
-    // })
+    it('should show word limit error message and disable submit button if response is over 500 words', async () => {
+        await visitPage(page, '/course/1/task/3') // task id 3 is a hardcoded WRM task
+        await expect(page).toHaveSelector('testEl=student-free-response')
+        await page.type('testEl=free-response-box', longFreeResponseAnswer)
+        await expect(page).toHaveSelector('.word-limit-error-info')
+        await expect(page).toHaveSelector('testEl=submit-answer-btn-disabled')
+    })
 
-    // it.only('should be able to save question to my practice', () => {
+    it('should be able to save question to my practice', async () => {
+        await visitPage(page, '/course/1/task/2') 
+        await page.click('.sticky-table [data-step-index="4"]')
+        // start fresh - deleting the practice questions from course
+        await page.evaluate(() => {
+            window._MODELS.courses.get(1).practiceQuestions.clear()
+        })
 
-    //     cy.visit('/course/1/task/2')
-    //     cy.get('.sticky-table [data-step-index=3]').click({ force: true })
-
-
-    //     cy.get('.exercise-step').then(st => {
-    //         // reset practice question state
-    //         cy.window().then(w => w._MODELS.courses.get(1).practiceQuestions.clear())
-    //         const fr = st.find('[data-test-id="free-response-box"]')
-    //         if (fr.length > 0){
-    //             cy.wrap(fr).type('test')
-    //             cy.getTestElement('submit-answer-btn').click()
-    //         }
-    //     })
-    //     cy.getTestElement('save-practice-button').should('exist');
-    //     cy.getTestElement('save-practice-button').should('have.text', 'Save to practice');
-    // // cy.getTestElement('save-practice-button').click();
-    // // cy.getTestElement('save-practice-button').should('have.text', 'Remove from practice');
-    // })
-
-    // it('deals with steps being removed', () => {
-    //     const taskId = 8
-    //     cy.visit(`/course/1/task/${taskId}`)
-    // FIXME - this has infinite loop and eventually crashes cypress
-    
-    // cy.get('.task-homework').then(card => {
-    //   let btn = card.find('[data-test-id="value-prop-continue-btn"]')
-    //   while(btn.length > 0) {
-    //     cy.wrap(btn).click()
-    //     btn = card.find('[data-test-id="value-prop-continue-btn"]')
-    //   }
-    // })
-    //    submitAnswer()
-
-    // cy.window().then(win => {
-    //   win._MODELS.courses.get(1).studentTasks.get(taskId).steps[3].type = 'placeholder'
-    // })
-    // range(2).forEach(submitAnswer)
-    // cy.getTestElement('individual-review-intro-value-prop').should('exist')
-    // cy.getTestElement('value-prop-continue-btn').click()
-    // cy.get('.exercise-step').should('exist')
-    //})
+        await page.type('.exercise-step [data-test-id="free-response-box"]', 'why do i need to fill this out?')
+        await page.click('testEl=submit-answer-btn')
+        await expect(page).toHaveSelector('testEl=save-practice-button')
+        await expect(page).toHaveText('testEl=save-practice-button', 'Save to practice')
+    })
 })
