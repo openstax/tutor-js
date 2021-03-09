6d5192930b2342f02a4e4b4aef04e92bfe63b637
Nathan Stitt <nathan@stitt.org>
2021-03-09 17:46:36 -0600

    revert erroneous port change

--- tutor/jest-playwright.config.js
+++ tutor/jest-playwright.config.js
@@ -1,4 +1,3 @@
-const httpPort = process.env.FRONTEND_PORT || 8111;
 const serverPort = process.env.SERVER_PORT || 8110;
 
 const DEBUG = Boolean(process.env.DEBUG || false);
@@ -15,7 +14,7 @@ if (!process.env.EXTERNAL_SERVER) {
         command: 'node tutor/scripts/test-server/index.js',
         basePath: '/dist/tutor.js',
         launchTimeout: 5 * 60 * 1000,
-        port: httpPort,
+        port: serverPort,
         protocol: 'http',
     };
 }
