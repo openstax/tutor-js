af72cad8dd6e261ae724a8fd28fd4a715ffb1a0f
Alexander Cam Liu <acamliu89@gmail.com>
2021-03-12 14:48:23 -0600

    fix pre-filled offering test

--- tutor/src/screens/new-course/ux.js
+++ tutor/src/screens/new-course/ux.js
@@ -92,9 +92,7 @@ export default class CourseBuilderUX extends BaseModel {
 
   @action.bound onOfferingsAvailable() {
       if (this.preselectedOfferingId) {
-          this.newCourse.offering = find(this.offerings.available.array,
-              { id: this.preselectedOfferingId }
-          );
+          this.newCourse.offering = find(this.offerings.available.array, a => a.id == this.preselectedOfferingId );
       }
   }
 
