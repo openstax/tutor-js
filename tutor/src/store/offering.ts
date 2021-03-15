0255c315bd3bcd50537182b551f0b62ffd108da7
Alexander Cam Liu <acamliu89@gmail.com>
2021-03-15 16:32:49 -0500

    sort the subject blocks by the BE sorting, not by the courses sorting

--- tutor/src/store/offering.ts
+++ tutor/src/store/offering.ts
@@ -1,11 +1,12 @@
 import { createSlice, createEntityAdapter } from '@reduxjs/toolkit'
 import { useSelector } from 'react-redux'
+import { union } from 'lodash'
 import { bootstrap } from './bootstrap'
 import { Offering, Course, ID } from './types'
+import UiSettings from 'shared/model/ui-settings'
 
-const offeringAdapter = createEntityAdapter<Offering>({
-    sortComparer: (a, b) => a.default_course_name.localeCompare(b.default_course_name),
-})
+//sorting is done in the BE
+const offeringAdapter = createEntityAdapter<Offering>()
 
 const initialState = offeringAdapter.getInitialState()
 interface OfferingSlice { offerings: typeof initialState }
@@ -27,12 +28,18 @@ export const useAllOfferings = () => useSelector<OfferingSlice, Offering[]>(stat
     return selectors.selectAll(state)
 })
 
-export const useAvailableOfferings = ( courses: Course[] = []) => (
+export const useAvailableOfferings = (courses: Course[] = []) => (
     useAllOfferings().filter(o => o.is_available || o.is_preview_available || courses.find(c => c.offering_id == o.id))
 )
 
 export const useOfferingForId = (offeringId: ID) => useSelector<OfferingSlice, Offering | undefined>(state => {
     return selectors.selectById(state, offeringId)
 })
+
+export const useDisplayedOfferingsIds = (courses: Course[] = []) => {
+    const defaultOfferingIds = useAllOfferings().filter(o => courses.find(c => c.offering_id == o.id)).map(fo => fo.id)
+    const selectedIds = UiSettings.get('displayedOfferingIds') || []
+    return union(selectedIds, defaultOfferingIds)
+}
 
 export const offeringsReducer = offeringSlice.reducer
