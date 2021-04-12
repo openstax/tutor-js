804bfc46bb05713cecd4d0cfd90cffa1d606cc15
Josiah Ivey <josiah.ivey@gmail.com>
2021-04-12 10:24:27 -0700

    fix some assignment type issues

--- tutor/specs/e2e/assignment-grade.e2e.ts
+++ tutor/specs/e2e/assignment-grade.e2e.ts
@@ -7,6 +7,7 @@ describe('Assignment Grade', () => {
     const PLAN_SETTINGS = {
         1: { type: 'reading' },
         2: { type: 'homework' },
+        3: { type: 'homework-wrm' },
     }
     let GRADES = {}
 
@@ -22,7 +23,7 @@ describe('Assignment Grade', () => {
                 mock.data.plan(planId).type.match(/homework/) ? times(16).map((id) => mock.data.exercise(id)) : []
             ),
             exercise: (id) => (
-                Factory.create(Number(id) % 3 == 0 ? 'OpenEndedTutorExercise' : 'TutorExercise', { id })
+                Factory.create('OpenEndedTutorExercise', { id })
             ),
             scores: (id, mock) => (
                 Factory.create('TaskPlanScores', {
@@ -39,8 +40,17 @@ describe('Assignment Grade', () => {
                 return merge({}, mock.data.plan(id), request.postDataJSON())
             },
             '/api/plans/:id/scores*': async ({ mock, params: { id } }) => {
-                const _scores = mock.data.scores(id)
+                // const _scores = mock.data.scores(id)
+                const _scores = Factory.create('TaskPlanScores', {
+                    task_plan: mock.data.plan(id),
+                    grades: {},
+                    course: mock.course(COURSE_ID),
+                    exercises: mock.data.exercises(id),
+                })
                 _scores.tasking_plans.forEach((tp: any) => {
+                    // tp.question_headings.forEach((qh: any) => {
+                    //     qh.question_id = qh.exercise.content.questions[0].id
+                    // })
                     tp.students.forEach((s: any) => {
                         s.questions.forEach((q: any) => {
                             const grade = GRADES[q.task_step_id]
@@ -76,13 +86,14 @@ describe('Assignment Grade', () => {
     })
 
     it('loads and views questions', async () => {
-        await visitPage(page, `/course/${COURSE_ID}/assignment/grade/2`)
-        await expect(page).toHaveSelector('testEl=question-1')
+        await visitPage(page, `/course/${COURSE_ID}/assignment/grade/3`)
+        await expect(page).toHaveSelector('testEl=question-0')
+        // await page.waitForTimeout(9999100)
     })
 
     it('changes focused student once graded', async () => {
-        await visitPage(page, `/course/${COURSE_ID}/assignment/grade/2`)
-        await page.click('testEl=question-1')
+        await visitPage(page, `/course/${COURSE_ID}/assignment/grade/3`)
+        await page.click('testEl=question-0')
         await expect(page).toHaveSelector('testEl=student-answer')
         await page.type('[data-test-id=student-answer] input[name=score]', '0.9')
         await page.type('[data-test-id=student-answer] textarea[name=comment]', 'i like this answer a lot!')
