ae6a5a89c644e19e8a51c594162ca4589c408c2c
Josiah Ivey <josiah.ivey@gmail.com>
2021-04-16 18:24:25 -0700

    update more navbar components

--- tutor/specs/components/navbar/account-link.spec.js
+++ tutor/specs/components/navbar/account-link.spec.js
@@ -1,17 +1,16 @@
 import Link from '../../../src/components/navbar/account-link';
-
-import User from '../../../src/models/user';
-jest.mock('../../../src/models/user', () => ({ }));
+import { runInAction } from '../../helpers';
+import { currentUser } from '../../../src/models';
 
 describe('Account Link', function() {
     it('only renders if account profile_url is present', function() {
-        User.profile_url = undefined;
+        runInAction(() => currentUser.profile_url = undefined);
         const link = shallow(<Link />);
         expect(link.html()).toBeNull();
     });
 
     it('renders link with profile url and target set to _blank', function() {
-        User.profile_url = 'a.test.url';
+        runInAction(() => currentUser.profile_url = 'a.test.url');
         const link = shallow(<Link />);
         expect(link).toHaveRendered('[href="a.test.url"][target="_blank"]');
     });
