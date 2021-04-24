1d88bd1a7e6a249114d29f3be50585d6203025b2
Nathan Stitt <nathan@stitt.org>
2021-04-24 10:21:56 -0500

    lock time zone

--- tutor/specs/helpers/time-mock.ts
+++ tutor/specs/helpers/time-mock.ts
@@ -1,6 +1,7 @@
 import Time from 'shared/model/time'
 import MockDate from 'mockdate';
-
+// @ts-ignore
+import moment from 'moment-timezone';
 import { partial } from 'lodash';
 import FactoryBot from 'object-factory-bot';
 
@@ -14,11 +15,16 @@ const TimeMock = {
     },
 
     mock(now: Time) {
-        // const tz  = 'America/Chicago';
+        const tz  = 'America/Chicago';
+        Time.defaultZoneName = tz;
         MockDate.set(now.asDate, -360);
         FactoryBot.defaults!.now = now.asDate
         const spy = jest.spyOn(Time, 'now', 'get');
         spy.mockImplementation(() => now);
+
+        jest.spyOn(moment.tz, 'guess').mockImplementation(() => tz);
+        moment.tz.setDefault(tz);
+        moment.locale('en');
     },
 
     restore() {
