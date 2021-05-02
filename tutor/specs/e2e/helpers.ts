94755d5d69796b146725914da8e9ac8210b2ab81
Nathan Stitt <nathan@stitt.org>
2021-05-02 09:53:00 -0500

    fix my-courses test

--- tutor/specs/e2e/helpers.ts
+++ tutor/specs/e2e/helpers.ts
@@ -1,8 +1,13 @@
 import { Page } from 'playwright-core'
 import Factory from 'object-factory-bot'
 import { forEach } from 'lodash'
-import { BootstrapData } from '../../src/models/types'
-import fetch from 'node-fetch'
+import * as cp from 'child_process'
+import * as faker from 'faker';
+
+export { faker }
+
+// import { BootstrapData } from '../../src/models/types'
+//import fetch from 'node-fetch'
 import '../factories/definitions'
 export * from './mocker'
 
@@ -18,38 +23,100 @@ export const visitPage = async (page: Page, path: string) => {
 }
 
 // the test endpoint doesn't yet support teacher-student role
-export type Roles = 'teacher' | 'student'
+// export type Roles = 'teacher' | 'student'
 
-export const setRole = async (role: Roles): Promise<boolean> => {
-    const resp = await fetch(`${testConfig.API_URL}/setrole?role=${role}`)
-    return resp.ok
-}
+// export const setRole = async (role: Roles): Promise<boolean> => {
+//     const resp = await fetch(`${testConfig.API_URL}/setrole?role=${role}`)
+//     return resp.ok
+// }
 
 // use with care!  jest runs specs multi-process;
 // calling this in one spec may reset the state for another spec
 // while it's running
-export const resetState = async () => {
-    const resp = await fetch(`${testConfig.API_URL}/resetState`)
-    return resp.ok
-}
+// export const resetState = async () => {
+//     const resp = await fetch(`${testConfig.API_URL}/resetState`)
+//     return resp.ok
+// }
 
-// IMPORTANT! the page is shared between all specs in file.
-// you must call 'await jestPlaywright.resetPage()' in a beforeEach if
-// different specs attempt to set different data in the same test file
-type bootstrapModifierFn = (data: BootstrapData) => BootstrapData // eslint-disable-line
-export const modifyBootstrapData = async (page: Page, cb: bootstrapModifierFn) => {
-    await page.route(/bootstrap/, async (route) => {
-        const req = await fetch(`${testConfig.API_URL}/user/bootstrap`)
-        const bs = await req.json() as BootstrapData
-        route.fulfill({
-            body: JSON.stringify(cb(bs)),
-            headers: {
-                'x-app-date': (new Date()).toISOString(),
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json',
-            },
+// // IMPORTANT! the page is shared between all specs in file.
+// // you must call 'await jestPlaywright.resetPage()' in a beforeEach if
+// // different specs attempt to set different data in the same test file
+// type bootstrapModifierFn = (data: BootstrapData) => BootstrapData // eslint-disable-line
+// export const modifyBootstrapData = async (page: Page, cb: bootstrapModifierFn) => {
+//     await page.route(/bootstrap/, async (route) => {
+//         const req = await fetch(`${testConfig.URL}/user/bootstrap`)
+//         const bs = await req.json() as BootstrapData
+//         route.fulfill({
+//             body: JSON.stringify(cb(bs)),
+//             headers: {
+//                 'x-app-date': (new Date()).toISOString(),
+//                 'Access-Control-Allow-Origin': '*',
+//                 'Content-Type': 'application/json',
+//             },
+//         })
+//     })
+// }
+
+
+export const execRailsCmd = async (irb: string) => {
+    return new Promise((result, reject) => {
+        const cmd = `docker exec tutor-server_rails_1 bin/rails r "${irb}"`
+        cp.exec(cmd, (error, stdout) => {
+            if (error) {
+                console.warn(cmd, error, stdout) // eslint-disable-line
+                reject(error)
+            }
+            result(stdout)
         })
     })
+
+}
+
+interface AccountOptions {
+    page: Page,
+    role?: 'instructor' | 'student' | 'unknown_role',
+    userName: string
+    isVerified?: boolean
+    approvedSchool?: boolean
+    agreeToTerms?: boolean
+}
+
+export const findOrCreateTeacherAccount = async ({
+    page, userName,
+    agreeToTerms = true,
+    role = 'instructor',
+    isVerified = true,
+    approvedSchool = true,
+}: AccountOptions) => {
+    const returnedId = await execRailsCmd(
+        `puts User::FindOrCreateUser[
+username:'${userName}',
+role:'${role}',
+faculty_status:'${isVerified ? 'confirmed_faculty' : 'no_faculty_info'}',
+school_type:'${approvedSchool ? 'college' : 'unknown_school_type' }',
+school_location:'domestic_school'].id`
+    )
+    const userId = Number(returnedId)
+    if (agreeToTerms) {
+        await execRailsCmd(`
+            pr=OpenStax::Accounts::Account.find(${userId}).profile;
+            FinePrint.sign_contract(pr,:general_terms_of_use);
+            FinePrint.sign_contract(pr,:privacy_policy)
+        `)
+    }
+    await loginAs({ page, userName })
+    return userId
+}
+
+
+export const loginAs = async ({ page, userName } : { page: Page, userName: string }) => {
+    const userMenu = await page.$('#user-menu')
+    if (userMenu) {
+        await userMenu.click()
+        await page.click('.logout [type=submit]')
+    }
+    await page.goto(`${testConfig.URL}/accounts/dev/accounts`)
+    await page.click(`text=${userName}`)
 }
 
 export const setTimeouts = async () => {
--- tutor/specs/e2e/my-courses.e2e.ts
+++ tutor/specs/e2e/my-courses.e2e.ts
@@ -1,21 +1,25 @@
-import { Course } from '../../src/store/types'
-import { visitPage, Mocker, setTimeouts } from './helpers'
+
+import { visitPage, faker, setTimeouts, loginAs, findOrCreateTeacherAccount } from './helpers'
 
 describe('My Courses', () => {
-
-    const mock = Mocker.mock({
-        page,
-        routes: {},
-    })
 
     beforeEach(async () => {
         await setTimeouts()
     })
 
     describe('Onboarding', () => {
-        it('allows a new teacher select and suggest subjects', async () => {
-            mock.current.bootstrapData.courses = []
+
+        beforeEach(async () => {
+            await findOrCreateTeacherAccount({
+                userName: faker.internet.userName(),
+                agreeToTerms: true,
+                page,
+            })
             await visitPage(page, '/courses')
+        })
+
+        it('allows a new teacher select and suggest subjects', async () => {
+
             await expect(page).toHaveSelector('testEl=new-teacher-screen')
             await page.type('testEl=input-suggested-subject', 'test')
             await page.click('testEl=submit-suggested-subject')
@@ -38,159 +42,120 @@ describe('My Courses', () => {
             await page.click('testEl=offering-0', { force: true })
             await page.click('testEl=show-detail')
         })
+
     })
 
     describe('Dashboard', () => {
-        it('shows new UI', async () => {
-            mock.current.bootstrapData.courses.forEach((c: any) => {
-                c.offering_id = mock.current.bootstrapData.offerings[0].id
+        describe('single course teacher', () => {
+            beforeEach(async () => {
+                await loginAs({ page, userName: 'teacher01' })
             })
-            await visitPage(page, '/courses')
-            await expect(page).toHaveSelector('testEl=existing-teacher-screen')
-            await expect(page).toHaveSelector('testEl=offering-container')
-            //course card
-            await expect(page).toHaveSelector('testEl=course-card')
-            // //preview card
-            await expect(page).toHaveSelector('testEl=preview-course-card')
-            // //create course card
-            await expect(page).toHaveSelector('testEl=create-course')
-    
-            //Resources
-            await page.click('testEl=resources-tab')
-            await expect(page).toHaveText('testEl=getting-started-guide', 'Instructor Getting Started Guide')
-            await expect(page).toHaveText('testEl=getting-started-video', ' Video Tutorials ')
-        })
-    
-        it('enters edit mode', async () => {
-            const getOfferingIds = () => {
-                return Array.from(document.querySelectorAll<HTMLDivElement>('.offering-container'))
-                    .reduce((current, el) => el.dataset?.offeringId 
-                        ? current.concat(el.dataset.offeringId) : current, [] as string[])
-            }
 
-            mock.current.bootstrapData.courses.forEach((c: any, index: number) => {
-                //grouping courses with the first two offerings, id 1 and id 2
-                c.offering_id = mock.current.bootstrapData.offerings[index % 2].id
+            it('can add an offering', async () => {
+                await page.click('testEl=dashboard-settings-btn')
+                expect(await page.$eval('testEl=dashboard-settings-btn', node => node.textContent)).toEqual('Exit settings')
+
+                const offerings = await page.$$('testEl=offering-container')
+                await page.click('testEl=add-subject-dropdown-btn')
+                // there is a delay between clicking the offering and scrolling to the top
+                await page.click('.dropdown-menu.show .offering-item:not(.disabled)', { timeout: 500 })
+                const updatedOfferings = await page.$$('testEl=offering-container')
+                // we added a new offering
+                expect(updatedOfferings).toHaveLength(offerings.length + 1)
+
+                await page.click('testEl=dashboard-settings-btn')
+                expect(await page.$eval('testEl=dashboard-settings-btn', node => node.textContent)).toEqual('Manage subjects')
             })
-            await visitPage(page, '/courses')
-            await expect(page).toHaveSelector('testEl=existing-teacher-screen')
 
-            await page.click('testEl=dashboard-settings-btn')
-            expect(page).toHaveText('testEl=dashboard-settings-btn', 'Exit settings')
-            await expect(page).toHaveSelector('testEl=edit-mode-icons')
-            await expect(page).toHaveSelector('testEl=add-subject-dropdown')
-
-            // [1, 2]
-            const offeringOrderIds = await page.evaluate(getOfferingIds)
-            // ordering the offerings
-            await page.click(`.offering-container[data-offering-id="${offeringOrderIds[0]}"] .ox-icon-arrow-down`)
-            expect(await page.evaluate(getOfferingIds)).toEqual(offeringOrderIds.reverse())
-
-            const offerings = await page.$$('testEl=offering-container')
-            await page.click('testEl=add-subject-dropdown-btn')
-            // there is a delay between clicking the offering and scrolling to the top
-            await page.click('.dropdown-menu.show .offering-item:not(.disabled)', { timeout: 500 })
-            const updatedOfferings = await page.$$('testEl=offering-container')
-            // we added a new offering
-            expect(updatedOfferings).toHaveLength(offerings.length + 1)
-
-            await page.click('testEl=dashboard-settings-btn')
-            expect(await page.$eval('testEl=dashboard-settings-btn', node => node.textContent)).toEqual('Manage subjects')
         })
 
-        it('displays a preview course', async () => {
-            mock.current.bootstrapData.courses.forEach((c: any) => {
-                c.offering_id = mock.current.bootstrapData.offerings[0].id
+        describe('multi course teacher', () => {
+
+
+            beforeEach(async () => {
+                await loginAs({ page, userName: 'reviewteacher' })
+                await visitPage(page, '/courses')
             })
-            await visitPage(page, '/courses')
-            await page.click('testEl=preview-course-item-title')
-            await page.waitForNavigation()
-            expect(
-                await page.evaluate(() => window.location.pathname)
-            ).toMatch(/course\/\d+/)
-        })
-    
-        it('hides concept coach courses', async () => {
-            const c = mock.current.course(1)
-            c.is_concept_coach = true
-            c.name = 'ConceptCoach'
-            await visitPage(page, '/courses')
-            await expect(page).toHaveSelector('testEl=existing-teacher-screen')
-            await expect(page).not.toHaveText('testEl=course-card', 'ConceptCoach', { timeout: 100 })
-        })
 
-        it('deletes offering', async () => {
-            let courseIdWithStudents;
-            const offeringIdWithStudents = mock.current.bootstrapData.offerings[0].id
-            const offeringIdWithEmptyStudents = mock.current.bootstrapData.offerings[1].id
-            mock.current.bootstrapData.courses.forEach((c: Course, index: number) => {
-                if(index === 0) {
-                    // leave one course with students
-                    courseIdWithStudents = c.id;
-                    c.offering_id = offeringIdWithStudents
-                } else {
-                    // rest of the course give a current offering, and set number of students to 0
-                    c.offering_id = offeringIdWithEmptyStudents
-                    c.periods.forEach(p => p.num_enrolled_students = 0)
+            it('shows new UI', async () => {
+                await expect(page).toHaveSelector('testEl=existing-teacher-screen')
+                await expect(page).toHaveSelector('testEl=offering-container')
+                //course card
+                await expect(page).toHaveSelector('testEl=course-card')
+                // //preview card
+                await expect(page).toHaveSelector('testEl=preview-course-card')
+                // //create course card
+                await expect(page).toHaveSelector('testEl=create-course')
+
+                //Resources
+                await page.click('testEl=resources-tab')
+                await expect(page).toHaveText('testEl=getting-started-guide', 'Instructor Getting Started Guide')
+                await expect(page).toHaveText('testEl=getting-started-video', ' Video Tutorials ')
+            })
+
+            it('moves offerings', async () => {
+                await expect(page).toHaveSelector('testEl=existing-teacher-screen')
+
+                await page.click('testEl=dashboard-settings-btn')
+                expect(page).toHaveText('testEl=dashboard-settings-btn', 'Exit settings')
+                await expect(page).toHaveSelector('testEl=edit-mode-icons')
+                await expect(page).toHaveSelector('testEl=add-subject-dropdown')
+
+                const getOfferingIds = () => {
+                    return Array.from(document.querySelectorAll<HTMLDivElement>('.offering-container'))
+                        .reduce((current, el) => el.dataset?.offeringId
+                            ? current.concat(el.dataset.offeringId) : current, [] as string[])
                 }
+
+                // [1, 2]
+                const offeringOrderIds = await page.evaluate(getOfferingIds)
+
+                // ordering the offerings
+                await page.click(`.offering-container[data-offering-id="${offeringOrderIds[0]}"] .ox-icon-arrow-down`)
+                expect(await page.evaluate(getOfferingIds)).toEqual(offeringOrderIds.reverse())
             })
 
-            // delete an offering block
-            await visitPage(page, '/courses')
-            await page.click('testEl=dashboard-settings-btn')
-            await page.click(`.offering-container[data-offering-id="${offeringIdWithEmptyStudents}"] >> testEl=delete-offering`)
-            await expect(page).toHaveSelector('testEl=delete-offering-modal')
-            await page.click('testEl=delete-offering-modal-btn')
-            await expect(page).not.toHaveSelector(`.offering-container[data-offering-id="${offeringIdWithEmptyStudents}"]`, { timeout: 100 })
-
-            // go to a course
-            await page.click('testEl=dashboard-settings-btn')
-            await page.click(`[data-course-id="${courseIdWithStudents}"]`)
-            expect(
-                await page.evaluate(() => document.location.pathname)
-            ).toMatch(`/course/${courseIdWithStudents}`)
-
-            // go back to dashboard and make sure the offering block is not shown
-            await page.goBack()
-            await expect(page).not.toHaveSelector(`.offering-container[data-offering-id="${offeringIdWithEmptyStudents}"]`, { timeout: 100 })
-
-            // refreshing the page and make sure the offering block is not shown
-            await visitPage(page, '/courses')
-            await expect(page).not.toHaveSelector(`.offering-container[data-offering-id="${offeringIdWithEmptyStudents}"]`, { timeout: 100 })
-        })
-        
-        it('goes to course settings details tab when clicking on "Course Settings" option', async () => {
-            mock.current.bootstrapData.courses.forEach((c: any) => {
-                c.offering_id = mock.current.bootstrapData.offerings[0].id
+            it('displays a preview course', async () => {
+                await page.click('testEl=preview-course-item-title')
+                await page.waitForNavigation()
+                expect(
+                    await page.evaluate(() => window.location.pathname)
+                ).toMatch(/course\/\d+/)
             })
-            await visitPage(page, '/courses')
-            await page.waitForSelector('testEl=offering-container')
-            const courseId = await page.$eval('.offering-container .my-courses-item' , ex => ex.dataset.courseId);
-            await page.click(`testEl=course-card-item-actions-${courseId}`)
-            await page.click(`testEl=course-card-item-actions-course-settings-${courseId}`)
-            expect(
-                await page.evaluate(() => `${document.location.pathname}${document.location.search}`)
-            ).toMatch(`/course/${courseId}/settings?tab=1`)
+
+
+            it('goes to course settings details tab when clicking on "Course Settings" option', async () => {
+                await page.waitForSelector('testEl=offering-container')
+                const courseId = await page.$eval('.offering-container .my-courses-item' , ex => ex.dataset.courseId);
+                await page.click(`testEl=course-card-item-actions-${courseId}`)
+                await page.click(`testEl=course-card-item-actions-course-settings-${courseId}`)
+                expect(
+                    await page.evaluate(() => `${document.location.pathname}${document.location.search}`)
+                ).toMatch(`/course/${courseId}/settings?tab=1`)
+            })
         })
+
     })
 
-    describe('unavailable messages', () => {
-        it('displays pending message', async () => {
-            mock.current.bootstrapData.courses = []
-            mock.current.bootstrapData.user.can_create_courses = false
-            mock.current.bootstrapData.user.created_at = (new Date()).toISOString()
+    describe('invalid teacher', () => {
+        beforeEach(async () => {
+            await findOrCreateTeacherAccount({
+                userName: faker.internet.userName(),
+                agreeToTerms: true,
+                isVerified: false,
+                page,
+            })
+        })
 
+        it('displays pending message', async () => {
             await visitPage(page, '/courses')
             await expect(page).toHaveText('testEl=pending-verification', 'Pending faculty verification')
         })
 
-        it('displays not available message', async () => {
-            mock.current.bootstrapData.courses = []
-            mock.current.bootstrapData.user.can_create_courses = false
-            const now = new Date()
-            now.setDate(now.getDate() - 7)
-            mock.current.bootstrapData.user.created_at = now.toISOString()
+        it('displays not available message when account is old', async () => {
             await visitPage(page, '/courses')
+            await page.waitForTimeout(100) // let page fully render so models are set up
+            await page.evaluate(() => window._MODELS.user.created_at = '2010-01-01')
             await expect(page).toHaveText('testEl="non-allowed-instructors"', 'not able to offer')
         })
     })
