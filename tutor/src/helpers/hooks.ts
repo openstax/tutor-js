d052c1f1d849c623ecf6d5ec4a42188e33aee4df
Nathan Stitt <nathan@stitt.org>
2021-05-11 16:46:48 -0500

    a useCourse hook

--- tutor/src/helpers/hooks.ts
+++ tutor/src/helpers/hooks.ts
@@ -1,5 +1,8 @@
-import { currentCourses, currentOfferings, CoursesMap } from '../models'
+import { currentCourses, currentOfferings, CoursesMap, ID } from '../models'
+import { useParams } from 'react-router-dom';
 
 export const useAvailableOfferings = (courses: CoursesMap = currentCourses) => (
     currentOfferings.array.filter(o => o.is_available || o.is_preview_available || courses.forOffering(o).any)
 )
+
+export const useCourse = (courseId: ID = useParams<{ courseId: string }>().courseId) => currentCourses.get(courseId)
