a4d4959a811c343e345602507232d5f564eda13b
Nathan Stitt <nathan@stitt.org>
2021-05-11 16:47:59 -0500

    kill hot accepting
    it wasn't working correctly, better to just reload page

--- tutor/index.js
+++ tutor/index.js
@@ -2,14 +2,8 @@ import renderRoot from 'shared/helpers/render-root';
 import { TutorApp } from './src/models/app';
 
 TutorApp.boot().then(async (app) => {
-    const rootRenderer = await renderRoot({
+    await renderRoot({
         Component: TutorApp.rootComponent,
         props: { app },
     });
-    if (module.hot) {
-        module.hot.accept('./src/models/app', async () => {
-            const NewApp = await import('./src/models/app');
-            rootRenderer(NewApp.default.rootComponent);
-        });
-    }
 });
