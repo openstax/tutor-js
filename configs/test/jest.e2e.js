cd9f64820ea0e4539f3e0b8c2f6263b790c274d9
Josiah Ivey <josiah.ivey@gmail.com>
2021-05-18 09:22:29 -0700

    Revert "test runinband"
    This reverts commit 40b2129bb06094ae86e058a23c9267b497e27e86.

--- configs/test/jest.e2e.js
+++ configs/test/jest.e2e.js
@@ -5,7 +5,6 @@ const DEBUG = !!process.env.DEBUG;
 const config = {
     rootDir: '../../tutor',
     testRegex: 'specs/e2e/.*\\.e2e\\.(ts|js)$',
-    testSequencer: '../configs/test/jest.sequencer.js',
     preset: 'jest-playwright-preset',
     transform: {
         '^.+\\.jsx?$': 'babel7-jest',
