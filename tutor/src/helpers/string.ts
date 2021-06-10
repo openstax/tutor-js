5f035fec1605344ff1dc69670692da5f2f5fb12b
Nathan Stitt <nathan@stitt.org>
2021-06-10 13:05:21 -0500

    export default for modules that use it that way

--- tutor/src/helpers/string.ts
+++ tutor/src/helpers/string.ts
@@ -2,5 +2,7 @@ export function assignmentHeaderText(type: string) {
     if(type === 'external') return 'external assignment';
     return type;
 }
+import S from 'shared/helpers/string'
 
 export * from 'shared/helpers/string'
+export default S
