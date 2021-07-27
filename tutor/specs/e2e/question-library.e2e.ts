2ea070ed9c75220d693edd5d1098467fb00022ae
Josiah Ivey <josiah.ivey@gmail.com>
2021-07-27 15:50:20 -0700

    fix question library spec

--- tutor/specs/e2e/question-library.e2e.ts
+++ tutor/specs/e2e/question-library.e2e.ts
@@ -1,18 +1,18 @@
-import { visitPage, test, expect } from './test'
+import { visitPage, test, expect, withUser } from './test'
 
 test.describe('Question Library', () => {
 
-    test.skip() // FIXME
+    withUser('teacher01')
 
     test.beforeEach(async ({ page }) => {
         await visitPage(page, '/course/1/questions')
     })
 
     test('can select a chapter and display questions', async ({ page }) => {
-        await page.click('testId=section-chapter-chooser >> [data-chapter-section="1"] button')
+        await page.click('testId=section-chapter-chooser >> [data-section-id="1"] svg')
         await page.click('testId=show-questions')
         await expect(page).toHaveSelector('.exercises-display')
         // all exercises has 'science-practice:modeling' from the test server
-        await expect(page).toMatchText('testId=tag-type-science-practice', /science-practice:modeling/)
+        await expect(page).toMatchText('testId=tag-type-lo', /stax-phys/)
     });
 })
