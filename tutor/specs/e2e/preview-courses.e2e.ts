47d568558b0b2532bdc349a368f48cc67e9589f6
Josiah Ivey <josiah.ivey@gmail.com>
2021-07-27 15:02:48 -0700

    fix preview course spec

--- tutor/specs/e2e/preview-courses.e2e.ts
+++ tutor/specs/e2e/preview-courses.e2e.ts
@@ -1,13 +1,8 @@
-import { visitPage, test, expect, loaderNotVisible, findOrCreateTeacherAccount } from './test'
+import { visitPage, test, expect, findOrCreateTeacherAccount } from './test'
 
 
 test.describe('Preview Courses', () => {
-
-    test.skip() // FIXME
-
-    test.beforeAll(async ({ browser }) => {
-        const context = await browser.newContext()
-        const page = await context.newPage()
+    test('displays a collapsible side panel that creates a course', async ({ page }) => {
         await findOrCreateTeacherAccount({ page, userName: 'previewteacher' })
         await page.waitForSelector('.tutor-navbar')
         const onOfferingScreen = await page.$('data-test-id=new-teacher-screen')
@@ -16,38 +11,35 @@ test.describe('Preview Courses', () => {
             await page.click('testId=show-detail')
             await page.click('testId=create-preview')
         }
-    })
 
-    test('displays a collapsible side panel that creates a course', async ({ page }) => {
         await visitPage(page, '/courses')
         await page.click('testId=preview-course-card')
         await page.click('testId=preview-panel-create-course')
         await expect(page).toHaveSelector('testId=new-course-wizard')
         await expect(page).toHaveSelector('.select-dates')
+
     })
 
-    test('hides preview panel for non-preview courses', async ({ page }) => {
+    test('hides preview panel if the offering is not available for course creation', async ({ page }) => {
+        await findOrCreateTeacherAccount({ page, userName: 'previewteacher' })
+        await page.waitForSelector('.tutor-navbar')
+
         await visitPage(page, '/courses')
         await page.click('testId=preview-course-card')
-
-        await page.goBack()
         await page.evaluate(() => {
             window._MODELS.offerings.array.forEach((a:any) => a.is_available = false)
         })
-        await page.goForward()
-
-        await expect(page).not.toHaveSelector('testId=side-panel >> testId=preview-panel-create-course', { timeout: 100 })
+        await expect(page).not.toHaveSelector('testId=side-panel >> testId=preview-panel-create-course')
     })
 
-    test('hides preview panel for non-available offerings', async ({ page }) => {
+    test('hides preview panel for real courses and the associated preview course', async ({ page }) => {
+        await findOrCreateTeacherAccount({ page, userName: 'teacher01' })
+
+        await visitPage(page, '/courses')
+        await page.click('testId=course-card-1')
+        await expect(page).not.toHaveSelector('testId=side-panel >> testId=preview-panel-create-course')
         await visitPage(page, '/courses')
         await page.click('testId=preview-course-card')
-        await page.goBack()
-        await page.evaluate(() => {
-            window._MODELS.offerings.array.forEach((a:any) => a.is_preview_available = false)
-        })
-        await page.goForward()
-        await loaderNotVisible()
-        await expect(page).not.toHaveSelector('testId=side-panel >> testId=preview-panel-create-course', { timeout: 100 })
+        await expect(page).not.toHaveSelector('testId=side-panel >> testId=preview-panel-create-course')
     })
 })
