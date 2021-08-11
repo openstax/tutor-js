195414bb0baaa332766cda49babde21baa2c2bc0
Nathan Stitt <nathan@stitt.org>
2021-08-11 17:03:05 -0500

    create context for each login

--- tutor/specs/e2e/setup.ts
+++ tutor/specs/e2e/setup.ts
@@ -1,5 +1,5 @@
 import { chromium } from '@playwright/test'
-const fs = require('fs')
+import fs from 'fs'
 import { loginAs } from './helpers'
 
 const AUTH_SENTINEL = 'temp/auth-complete'
@@ -8,15 +8,20 @@ const preAuthUsers = async () => {
     if (fs.existsSync(AUTH_SENTINEL)) {
         return
     }
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
     for (const n of [1, 2]) {
+        const browser = await chromium.launch();
+        const page = await browser.newPage();
         await loginAs(`teacher0${n}`, page)
         await page.context().storageState({ path: `temp/teacher0${n}-state.json` });
+    }
+
+    for (const n of [1, 2]) {
+        const browser = await chromium.launch();
+        const page = await browser.newPage();
         await loginAs(`reviewstudent${n}`, page)
         await page.context().storageState({ path: `temp/reviewstudent${n}-state.json` });
+        await browser.close();
     }
-    await browser.close();
     fs.closeSync(fs.openSync(AUTH_SENTINEL, 'w'));
 }
 
