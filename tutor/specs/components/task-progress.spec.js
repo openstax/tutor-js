9175ac30412065bdcb123e3478af9683d475673b
Dante Soares <dante.m.soares@rice.edu>
2021-08-13 10:40:51 -0500

    Fixed lint issues in the task progress spec

--- tutor/specs/components/task-progress.spec.js
+++ tutor/specs/components/task-progress.spec.js
@@ -1,29 +1,29 @@
-import { renderPointsScoredCell } from '../../src/components/task-progress';
+import { renderPointsScoredCell } from '../../src/components/task-progress'
 
 describe('Task Progress', () => {
     describe('renderPointsScoredCell', () => {
         it('renders ungraded icon and popover if the step is completed and points are null', () => {
-            const step = { is_completed: true, pointsScored: null, isLate: false, }
+            const step = { is_completed: true, pointsScored: null, isLate: false }
 
-            expect(renderPointsScoredCell(step)).toMatchSnapshot();
-        });
+            expect(renderPointsScoredCell(step)).toMatchSnapshot()
+        })
 
         it('renders late icon and score popover if the step is late', () => {
-            const step = { is_completed: true, pointsScored: 0.0, isLate: true, }
+            const step = { is_completed: true, pointsScored: 0.0, isLate: true }
 
-            expect(renderPointsScoredCell(step)).toMatchSnapshot();
-        });
+            expect(renderPointsScoredCell(step)).toMatchSnapshot()
+        })
 
         it('does not render a popover for incomplete steps', () => {
-            const step = { is_completed: false, pointsScored: null, isLate: false, }
+            const step = { is_completed: false, pointsScored: null, isLate: false }
 
-            expect(renderPointsScoredCell(step)).toMatchSnapshot();
-        });
+            expect(renderPointsScoredCell(step)).toMatchSnapshot()
+        })
 
         it('does not render a popover for graded steps completed on time', () => {
-            const step = { is_completed: true, pointsScored: 0.0, isLate: false, }
+            const step = { is_completed: true, pointsScored: 0.0, isLate: false }
 
-            expect(renderPointsScoredCell(step)).toMatchSnapshot();
-        });
-    });
-});
+            expect(renderPointsScoredCell(step)).toMatchSnapshot()
+        })
+    })
+})
