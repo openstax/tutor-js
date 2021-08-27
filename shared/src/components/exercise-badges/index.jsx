1e99e97bb16736a8f2edde4e23dde5ee9010cb69
Josiah Ivey <josiah.ivey@gmail.com>
2021-08-27 15:51:56 -0700

    set displayname for observed component

--- shared/src/components/exercise-badges/index.jsx
+++ shared/src/components/exercise-badges/index.jsx
@@ -143,6 +143,7 @@ const ExerciseBadges = observer(({ className, ...badgeProps }) => {
         </div>
     );
 });
+ExerciseBadges.displayName = 'ExerciseBadges';
 
 export default ExerciseBadges;
 
