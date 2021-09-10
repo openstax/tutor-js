8753b93cba5e8f5d61d08fb56d422e769c373677
JD Ivey <josiah.ivey@gmail.com>
2021-09-10 13:18:07 -0700

    fix response validation flexbox issue (#3749)
    Co-authored-by: Thomas Woodward <tom.g.woodward@gmail.com>

--- tutor/src/screens/task/step/exercise-free-response.js
+++ tutor/src/screens/task/step/exercise-free-response.js
@@ -26,18 +26,22 @@ const TextAreaErrorStyle = css`
 const InfoRow = styled.div`
   margin: 8px 0;
   display: flex;
-  justify-content: ${props => props.hasSubmitted ? 'space-between' : 'flex-end'};
+  justify-content: space-between;
   line-height: 1.6rem;
+
   .word-limit-error-info {
     color: ${colors.danger};
   }
+
   span {
     font-size: 12px;
     line-height: 16px;
+
     + span {
       margin-left: 1rem;
     }
-  };
+  }
+
   color: ${colors.neutral.thin};
 `;
 
