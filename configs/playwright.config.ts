93af1caead34135c44045f27193581e15efd8328
Josiah Ivey <josiah.ivey@gmail.com>
2021-09-27 11:31:46 -0700

    test removing timeout

--- configs/playwright.config.ts
+++ configs/playwright.config.ts
@@ -6,7 +6,6 @@ const config: PlaywrightTestConfig = {
     testDir: '../tutor/specs/e2e',
     testMatch: '*.e2e.ts',
     workers: 2,
-    timeout: 60000,
     forbidOnly: !!process.env.CI,
     globalSetup: '../tutor/specs/e2e/setup.ts',
     use: {
