ba9e62928773cdb772fc389147e1330aca44013f
Josiah Ivey <josiah.ivey@gmail.com>
2021-11-11 13:40:03 -0800

    add clarifying comment

--- exercises/src/models/exercises.ts
+++ exercises/src/models/exercises.ts
@@ -92,6 +92,7 @@ export class ExercisesMap extends Map<ID, Exercise | ExerciseVersions> {
         } else {
             url = urlFor('saveExistingDraft', { number: exercise.number })
         }
+        // Prevent method 'all' from TagsAssociation being serialized into tags array
         const data = merge(exercise.toJSON(), { 'tags': exercise.tags.all.toJSON() })
         this.onSaved(
             await this.api.request(url, { data: data }),
