a2f1f16f8b3cbfa3700ba5994c4a238d9c4f2b5e
Dante Soares <dante.m.soares@rice.edu>
2022-03-02 16:27:59 -0600

    Fixed failing exercises tests

--- exercises/specs/components/search/book-sections.spec.jsx
+++ exercises/specs/components/search/book-sections.spec.jsx
@@ -75,42 +75,42 @@ describe('Search book sections', function() {
             )).toEqual([
                 {
                     label: '<span data-type="" itemprop="" class="os-text">Preface</span>',
-                    value: 'db36053c-5281-42f4-90ec-afcc21ab28c3',
+                    value: 'preface',
                 },
                 {
                     label: '<span class="os-number"><span class="os-part-text">Chapter </span>1</span><span class="os-divider"> </span><span data-type="" itemprop="" class="os-text">The Study of Life</span>',
                     options: [
                         {
                             label: '<span data-type="" itemprop="" class="os-text">Introduction</span>',
-                            value: '2230ab90-3137-4dcb-b6bd-72630222948c',
+                            value: '1-introduction',
                         },
                         {
                             label: '<span class="os-number">1.1</span><span class="os-divider"> </span><span data-type="" itemprop="" class="os-text">The Science of Biology</span>',
-                            value: 'ce6cfde4-1ac3-480f-aa0f-4777111f9e23',
+                            value: '1-1-the-science-of-biology',
                         },
                         {
                             label: '<span class="os-number">1.2</span><span class="os-divider"> </span><span data-type="" itemprop="" class="os-text">Themes and Concepts of Biology</span>',
-                            value: '07dbec85-8530-4911-b863-5d73b5d7e211',
+                            value: '1-2-themes-and-concepts-of-biology',
                         },
                         {
                             label: '<span class="os-text">Key Terms</span>',
-                            value: '761608a3-5b67-533e-a199-c03f0b88a95b',
+                            value: '1-key-terms',
                         },
                         {
                             label: '<span class="os-text">Chapter Summary</span>',
-                            value: '0d3ce2c6-8ebe-5f8e-b3d4-1c91cfca2a91',
+                            value: '1-chapter-summary',
                         },
                         {
                             label: '<span class="os-text">Visual Connection Questions</span>',
-                            value: '1c6c7f98-e992-536c-9769-cf24cefe451d',
+                            value: '1-visual-connection-questions',
                         },
                         {
                             label: '<span class="os-text">Review Questions</span>',
-                            value: '3eec7061-a32e-529d-a594-b9f53aed0331',
+                            value: '1-review-questions',
                         },
                         {
                             label: '<span class="os-text">Critical Thinking Questions</span>',
-                            value: 'bbe32ad1-f92d-5f62-b911-4fde45c04a58',
+                            value: '1-critical-thinking-questions',
                         },
                     ],
                 },
